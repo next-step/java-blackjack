e59facd546991c44461c83ed6ccddaf1c3242c59
koeonyack <covenant.ko@kakao.com>
2021-02-04 19:19:56 +0900

    feat: K5 구현체 추가

---
+++ src/main/java/rentcar/K5.java
@@ -0,0 +1,26 @@
+package rentcar;
+
+public class K5 extends Car {
+    private final static String CAR_NAME = "K5";
+    private final static int DISTANCE_PER_LITER = 13;
+    private final double tripDistance;
+
+    public K5(double tripDistance) {
+        this.tripDistance = tripDistance;
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return DISTANCE_PER_LITER;
+    }
+
+    @Override
+    double getTripDistance() {
+        return tripDistance;
+    }
+
+    @Override
+    String getName() {
+        return CAR_NAME;
+    }
+}
