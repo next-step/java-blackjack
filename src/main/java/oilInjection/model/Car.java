5b71a20f61c6ac1a45ba7884ef7e90f22864a723
KimBB <tree9295@gmail.com>
2021-02-04 17:12:41 +0900

    feat: Create Car abstract class
    - 자동차 추상 클래스 생성

--- src/main/java/oilInjection/model/Car.java
+++ src/main/java/oilInjection/model/Car.java
@@ -1,5 +1,15 @@
 package oilInjection.model;
 
 public abstract class Car {
+    protected double distancePerLiter;
+    protected double tripDistance;
+    protected String name;
 
+    abstract double getDistancePerLiter();
+    abstract double getTripDistance();
+    abstract String getName();
+
+    double getChargeQuantity() {
+        return getTripDistance() / getDistancePerLiter();
+    }
 }
