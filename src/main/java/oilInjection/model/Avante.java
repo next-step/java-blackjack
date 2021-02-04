2e29b55712f36480483d57e5d683f87ae6ffde4c
KimBB <tree9295@gmail.com>
2021-02-04 17:12:59 +0900

    feat: Create Avante class
    - Avante 자동차 클래스

--- src/main/java/oilInjection/model/Avante.java
+++ src/main/java/oilInjection/model/Avante.java
@@ -1,4 +1,24 @@
 package oilInjection.model;
 
-public class Avante {
+public class Avante extends Car {
+    public Avante(double tripDistance){
+        this.tripDistance = tripDistance;
+        this.distancePerLiter = 15;
+        this.name = "Avante";
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return this.distancePerLiter;
+    }
+
+    @Override
+    double getTripDistance() {
+        return this.tripDistance;
+    }
+
+    @Override
+    String getName() {
+        return this.name;
+    }
 }
