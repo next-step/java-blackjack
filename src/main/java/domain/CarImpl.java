9f792afe6cda6cbedd6d6c62cd12fbfff58cf8d6
jeongdaeun98 <uneap@sk.com>
2021-02-04 17:47:19 +0900

    feat: create CarImpl

---
+++ src/main/java/domain/CarImpl.java
@@ -0,0 +1,15 @@
+package domain;
+
+public abstract class CarImpl implements Car {
+
+    abstract double getDistancePerLiter();
+
+    abstract double getTripDistance();
+
+    abstract String getName();
+
+    @Override
+    public double getChargeQuantity() {
+        return getTripDistance() / getDistancePerLiter();
+    }
+}
