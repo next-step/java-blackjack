0b672b71efea5f509668288d6ef768e29b7e5f0d
KimBB <tree9295@gmail.com>
2021-02-04 17:15:31 +0900

    feat: Create Kfive(K5) class
    - Kfive(K5) 자동차 클래스

--- src/main/java/oilInjection/model/Kfive.java
+++ src/main/java/oilInjection/model/Kfive.java
@@ -1,4 +1,24 @@
 package oilInjection.model;
 
-public class Kfive {
+public class Kfive extends Car{
+    public Kfive(double tripDistance){
+        this.tripDistance = tripDistance;
+        this.distancePerLiter = 13;
+        this.name = "Kfive";
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return this.distancePerLiter;
+    }
+
+    @Override
+    double getTripDistance() {
+        return this.tripDistance;
+    }
+
+    @Override
+    String getName() {
+        return this.name;
+    }
 }
