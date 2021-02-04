7ceb0c4749628515dae169712185a625f254114a
jeongdaeun98 <uneap@sk.com>
2021-02-04 18:20:59 +0900

    feat: create generateReport and addCar function

--- src/main/java/domain/RentCompany.java
+++ src/main/java/domain/RentCompany.java
@@ -2,22 +2,32 @@ package domain;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
-public class RentCompany extends Factory {
-    private List<Car> cars;
+public class RentCompany {
 
-    @Override
-    protected RentCompany createRentCompany() {
+    private List<CarImpl> cars;
+    private static final String DELIMITER = "";
+    private static final String COLON = " : ";
+    private static final String LITTER_NEW_LINE = "리터\n";
+
+    public static RentCompany create() {
         RentCompany rentCompany = new RentCompany();
         rentCompany.cars = new ArrayList<>();
         return rentCompany;
     }
 
     public String generateReport() {
-        return new String();
+        return cars.stream().map(car -> String.join(
+            DELIMITER,
+            car.getName(),
+            COLON,
+            String.format("%d", (int) Math.ceil(car.getChargeQuantity())),
+            LITTER_NEW_LINE
+        )).collect(Collectors.joining());
     }
 
     public void addCar(CarImpl car) {
-
+        cars.add(car);
     }
 }
