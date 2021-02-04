52894739b391a555906d1520106b9c8fd5ffe226
koeonyack <covenant.ko@kakao.com>
2021-02-04 19:20:08 +0900

    feat: Avante 구현체 추가

---
+++ src/main/java/rentcar/Avante.java
@@ -0,0 +1,26 @@
+package rentcar;
+
+public class Avante extends Car {
+    private final static String CAR_NAME = "아반떼";
+    private final static int DISTANCE_PER_LITER = 15;
+    private final double tripDistance;
+
+    public Avante(double tripDistance) {
+        this.tripDistance = tripDistance;
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return DISTANCE_PER_LITER;
+    }
+
+    @Override
+    double getTripDistance() {
+        return tripDistance;
+    }
+
+    @Override
+    String getName() {
+        return CAR_NAME;
+    }
+}
