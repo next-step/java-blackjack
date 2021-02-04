9f9f93cbada96e3797ea53f9133935259b4b4320
jeongdaeun98 <uneap@sk.com>
2021-02-04 17:05:24 +0900

    feat: create Avante, K5, Sonata that inherit the car

---
+++ src/main/java/domain/Avante.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class Avante extends Car {
+
+    public Avante(int i) {
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return 0;
+    }
+
+    @Override
+    double getTripDistance() {
+        return 0;
+    }
+
+    @Override
+    String getName() {
+        return null;
+    }
+}
---
+++ src/main/java/domain/K5.java
@@ -0,0 +1,22 @@
+package domain;
+
+public class K5 extends Car {
+
+    public K5(int i) {
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return 0;
+    }
+
+    @Override
+    double getTripDistance() {
+        return 0;
+    }
+
+    @Override
+    String getName() {
+        return null;
+    }
+}
