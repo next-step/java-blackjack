bedd455ea87eaf5cd72cd58193543ddb625146b3
jeongdaeun98 <uneap@sk.com>
2021-02-04 17:05:56 +0900

    feat: create RentCompany

---
+++ src/main/java/domain/RentCompany.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class RentCompany {
+
+    public static RentCompany create() {
+        return new RentCompany();
+    }
+
+    public String generateReport() {
+        return new String();
+    }
+
+    public void addCar(Car car) {
+    }
+}
