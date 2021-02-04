bc72de5954db01cdb8746d5220fcccec4133b507
KimBB <tree9295@gmail.com>
2021-02-04 17:15:16 +0900

    feat: Create Sonata class
    - Sonata 자동차 클래스

--- src/main/java/oilInjection/model/Sonata.java
+++ src/main/java/oilInjection/model/Sonata.java
@@ -1,4 +1,24 @@
 package oilInjection.model;
 
-public class Sonata {
+public class Sonata extends Car{
+    public Sonata(double tripDistance){
+        this.tripDistance = tripDistance;
+        this.distancePerLiter = 10;
+        this.name = "Sonata";
+    }
+
+    @Override
+    double getDistancePerLiter() {
+        return this.distancePerLiter;
+    }
+
+    @Override
+    double getTripDistance() {
+        return this.tripDistance;
+    }
+
+    @Override
+    String getName() {
+        return this.name;
+    }
 }
