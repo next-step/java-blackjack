5e27ffbcbeac8fe2fee65ecdde0b043ee5e82425
KimBB <tree9295@gmail.com>
2021-02-04 17:21:14 +0900

    feat: Create Distance class
    - 거리 속성 (원시값) 포장 클래스

---
+++ src/main/java/oilInjection/model/Distance.java
@@ -0,0 +1,13 @@
+package oilInjection.model;
+
+public class Distance {
+    private final double distance;
+
+    public Distance(final double distance){
+        this.distance = distance;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+}
