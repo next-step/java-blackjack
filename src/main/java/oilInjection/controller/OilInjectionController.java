433eed5e1def25509f0aaa7fd7f3172d6fb714af
d36choi <yuio1126@gmail.com>
2021-02-04 17:52:08 +0900

    feat: Create OilInjectionController

---
+++ src/main/java/oilInjection/controller/OilInjectionController.java
@@ -0,0 +1,18 @@
+package oilInjection.controller;
+
+import oilInjection.RentCompany;
+import oilInjection.model.Car;
+
+import java.util.List;
+
+public class OilInjectionController {
+
+
+    public void getDistanceForEachCar(RentCompany rentCompany) {
+        System.out.println("차 별 원하는 거리를 입력하세요.");
+       for (Car car : rentCompany.getCarList()) {
+           System.out.println(car.getName());
+
+       }
+    }
+}
