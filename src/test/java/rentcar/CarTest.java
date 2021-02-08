7a29f0cf550725af8c3f7800c0a9ce527e903daa
koeonyack <covenant.ko@kakao.com>
2021-02-08 20:30:58 +0900

    test: car Test 구현
    - N개 자동차 테스트
    - 연비 테스트

--- src/test/java/rentcar/CarTest.java
+++ src/test/java/rentcar/CarTest.java
@@ -1,44 +1,58 @@
 package rentcar;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class CarTest {
+    private static final Map<String, Car> CARS = new HashMap<>();
 
-    @Test
-    @DisplayName("소나타의 리터당 이동 거리를 구하고, 리터당 이동거리가 10인 지 확인하는 테스트")
-    void 소나타의_리터당_이동거리_출력() {
-        final Car car = new Sonata(100);
+    @BeforeAll
+    static void beforeAll() {
+        CARS.put("Sonata", new Sonata(100));
+        CARS.put("Avante", new Avante(200));
+        CARS.put("K5", new K5(300));
+    }
+
+    @DisplayName("자동차들의 리터당 이동거리를 출력하는 테스트")
+    @ParameterizedTest(name = "{index} => 자동차: {0}, 리터당 이동거리: {1}")
+    @CsvSource({"Sonata, 10", "Avante, 15", "K5, 13"})
+    void 자동차들의_리터당_이동거리_출력(String inputCarName, int inputDistancePerLiter) {
+        final Car car = CARS.get(inputCarName);
         final double distancePerLiter = car.getDistancePerLiter();
-        assertThat(distancePerLiter).isEqualTo(10);
+        assertThat(distancePerLiter).isEqualTo(inputDistancePerLiter);
     }
 
-    @ParameterizedTest
-    @ValueSource(doubles = {100.0, 200.0, 300.0, 400.0})
-    @DisplayName("소나타의 이동거리를 출력하는 테스트")
-    void 소나타의_이동거리_출력(double distance) {
-        final Car car = new Sonata(distance);
+    @DisplayName("자동차들의 이동거리를 출력하는 테스트")
+    @ParameterizedTest(name = "{index} => 자동차: {0}, 이동거리: {1}")
+    @CsvSource({"Sonata, 100", "Avante, 200", "K5, 300"})
+    void 자동차들의_이동거리_출력(String inputCarName, int inputTripDistance) {
+        final Car car = CARS.get(inputCarName);
         final double tripDistance = car.getTripDistance();
-        assertThat(tripDistance).isEqualTo(distance);
+        assertThat(tripDistance).isEqualTo(inputTripDistance);
     }
 
-    @Test
-    @DisplayName("소나타의 이름을 출력하는 테스트")
-    void 소나타의_이름을_출력() {
-        final Car car = new Sonata(100);
-        final String name = car.getName();
-        assertThat(name).isEqualTo("소나타");
+    @DisplayName("자동차들의 이름을 출력하는 테스트")
+    @ParameterizedTest(name = "{index} => 자동차 {0}, 이름: {1}")
+    @CsvSource({"Sonata, 소나타", "Avante, 아반떼", "K5, K5"})
+    void 자동차들의_이름을_출력(String inputCarName, String inputCarDisplayName) {
+        final Car car = CARS.get(inputCarName);
+        final String carName = car.getName();
+        assertThat(carName).isEqualTo(inputCarDisplayName);
     }
 
-    @Test
-    @DisplayName("소나타의 거리에 따른 연료량을 출력하는 테스트")
-    void 소나타의_거리에_따른_연료량() {
-        final Car car = new Sonata(100);
+    @DisplayName("자동차들의 거리에 따른 연료량을 출력하는 테스트")
+    @ParameterizedTest(name = "{index} -> 자동차 {0}, 연료량: {1}")
+    @CsvSource({"Sonata, 10.00", "Avante, 13.33", "K5, 23.08"})
+    void 자동차들의_거리에_따른_연료량_출력(String inputCarName, double inputChargeQuantity) {
+        final Car car = CARS.get(inputCarName);
         double chargeQuantity = car.getChargeQuantity();
-        assertThat(chargeQuantity).isEqualTo(10.0);
+        assertThat(chargeQuantity).isEqualTo(inputChargeQuantity);
     }
 }
