b6e9842b1875c9ad65683d90963055a8112862af
d36choi <yuio1126@gmail.com>
2021-02-09 16:56:33 +0900

    feat: equals & hashCode 를 overriding 함.

--- src/main/java/oilInjection/model/Distance.java
+++ src/main/java/oilInjection/model/Distance.java
@@ -1,13 +1,32 @@
 package oilInjection.model;
 
+import java.util.Objects;
+
 public class Distance {
     private final double distance;
 
-    public Distance(final double distance){
+    private Distance(final double distance){
         this.distance = distance;
     }
 
     public double getDistance() {
         return distance;
+    }
+
+    public static Distance of (double distance) {
+        return new Distance(distance);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Distance distance1 = (Distance) o;
+        return Double.compare(distance1.distance, distance) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(distance);
     }
 }
