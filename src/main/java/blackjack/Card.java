187cf3b5ce0e4dda9b4b5fb38577bfb1e8448538
koeonyack <covenant.ko@kakao.com>
2021-02-09 17:46:27 +0900

    feat: Card 구현체 추가

---
+++ src/main/java/blackjack/Card.java
@@ -0,0 +1,25 @@
+package blackjack;
+
+public class Card {
+    private CardType cardType;
+    private CardValue cardValue;
+    private String name;
+
+    public Card(CardType type, CardValue value) {
+        this.cardType = type;
+        this.cardValue = value;
+        this.name = parseCard(cardType, cardValue);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getScore(){
+        return cardValue.getScore();
+    }
+
+    private String parseCard(CardType cardType, CardValue cardValue) {
+        return cardValue.getTag() + cardType.getType();
+    }
+}
