8353443f7e3f3db7576a0a74e8073007eaa72376
d36choi <yuio1126@gmail.com>
2021-02-09 16:54:41 +0900

    feat: InputView class 에 getDistance() 를 추가함.

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,16 @@
+package view;
+
+import oilInjection.model.Car;
+import oilInjection.model.Distance;
+
+import java.util.Scanner;
+
+public class InputView {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static Distance getDistance() {
+        String distance = scanner.nextLine();
+        return Distance.of(Double.parseDouble(distance));
+    }
+}
