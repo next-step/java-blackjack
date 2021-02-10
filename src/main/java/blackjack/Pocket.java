c9874fe42b40eabacc99bcc1dfe4ed2baff2c54b
koeonyack <covenant.ko@kakao.com>
2021-02-10 17:23:01 +0900

    feat: 빈 포켓 에러처리

--- src/main/java/blackjack/Pocket.java
+++ src/main/java/blackjack/Pocket.java
@@ -16,6 +16,9 @@ public class Pocket {
     }
 
     public String getCardsName() {
+        if(cards.size() == 0){
+            throw new IllegalStateException("포켓에 카드가 없는 상태입니다.");
+        }
         return cards.stream()
                 .map(Card::getName)
                 .collect(Collectors.joining(", "));
--- src/test/java/blackjack/PocketTest.java
+++ src/test/java/blackjack/PocketTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class PocketTest {
     @Test
@@ -13,5 +14,12 @@ class PocketTest {
         pocket.add(new Card(CardType.CLOVER, CardValue.ONE));
         final String cardsName = pocket.getCardsName();
         assertThat(cardsName).isEqualTo("1클로버");
+    }
+
+    @Test
+    @DisplayName("빈 포켓의 카드의 이름을 출력하면 오류가 나는 테스트")
+    void getCardsNameErrorTest() {
+        final Pocket pocket = new Pocket();
+        assertThrows(IllegalStateException.class, pocket::getCardsName);
     }
 }
