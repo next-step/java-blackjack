57ceba60f4be9e08ae0567eab50252131afc6544
quadam36 <quadam36@gmail.com>
2021-02-10 17:00:43 +0900

    feat: add name property to Denomination and Suit

--- src/main/java/blackjack/domain/Denomination.java
+++ src/main/java/blackjack/domain/Denomination.java
@@ -3,21 +3,22 @@ package blackjack.domain;
 import java.util.Arrays;
 
 public enum Denomination {
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+    ACE(1, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    JACK(10, "J"),
+    QUEEN(10, "Q"),
+    KING(10, "K");
 
     private final int score;
+    private final String name;
 
     public static Denomination of(final int score) {
         return Arrays.stream(
@@ -29,8 +30,9 @@ public enum Denomination {
         );
     }
 
-    Denomination(final int score) {
+    Denomination(final int score, final String name) {
         this.score = score;
+        this.name = name;
     }
 
     public boolean isAce() {
@@ -40,4 +42,6 @@ public enum Denomination {
     public int getScore() {
         return score;
     }
+
+    public String getName() { return name; }
 }
--- src/main/java/blackjack/domain/Suit.java
+++ src/main/java/blackjack/domain/Suit.java
@@ -1,5 +1,18 @@
 package blackjack.domain;
 
 public enum Suit {
-    CLUBS, DIAMONDS, HEARTS, SPADES
+    CLUBS("클로버"), 
+    DIAMONDS("다이아몬드"),
+    HEARTS("하트"),
+    SPADES("스페이드");
+
+    private final String name;
+
+    Suit(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
