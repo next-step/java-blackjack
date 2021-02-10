15f9901f7234888f8e6a8df38b6bc0da884b5dec
KimBB <tree9295@gmail.com>
2021-02-10 18:20:41 +0900

    feat: StringUtils 유틸리티 클래스 추가

---
+++ src/main/java/blackjack/utils/StringUtils.java
@@ -0,0 +1,7 @@
+package blackjack.utils;
+
+public class StringUtils {
+    public static final String COLON = " : ";
+    public static final String NEW_LINE = "\n";
+    public static final String SPACE = " ";
+}
