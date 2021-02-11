b9f64656204d6cc0002786cf48cb8bf5eefddad0
koeonyack <covenant.ko@kakao.com>
2021-02-11 22:49:09 +0900

    feat: 카드 뽑는지 묻는 메시지

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,5 +1,6 @@
 package blackjack.view;
 
+import blackjack.controller.AnswerRunnable;
 import blackjack.model.Players;
 
 import java.util.Scanner;
@@ -10,5 +11,13 @@ public class InputView {
         System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
 
         return new Players(scanner.nextLine());
+    }
+
+    public boolean isPopCard(final String name){
+        final Scanner scanner = new Scanner(System.in);
+        final String answerText = String.format("%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)", name);
+        System.out.println(answerText);
+
+        return scanner.nextBoolean();
     }
 }
