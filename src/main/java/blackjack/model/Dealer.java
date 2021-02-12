4dabe4fcab52b4304d1e95d6a87b5c6adbd7edb2
koeonyack <covenant.ko@kakao.com>
2021-02-12 21:59:03 +0900

    feat(Dealer): Card 점수 계산

--- src/main/java/blackjack/model/Dealer.java
+++ src/main/java/blackjack/model/Dealer.java
@@ -1,9 +1,12 @@
 package blackjack.model;
 
+import blackjack.model.Job;
+
 public class Dealer implements Player {
     private final Pocket pocket = new Pocket();
     private final Deck deck;
     private final String name = "Dealer";
+    private final Job job = Job.DEALER;
 
     public Dealer(final Deck deck) {
         this.deck = deck;
@@ -15,6 +18,7 @@ public class Dealer implements Player {
         pocket.add(card);
     }
 
+    //TODO : 밖에서 안 쓰이면, 지워도 된다.
     @Override
     public Pocket getCardStats() {
         return pocket;
@@ -26,7 +30,34 @@ public class Dealer implements Player {
     }
 
     @Override
+    public Job getJob() {
+        return job;
+    }
+
+    @Override
     public String exportCardStats() {
         return getName() + " : " + pocket.getCardsName();
+    }
+
+    @Override
+    public int getCardsScore() {
+        int totalScore = 0;
+
+        for(final Card card : pocket.getCards()){
+            totalScore += card.getScore();
+        }
+
+        if(hasAce() && totalScore < 11){
+            totalScore += 10;
+        }
+
+        return totalScore;
+    }
+
+    @Override
+    public boolean hasAce(){
+        return pocket.getCards()
+                .stream()
+                .anyMatch(card-> card.getValue()==CardValue.ONE);
     }
 }
