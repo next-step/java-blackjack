fd0a8c3577b5ef25537d771bddb28f6b9076a6ab
koeonyack <covenant.ko@kakao.com>
2021-02-13 22:54:35 +0900

    refactor: OutputView

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,17 +1,7 @@
 package blackjack.view;
 
-import blackjack.model.Players;
-
 public class OutputView {
-
-    public void initialPopCard(final Players players) {
-        final String gamer = players.getPlayerNames(player -> !player.getName().equals("Dealer"));
-        final String dealer = players.getPlayerNames(player -> player.getName().equals("Dealer"));
-        final String printResult = String.format("%s와 %s에게 2장을 나누었습니다.", dealer, gamer);
-        System.out.println(printResult);
-    }
-
-    public void winningBoard(final Exporter exporter){
-        System.out.println(exporter.getResult());
+    public void printResult(final String result) {
+        System.out.println(result);
     }
 }
