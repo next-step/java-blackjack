c48cad6819a19683057f3431d17f3f517afd19e6
koeonyack <covenant.ko@kakao.com>
2021-02-13 22:58:32 +0900

    feat: 블랙젝 게임 시나리오 메소드 추가

--- src/main/java/blackjack/controller/BlackjackController.java
+++ src/main/java/blackjack/controller/BlackjackController.java
@@ -2,15 +2,31 @@ package blackjack.controller;
 
 import blackjack.model.Players;
 import blackjack.view.InputView;
+import blackjack.view.OutputView;
 
 public class BlackjackController {
     final private InputView inputView;
+    final private OutputView outputView;
 
-    public BlackjackController(final InputView inputView) {
+    public BlackjackController(final InputView inputView, final OutputView outputView) {
         this.inputView = inputView;
+        this.outputView = outputView;
     }
 
     public Players inputGamerNames() {
         return inputView.inputGamerNames();
+    }
+
+    public void play(Players players) {
+        final ScenarioController scenarioController = new ScenarioController(players);
+
+        final String prepareGameResult = scenarioController.prepareGame();
+        outputView.printResult(prepareGameResult);
+
+        final String startGameResult = scenarioController.startGame(inputView);
+        outputView.printResult(startGameResult);
+
+        final String endGameResult = scenarioController.endGame();
+        outputView.printResult(endGameResult);
     }
 }
