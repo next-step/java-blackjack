e54a1ae79f60bbb34f1e3447feb6db15caee00d2
koeonyack <covenant.ko@kakao.com>
2021-02-13 22:54:21 +0900

    feat(ScenarioController): 시나리오 컨트롤러 수정
    - exporter를 통한 시나리오 결과 관리

--- src/main/java/blackjack/controller/ScenarioController.java
+++ src/main/java/blackjack/controller/ScenarioController.java
@@ -1,50 +1,53 @@
 package blackjack.controller;
 
-import blackjack.model.*;
-import blackjack.view.Exporter;
+import blackjack.model.Exporter;
+import blackjack.model.Job;
+import blackjack.model.Player;
+import blackjack.model.Players;
 import blackjack.view.InputView;
-import blackjack.view.OutputView;
 
 import java.util.List;
 
-public class Scenario {
-
-    private final Players players;
+public class ScenarioController {
     private final Player DEALER;
     private final List<Player> GAMERS;
+    private final Exporter exporter;
 
-    public Scenario(final Players players) {
-        this.players = players;
+    public ScenarioController(final Players players) {
         DEALER = players.getPlayers(player -> player.getJob() == Job.DEALER).get(0);
         GAMERS = players.getPlayers(player -> player.getJob() == Job.GAMER);
+        exporter = new Exporter(players);
     }
 
     /**
      * @Description. 게임 시작 시 게임 참가자에게 카드를 나눠준다.
      * 딜러 1장, 게이머 2장의 카드를 받는다.
      */
-    public void prepareGame() {
+    public String prepareGame() {
         DEALER.receiveCard();
         GAMERS.forEach(Player::receiveCard);
         GAMERS.forEach(Player::receiveCard);
+
+        return exporter.initialCardDistribution();
     }
 
     /**
      * @Description. 게이머가 카드를 받는지 물어본다.
      */
-    public void startGame(final InputView inputView) {
+    public String startGame(final InputView inputView) {
         GAMERS.forEach(player -> isPopCard(inputView, player));
-        if(DEALER.getCardsScore() < 16){
+        if (DEALER.getCardsScore() < 16) {
             DEALER.receiveCard();
         }
+
+        return exporter.getPlayersCardsStatusWithScore();
     }
 
     /**
      * @Description. 게임 결과를 본다.
      */
-    public void endGame(final OutputView outputView) {
-        final Exporter exporter = new Exporter(players);
-        outputView.winningBoard(exporter);
+    public String endGame() {
+        return exporter.getResult();
     }
 
     private void isPopCard(InputView inputView, Player player) {
