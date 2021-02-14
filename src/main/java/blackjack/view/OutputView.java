94ab4f578259e7db6c3481612d689e696eb3cd5c
koeonyack <covenant.ko@kakao.com>
2021-02-14 15:02:19 +0900

    feat(OutputView): 게임 결과 출력

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -4,4 +4,10 @@ public class OutputView {
     public void printResult(final String result) {
         System.out.println(result);
     }
+
+    public void gameScore(final String result) {
+        System.out.println();
+        System.out.println("## 최종 승패");
+        System.out.println(result);
+    }
 }
