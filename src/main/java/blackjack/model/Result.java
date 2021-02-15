003380e1a436cc7233227623a7d5b887cceec834
koeonyack <covenant.ko@kakao.com>
2021-02-15 16:53:20 +0900

    feat: Result 구현체 추가

---
+++ src/main/java/blackjack/model/Result.java
@@ -0,0 +1,15 @@
+package blackjack.model;
+
+public enum Result {
+    BLACKJACK("승"), WIN("승"), BUST("패"), LOSE("패"), PUSH("무"), DEFAULT("_");
+
+    private String status;
+
+    Result(String status) {
+        this.status = status;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+}
