ce98f604abb3172e382ba7d8467ac6e8148be340
KimBB <tree9295@gmail.com>
2021-02-15 16:35:29 +0900

    refactor: StringUtils 승패 메시지 추가

--- src/main/java/blackjack/utils/StringUtils.java
+++ src/main/java/blackjack/utils/StringUtils.java
@@ -3,4 +3,7 @@ package blackjack.utils;
 public class StringUtils {
     public static final String COLON = " : ";
     public static final String SPACE = " ";
+    public static final String WIN = "승";
+    public static final String LOSE = "패";
+
 }
