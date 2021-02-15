4e283405eafda4e6011c6b0e0cdfea5da2403cd4
d36choi <yuio1126@gmail.com>
2021-02-15 15:00:12 +0900

    fix: NEW_LINE 매크로를 제거함.

--- src/main/java/blackjack/utils/StringUtils.java
+++ src/main/java/blackjack/utils/StringUtils.java
@@ -2,6 +2,5 @@ package blackjack.utils;
 
 public class StringUtils {
     public static final String COLON = " : ";
-    public static final String NEW_LINE = "\n";
     public static final String SPACE = " ";
 }
