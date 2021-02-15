78f601fd803f56fe18c01cc27720b0c4236d57b5
Sunghwan-DS <jeonsung02@gmail.com>
2021-02-15 13:48:01 +0900

    feat: Add letter
    - 출력에 쓰이기 위한 카드 수 혹은 기호를 추가

--- src/main/java/blackjack/domain/Denomination.java
+++ src/main/java/blackjack/domain/Denomination.java
@@ -1,28 +1,32 @@
 package blackjack.domain;
 
 public enum Denomination {
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10),
+    ACE(1, "A"),
+    TWO(2, "2"),
+    THREE(3, "3"),
+    FOUR(4, "4"),
+    FIVE(5, "5"),
+    SIX(6, "6"),
+    SEVEN(7, "7"),
+    EIGHT(8, "8"),
+    NINE(9, "9"),
+    TEN(10, "10"),
+    JACK(10, "J"),
+    QUEEN(10, "Q"),
+    KING(10, "K"),
     ;
 
     private final int number;
+    private final String letter;
 
-    Denomination(final int number) {
+    Denomination(final int number, final String letter) {
         this.number = number;
+        this.letter = letter;
     }
 
     public int getNumber() {
         return number;
     }
+
+    public String getLetter() { return letter; }
 }
