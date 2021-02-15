5e78fc5ab66d6866c60192600bfb5fb1bcfadefe
d36choi <yuio1126@gmail.com>
2021-02-15 14:59:07 +0900

    feat: outputPlayerScore, outputDealersDraw 메서드를 추가함.
    - OutputView 에서 딜러의 드로우 현황과 플레이어의 점수를 출력하는 함수를 구현했다.

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -4,7 +4,18 @@ import blackjack.model.Card;
 import blackjack.model.Player;
 
 public class OutputView {
+
+    private static final String DEALERS_DRAW_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    private static final String PLAYER_SCORE_FORMAT = " - 결과 : %d\n";
     public static void outputPlayerStatus(Player player){
         System.out.println(player.toString());
+    }
+
+    public static void outputDealersDraw() {
+        System.out.println(DEALERS_DRAW_MESSAGE);
+    }
+    public static void outputPlayerScore(Player player){
+        System.out.print(player.toString());
+        System.out.printf(PLAYER_SCORE_FORMAT,player.getCardHandScore());
     }
 }
