5773a7cdfa1791216990a0cb64cb556310b86fcc
KimBB <tree9295@gmail.com>
2021-02-15 16:37:59 +0900

    feat: Gamers class 생성
    - Dealer/User wrapping class

---
+++ src/main/java/blackjack/model/Gamers.java
@@ -0,0 +1,59 @@
+package blackjack.model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Gamers {
+    private static final int FIRST_DRAW_CARD_COUNT = 2;
+    private final Dealer dealer;
+    private final List<User> users;
+
+
+    public Gamers() {
+        dealer = new Dealer("dealer");
+        users = new ArrayList<>();
+    }
+
+    public Dealer getDealer() {
+        return dealer;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public List<Player> getPlayers(){
+        List<Player> players = new ArrayList<>();
+        players.add(dealer);
+        players.addAll(users);
+        return players;
+    }
+
+    public void addUsers(String[] usersName){
+        Arrays.stream(usersName).forEach(name -> users.add(User.of(name)));
+    }
+
+    public void initGamerCardHand(BlackJackCard blackJackCard){
+        drawCardAsCount(dealer, blackJackCard);
+        users.forEach(user -> drawCardAsCount(user, blackJackCard));
+    }
+
+    private void drawCardAsCount(Player player, BlackJackCard blackJackCard) {
+        for (int i = 0; i < FIRST_DRAW_CARD_COUNT; ++i){
+            player.drawCard(blackJackCard.pickOneCard());
+        }
+    }
+
+    public void calculateResult(){
+        for (User user : users) {
+            if(dealer.getCardHandScore() >= user.getCardHandScore()){
+                dealer.setWinningCount(dealer.getWinningCount() + 1);
+                user.setWin(false);
+                continue;
+            }
+            dealer.setLosingCount(dealer.getLosingCount() + 1);
+            user.setWin(true);
+        }
+    }
+}
