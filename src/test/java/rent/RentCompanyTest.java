4b30322144447773cb8378f50e01627b0944fb5b
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 10:20:58 +0900

    style: removed comment

--- src/test/java/rent/RentCompanyTest.java
+++ src/test/java/rent/RentCompanyTest.java
@@ -4,12 +4,12 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class RentCompanyTest {
+class RentCompanyTest {
     private static final String NEWLINE = System.getProperty("line.separator");
 
     @Test
-    public void report() {
-        RentCompany company = RentCompany.create(); // factory method를 사용해 생성
+    void report() {
+        RentCompany company = RentCompany.create();
         company.addCar(new Sonata(150));
         company.addCar(new K5(260));
         company.addCar(new Sonata(120));
@@ -18,11 +18,11 @@ public class RentCompanyTest {
 
         String report = company.generateReport();
         assertThat(report).isEqualTo(
-            "Sonata : 15리터" + NEWLINE +
-                "K5 : 20리터" + NEWLINE +
-                "Sonata : 12리터" + NEWLINE +
-                "Avante : 20리터" + NEWLINE +
-                "K5 : 30리터" + NEWLINE
+                "Sonata : 15리터" + NEWLINE +
+                        "K5 : 20리터" + NEWLINE +
+                        "Sonata : 12리터" + NEWLINE +
+                        "Avante : 20리터" + NEWLINE +
+                        "K5 : 30리터" + NEWLINE
         );
     }
 }
