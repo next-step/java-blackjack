a4f97a5e3a2d59d14ba8e0d09d4448a9cbcc0fbd
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 10:23:01 +0900

    feat: add ScoreBoard dto class

--- src/main/java/blackjack/dto/ScoreBoardInfo.java
+++ src/main/java/blackjack/dto/ScoreBoardInfo.java
@@ -1,2 +1,26 @@
-package blackjack.dto;public class ScoreBoardInfo {
+package blackjack.dto;
+
+public class ScoreBoardInfo {
+
+    private final Integer winCount;
+    private final Integer drawCount;
+    private final Integer loseCount;
+
+    public ScoreBoardInfo(Integer winCount, Integer drawCount, Integer loseCount) {
+        this.winCount = winCount;
+        this.drawCount = drawCount;
+        this.loseCount = loseCount;
+    }
+
+    public Integer getWinCount() {
+        return winCount;
+    }
+
+    public Integer getDrawCount() {
+        return drawCount;
+    }
+
+    public Integer getLoseCount() {
+        return loseCount;
+    }
 }
