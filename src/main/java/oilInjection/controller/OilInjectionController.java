cdc8bf30dd2b54422f35034452ded22565b50813
d36choi <yuio1126@gmail.com>
2021-02-16 10:37:15 +0900

    fix: OilInjectionController 의 print 코드를 View 로 이전

--- src/main/java/oilInjection/controller/OilInjectionController.java
+++ src/main/java/oilInjection/controller/OilInjectionController.java
@@ -9,9 +9,9 @@ public class OilInjectionController {
 
 
     public void getDistanceForEachCar(RentCompany rentCompany) {
-        System.out.println("차 별 원하는 거리를 입력하세요.");
+       InputView.printGetDistanceMessage();
        for (Car car : rentCompany.getCarList()) {
-           System.out.println(car.getName());
+           OutputView.printCarName(car.getName());
            car.registerTripDistance(InputView.getDistance());
            car.injectOil();
        }
--- src/main/java/oilInjection/view/OutputView.java
+++ src/main/java/oilInjection/view/OutputView.java
@@ -6,4 +6,8 @@ public class OutputView {
     public static void printCarInfo(String report){
         System.out.println(report);
     }
+
+    public static void printCarName(String carName) {
+        System.out.println(carName);
+    }
 }
