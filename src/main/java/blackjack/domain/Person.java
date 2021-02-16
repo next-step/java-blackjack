8306b517aab19b012266a754447f2ba2406b94fd
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 10:28:30 +0900

    feat: add initalize card method

--- src/main/java/blackjack/domain/Person.java
+++ src/main/java/blackjack/domain/Person.java
@@ -4,31 +4,50 @@ import blackjack.dto.NameInfo;
 import blackjack.dto.PersonInfo;
 
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 public abstract class Person {
+
+    private static final int END_EXCLUSIVE = 2;
+
     protected final CardBunch cardBunch;
     protected final String name;
 
-    public Person(String name) {
+    protected Person(String name) {
         this.name = name;
         this.cardBunch = new CardBunch();
     }
 
-    public Person(String name, CardBunch cardBunch) {
+    protected Person(String name, CardBunch cardBunch) {
         this.name = name;
         this.cardBunch = cardBunch;
     }
 
     public PersonInfo getPersonInfo() {
         return new PersonInfo(
-            new NameInfo(name),
-            cardBunch.getCardBunchInfo()
+                new NameInfo(name),
+                cardBunch.getCardBunchInfo(),
+                getScore()
         );
+    }
+
+    public void initialCard(Deck deck) {
+        IntStream.range(0, END_EXCLUSIVE).forEach(n -> drawCard(deck));
+    }
+
+    public void drawCard(Deck deck) {
+        cardBunch.drawCard(deck);
     }
 
     public NameInfo getNameInfo() {
         return new NameInfo(name);
     }
+
+    public Integer getScore() {
+        return cardBunch.calcScore();
+    }
+
+    abstract public boolean canDraw();
 
     @Override
     public boolean equals(Object o) {
