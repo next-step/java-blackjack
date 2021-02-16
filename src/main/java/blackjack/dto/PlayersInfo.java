d4d8945a39d1eec60e6251b831d3d0926267c849
Changhwan Lee <chlee1252@gmail.com>
2021-02-16 10:22:41 +0900

    feat: add Players dto class

--- src/main/java/blackjack/dto/PlayersInfo.java
+++ src/main/java/blackjack/dto/PlayersInfo.java
@@ -1,2 +1,34 @@
-package blackjack.dto;public class PlayersInfo {
+package blackjack.dto;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class PlayersInfo {
+    private final List<PersonInfo> playersInfo;
+
+    public PlayersInfo(List<PersonInfo> playersInfo) {
+        this.playersInfo = playersInfo;
+    }
+
+    public List<PersonInfo> getPeopleInfo() {
+        return playersInfo;
+    }
+
+    public List<NameInfo> getNameInfo() {
+        return playersInfo.stream().map(PersonInfo::getNameInfo).collect(Collectors.toList());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlayersInfo that = (PlayersInfo) o;
+        return Objects.equals(playersInfo, that.playersInfo);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playersInfo);
+    }
 }
