a17e7dd3bee030076025e53e4f66b56d18ae3a23
d36choi <yuio1126@gmail.com>
2021-02-17 16:51:51 +0900

    feat: Add Fixtures.java for testing with card objects

---
+++ src/test/java/blackjack/model/Fixtures.java
@@ -0,0 +1,11 @@
+package blackjack.model;
+
+public class Fixtures {
+
+    public static Card HEART_JACK_CARD = Card.of(Type.HEART, Symbol.JACK);
+    public static Card HEART_TEN_CARD = Card.of(Type.HEART, Symbol.TEN);
+    public static Card HEART_NINE_CARD = Card.of(Type.HEART, Symbol.NINE);
+    public static Card HEART_EIGHT_CARD = Card.of(Type.HEART, Symbol.EIGHT);
+    public static Card HEART_TWO_CARD = Card.of(Type.HEART, Symbol.TWO);
+    public static Card HEART_ACE_CARD = Card.of(Type.HEART, Symbol.ACE);
+}
