581a9059dcf8095f721fc4b15b637fc3a996ac80
d36choi <yuio1126@gmail.com>
2021-02-18 18:35:36 +0900

    feat: 수익관련 메서드를 Money 에 추가함

--- src/main/java/blackjack/model/Money.java
+++ src/main/java/blackjack/model/Money.java
@@ -3,6 +3,7 @@ package blackjack.model;
 public class Money {
 
     private final int MONEY_LOW_BOUND = 0;
+    private final double EARNING_RATE = 1.5;
     private int money;
 
     private Money(int money) {
@@ -19,6 +20,22 @@ public class Money {
 
     public int getMoney() {
         return money;
+    }
+
+    public void setRevenueToMoney() {
+        money = (int) (money * EARNING_RATE);
+    }
+
+    public void subtractAsRevenue(int money) {
+        this.money -= money * (EARNING_RATE - 1);
+    }
+
+    public void addMoney (int money) {
+        this.money += money;
+    }
+
+    public void subtractMoney () {
+        money = -money;
     }
 
     public static Money of(int money) {
--- src/test/java/blackjack/model/MoneyTest.java
+++ src/test/java/blackjack/model/MoneyTest.java
@@ -26,4 +26,26 @@ class MoneyTest {
             Money money = Money.of(wrongMoneyAmount);
         });
     }
+    @DisplayName("Money 오브젝트 생성")
+    @Test
+    void loseMoney() {
+        int initMoney = 5000;
+        int betMoney = 2000;
+        int expectedMoney = (int) (initMoney - 0.5 * betMoney);
+        Money money = Money.of(initMoney);
+        money.subtractAsRevenue(betMoney);
+        assertThat(money.getMoney()).isEqualTo(expectedMoney);
+    }
+
+    @DisplayName("Money 오브젝트 생성")
+    @Test
+    void addRevenue() {
+        int betMoney = 2000;
+        int expectedMoney = (int) (1.5 * betMoney);
+        Money money = Money.of(betMoney);
+        money.setRevenueToMoney();
+        assertThat(money.getMoney()).isEqualTo(expectedMoney);
+    }
+
+
 }
