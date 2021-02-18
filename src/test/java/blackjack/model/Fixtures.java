c4fdef19031eba062a87a54b637753a65512f97f
d36choi <yuio1126@gmail.com>
2021-02-18 09:53:59 +0900

    feat: Add all symbol card in Fixtures

--- src/test/java/blackjack/model/Fixtures.java
+++ src/test/java/blackjack/model/Fixtures.java
@@ -6,6 +6,12 @@ public class Fixtures {
     public static Card HEART_TEN_CARD = Card.of(Type.HEART, Symbol.TEN);
     public static Card HEART_NINE_CARD = Card.of(Type.HEART, Symbol.NINE);
     public static Card HEART_EIGHT_CARD = Card.of(Type.HEART, Symbol.EIGHT);
+    public static Card HEART_SEVEN_CARD = Card.of(Type.HEART, Symbol.SEVEN);
+    public static Card HEART_SIX_CARD = Card.of(Type.HEART, Symbol.SIX);
+    public static Card HEART_FIVE_CARD = Card.of(Type.HEART, Symbol.FIVE);
+    public static Card HEART_FOUR_CARD = Card.of(Type.HEART, Symbol.FOUR);
+    public static Card HEART_THREE_CARD = Card.of(Type.HEART, Symbol.THREE);
     public static Card HEART_TWO_CARD = Card.of(Type.HEART, Symbol.TWO);
     public static Card HEART_ACE_CARD = Card.of(Type.HEART, Symbol.ACE);
+    public static Money TEST_MONEY = Money.of(1000);
 }
