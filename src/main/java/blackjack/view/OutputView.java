ff5c52a0f225b1b6f4a8cf60dcc693f0e20d3d04
d36choi <yuio1126@gmail.com>
2021-02-18 17:36:32 +0900

    feat: 최종수익 출력 메서드를 추가함

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -6,24 +6,31 @@ import java.util.List;
 
 public class OutputView {
 
-    private static final String DEALERS_DRAW_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    private static final String DEALERS_DRAW_MESSAGE = "\n딜러는 16이하라 한장의 카드를 더 받았습니다.";
     private static final String PLAYER_SCORE_FORMAT = " - 결과 : %d\n";
-    private static final String FINAL_RESULT_MESSAGE = "\n## 최종승패";
-    public static void outputPlayerStatus(Player player){
+    private static final String FINAL_RESULT_MESSAGE = "\n## 최종 승패";
+    private static final String FINAL_REVENUE_MESSAGE = "\n## 최종 수익";
+    private static final String FINAL_REVENUE_FOR_EACH_PLAYER_MESSAGE = "%s: %d\n";
+    public void outputPlayerStatus(Player player){
         System.out.println(player.toString());
     }
 
-    public static void outputDealersDraw() {
+    public void outputDealersDraw() {
         System.out.println(DEALERS_DRAW_MESSAGE);
     }
-    public static void outputPlayerScore(Player player){
+    public void outputPlayerScore(Player player){
         //TODO: outputPlayerScore 에서는 딜러 또한 모든 카드를 출력해야 함.
         System.out.print(player.toString());
         System.out.printf(PLAYER_SCORE_FORMAT,player.getCardHandScore());
     }
 
-    public static void outputFinalResult(List<Player> players) {
+    public void outputFinalResult(List<Player> players) {
         System.out.println(FINAL_RESULT_MESSAGE);
         players.forEach(player -> System.out.println(player.getResult()));
+    }
+
+    public void outputFinalRevenue(List<Player> players) {
+        System.out.println(FINAL_REVENUE_MESSAGE);
+        players.forEach(player -> System.out.printf(FINAL_REVENUE_FOR_EACH_PLAYER_MESSAGE,player.getName(),player.getMoney().getMoney()));
     }
 }
