cf600bc200118e95e0c16534dd855e1ea3f9f6a0
d36choi <yuio1126@gmail.com>
2021-02-19 11:04:14 +0900

    test: PlayerTest 를 추가함

---
+++ src/test/java/blackjack/model/PlayerTest.java
@@ -0,0 +1,43 @@
+package blackjack.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class PlayerTest {
+
+    Player player;
+
+    @BeforeEach
+    void setUp() {
+        player = User.of("test");
+    }
+
+    @Test
+    void drawCard() {
+        player.drawCard(Fixtures.HEART_TEN_CARD);
+        assertThat(player.getCardHand().getCards().get(0)).isEqualTo(Fixtures.HEART_TEN_CARD);
+    }
+
+    @Test
+    void getCardHandScore() {
+        player.drawCard(Fixtures.HEART_TEN_CARD);
+        assertThat(player.getCardHandScore()).isEqualTo(Fixtures.HEART_TEN_CARD.getSymbol().getScore());
+
+    }
+
+    @Test
+    void getCardBundleSize() {
+        player.drawCard(Fixtures.HEART_TEN_CARD);
+        assertThat(player.getCardBundleSize()).isEqualTo(1);
+    }
+
+    @Test
+    void getResult() {
+
+        player.drawCard(Fixtures.HEART_TEN_CARD);
+        assertThat(player.getResult()).isEqualTo(player.getName() + ": "+"패");
+    }
+}
