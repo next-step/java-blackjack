1883abc605855de0c075bf0657c76aeaef922b02
hanseokhyeon <sah0322@naver.com>
2021-02-21 00:31:50 +0900

    refactor: 길이가 긴 메서드 리팩터링

--- src/main/java/blackjack/domain/FinalResult.java
+++ src/main/java/blackjack/domain/FinalResult.java
@@ -13,23 +13,49 @@ public class FinalResult {
 
     public int getFinalResult(Dealer dealer, Gamer gamer) {
         int dealerScore = dealer.getScore().getValue();
-        int gamerScore = gamer.getScore().getValue();
         int gamerMoney = gamer.getMoney();
         if (dealerScore > THRESHOLD) {
             moneyOfDealer += gamerMoney;
             return gamerMoney * MINUS;
         }
+        return checkGamerBlackJack(dealer, gamer);
+    }
+
+    private int checkGamerBlackJack(Dealer dealer, Gamer gamer) {
+        int dealerScore = dealer.getScore().getValue();
+        int gamerScore = gamer.getScore().getValue();
+        int gamerMoney = gamer.getMoney();
         if (gamerScore == THRESHOLD && dealerScore < THRESHOLD) {
             moneyOfDealer += (int) (gamerMoney * BLACKJACK_RATE);
             return (int) (gamerMoney * MINUS * BLACKJACK_RATE);
         }
+        return checkDrawBlackJack(dealer, gamer);
+    }
+
+    private int checkDrawBlackJack(Dealer dealer, Gamer gamer) {
+        int dealerScore = dealer.getScore().getValue();
+        int gamerScore = gamer.getScore().getValue();
         if (gamerScore == THRESHOLD && dealerScore == THRESHOLD) {
             return 0;
         }
+        return checkGamerWin(dealer, gamer);
+    }
+
+    private int checkGamerWin(Dealer dealer, Gamer gamer) {
+        int dealerScore = dealer.getScore().getValue();
+        int gamerScore = gamer.getScore().getValue();
+        int gamerMoney = gamer.getMoney();
         if (gamerScore > dealerScore) {
             moneyOfDealer -= gamerMoney;
             return gamerMoney;
         }
+        return checkDraw(dealer, gamer);
+    }
+
+    private int checkDraw(Dealer dealer, Gamer gamer) {
+        int dealerScore = dealer.getScore().getValue();
+        int gamerScore = gamer.getScore().getValue();
+        int gamerMoney = gamer.getMoney();
         if (gamerScore == dealerScore) {
             return 0;
         }
