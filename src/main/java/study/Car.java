14c8e2ec2ec4d5bd17b50baac329f9f68939dab2
h1un <h1unha@naver.com>
2021-05-31 15:22:40 +0900

    feat : study

---
+++ src/main/java/study/Car.java
@@ -0,0 +1,24 @@
+package study;
+
+public class Car {
+    //불변 객체
+    private final String name;
+    private final int position;
+
+    public Car(final String name , final int position){
+        this.name = name;
+        this.position = position;
+    }
+
+    public Car  move(final MoveStrategy moveStrategy){
+        if(moveStrategy.isMovable()){
+           return new Car(name, position +1);
+        }
+        return this;
+    }
+
+
+    public int getPosition() {
+        return position;
+    }
+}
