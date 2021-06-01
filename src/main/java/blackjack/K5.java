dc51360c48f30ad608bb635a26d9adaea78a2b6c
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-06-01 15:00:39 +0900

    (리팩터링) K5 클래스 생성자 구현 및 이름 수정

--- src/main/java/blackjack/K5.java
+++ src/main/java/blackjack/K5.java
@@ -2,15 +2,19 @@ package blackjack;
 
 public class K5 extends Car{
     private static final double DISTANCE_PER_LITER = 13;
-    private static final String NAME = "k5";
+    private static final String NAME = "K5";
     private double tripDistance;
+
+    public K5(double tripDistance) {
+        this.tripDistance = tripDistance;
+    }
 
     public double getDistancePerLiter() {
         return DISTANCE_PER_LITER;
     }
 
     public double getTripDistance() {
-        return tripDistance;
+        return (int) tripDistance;
     }
 
     public String getName() {
