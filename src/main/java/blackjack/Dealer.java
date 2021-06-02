b902de5f3c6d492bc357eb83b20963cc69c6f407
Miot2J <ck96117@gmail.com>
2021-06-02 19:31:50 +0900

    refactor : Dealer 와 Player를 Player 인터페이스를 구현하는 Dealer 와 User로 변경

--- src/main/java/blackjack/Dealer.java
+++ src/main/java/blackjack/Dealer.java
@@ -2,21 +2,24 @@ package blackjack;
 
 import java.util.List;
 
-public class Dealer {
+public class Dealer implements Player {
     private final BunchOfCard bunchOfCard;
 
     public Dealer() {
         this.bunchOfCard = new BunchOfCard();
     }
 
+    @Override
     public void addCard(Card card) {
         this.bunchOfCard.addCard(card);
     }
 
+    @Override
     public int getCardValueSum() {
         return this.bunchOfCard.getCardValueSum();
     }
 
+    @Override
     public List<String> getCardNames() {
         return bunchOfCard.getCardNames();
     }
--- src/test/java/blackjack/UserTest.java
+++ src/test/java/blackjack/UserTest.java
@@ -7,47 +7,47 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
-public class PlayerTest {
+public class UserTest {
 
     @DisplayName("게임 시작시 이름들을 입력하면 유저들이 생성 된다.")
     @Test
     void playerGenerateTest() {
-        assertDoesNotThrow(() -> new Player("현진"));
+        assertDoesNotThrow(() -> new User("현진"));
     }
 
     @DisplayName("게임 시작시 이름이 없이는 유저를 생성할 수 없다.")
     @Test
     void playerGenerateFailTest() {
-        assertThrows(IllegalArgumentException.class, () -> new Player(" "));
+        assertThrows(IllegalArgumentException.class, () -> new User(" "));
     }
 
     @DisplayName("유저는 이름을 반환할 수 있다.")
     @Test
     void getNameTest() {
-        Player player = new Player("재언");
+        User user = new User("재언");
 
-        assertThat(player.getName()).isEqualTo("재언");
+        assertThat(user.getName()).isEqualTo("재언");
     }
 
     @DisplayName("유저는 카드의 이름들을 반환 할 수있다.")
     @Test
     void getNamesTest() {
-        Player player = new Player("재언");
+        User user = new User("재언");
 
-        player.addCard(new Card(CardTypes.CLOVER_1));
-        player.addCard(new Card(CardTypes.DIAMOND_J));
+        user.addCard(new Card(CardTypes.CLOVER_1));
+        user.addCard(new Card(CardTypes.DIAMOND_J));
 
-        assertThat(player.getCardNames()).contains("1클로버", "J다이아몬드");
+        assertThat(user.getCardNames()).contains("1클로버", "J다이아몬드");
     }
 
     @DisplayName("유저는 카드들의 값의 합을 반환 할 수있다.")
     @Test
     void getCardValueSumTest() {
-        Player player = new Player("재언");
+        User user = new User("재언");
 
-        player.addCard(new Card(CardTypes.CLOVER_1));
-        player.addCard(new Card(CardTypes.DIAMOND_J));
+        user.addCard(new Card(CardTypes.CLOVER_1));
+        user.addCard(new Card(CardTypes.DIAMOND_J));
 
-        assertThat(player.getCardValueSum()).isEqualTo(11);
+        assertThat(user.getCardValueSum()).isEqualTo(11);
     }
 }
