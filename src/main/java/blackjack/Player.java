884aa8954399e018f2b84588759ff25a96a26bab
Miot2J <ck96117@gmail.com>
2021-06-02 19:32:15 +0900

    feat: 딜러 생성 구현, 딜러는 카드의 이름들과 카드 뭉치의 합을 반환하는 기능 구현

--- src/main/java/blackjack/Player.java
+++ src/main/java/blackjack/Player.java
@@ -2,35 +2,8 @@ package blackjack;
 
 import java.util.List;
 
-public class Player {
-    private final String name;
-    private final BunchOfCard bunchOfCard;
-
-    public Player(String name) {
-        validateNullInput(name);
-        this.name = name;
-        this.bunchOfCard = new BunchOfCard();
-    }
-
-    private void validateNullInput(String name) {
-        if (name.trim().isEmpty()) {
-            throw new IllegalArgumentException("이름을 입력하지 않으면 생성할 수 없습니다.");
-        }
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void addCard(Card card) {
-        this.bunchOfCard.addCard(card);
-    }
-
-    public int getCardValueSum() {
-        return this.bunchOfCard.getCardValueSum();
-    }
-
-    public List<String> getCardNames() {
-        return bunchOfCard.getCardNames();
-    }
+public interface Player {
+    void addCard(Card card);
+    int getCardValueSum();
+    List<String> getCardNames();
 }
---
+++ src/main/java/blackjack/User.java
@@ -0,0 +1,39 @@
+package blackjack;
+
+import java.util.List;
+
+public class User implements Player{
+    private final String name;
+    private final BunchOfCard bunchOfCard;
+
+    public User(String name) {
+        validateNullInput(name);
+        this.name = name;
+        this.bunchOfCard = new BunchOfCard();
+    }
+
+    private void validateNullInput(String name) {
+        if (name.trim().isEmpty()) {
+            throw new IllegalArgumentException("이름을 입력하지 않으면 생성할 수 없습니다.");
+        }
+    }
+
+    @Override
+    public void addCard(Card card) {
+        this.bunchOfCard.addCard(card);
+    }
+
+    @Override
+    public int getCardValueSum() {
+        return this.bunchOfCard.getCardValueSum();
+    }
+
+    @Override
+    public List<String> getCardNames() {
+        return bunchOfCard.getCardNames();
+    }
+
+    public String getName() {
+        return name;
+    }
+}
