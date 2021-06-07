354e68483a4aae345465453c2e1e3161b56ffb5a
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-06-07 16:48:55 +0900

    (feature) playCardGame 구현

--- src/main/java/blackjack/Stadium.java
+++ src/main/java/blackjack/Stadium.java
@@ -1,7 +1,6 @@
 package blackjack;
 
 import java.util.List;
-import java.util.Scanner;
 
 public class Stadium {
     List<Gambler> gamblers;
@@ -10,21 +9,24 @@ public class Stadium {
         this.gamblers = gamblers;
     }
 
-    // 카드 게임 한 번
+    // 카드 게임
     public List<Gambler> playCardGame() {
         for (Gambler gambler : gamblers) {
-            gambler.drawCard();
+            initialSetting(gambler);
+            drawNewCard(gambler);
         }
         return gamblers;
     }
 
-    public boolean selectChoice(String name) {
-        System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
-        Scanner scanner = new Scanner(System.in);
-        if (scanner.nextLine() == "y") {
-            return true;
+    private void initialSetting(Gambler gambler) {
+        for (int i = 0; i < 2; i++) {
+            gambler.drawCard();
         }
-        return false;
     }
 
+    private void drawNewCard(Gambler gambler) {
+        if (gambler.selectChoice(gambler.getName())) {
+            gambler.drawCard();
+        }
+    }
 }
