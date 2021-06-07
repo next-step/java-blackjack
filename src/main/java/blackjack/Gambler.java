8c252fabaaac92726fbcf580bd5ab3ba62e59f80
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-06-07 16:31:45 +0900

    (feature) Gambler 접근제한자 변경, 생성자 추가, getName 구현

--- src/main/java/blackjack/Gambler.java
+++ src/main/java/blackjack/Gambler.java
@@ -3,18 +3,27 @@ package blackjack;
 import java.util.List;
 
 public class Gambler {
-    String name;
-    List<Card> card;
+    private String name;
+    private List<Card> card;
 
     public Gambler() {
-        this("dealer");
+        this("dealer", null);
     }
 
     public Gambler(String name) {
+        this(name, null);
+    }
+
+    public Gambler(String name, List<Card> card) {
         this.name = name;
+        this.card = card;
     }
 
     public void drawCard() {
         card.add(new Card());
+    }
+
+    public String getName() {
+        return name;
     }
 }
