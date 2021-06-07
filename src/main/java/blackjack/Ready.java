16b03ca125c4020cc6c03747412aaed1f4a19786
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-06-07 15:36:13 +0900

    (feature) createGamblers 구현

--- src/main/java/blackjack/Ready.java
+++ src/main/java/blackjack/Ready.java
@@ -1,6 +1,22 @@
 package blackjack;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ready {
+
+    public List<Gambler> createGamblers(String inputName) {
+        return createGamblers(splitName(inputName));
+    }
+    
+    public List<Gambler> createGamblers(String[] playerList) {
+        List<Gambler> gamblers = new ArrayList<>();
+        for (String player : playerList) {
+            gamblers.add(new Gambler(player));
+        }
+        return gamblers;
+    }
+
     public String[] splitName(String name) {
         return name.split(",");
     }
