675ca752fa0b763c7b23c216ee173a6afadc9082
Sung-mi <67546948+Sung-mi@users.noreply.github.com>
2021-06-07 17:30:58 +0900

    (feature) playCardGame까지 실행 해보기

---
+++ src/main/java/blackjack/Application.java
@@ -0,0 +1,20 @@
+package blackjack;
+
+import java.util.List;
+
+public class Application {
+    public static void main(String[] args) {
+        // 1. 게임 참가자 이름 입력(","구분)
+        Input input = new Input();
+        Ready ready = new Ready();
+
+        String inputName = input.inputName();
+        List<Gambler> gamblers = ready.createGamblers(inputName);
+
+        Stadium stadium = new Stadium(gamblers);
+        stadium.playCardGame();
+
+        // 4. 게임 결과 출력
+        // 5. 승패
+    }
+}
