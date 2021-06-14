222303cf9be5760eb711fd3c1772d6783a5a3248
h1un <h1unha@naver.com>
2021-06-14 17:16:29 +0900

    feat ( InputView.java ): 참여자 이름 입력 메소드 생성

--- src/main/java/blackjackgame/view/InputView.java
+++ src/main/java/blackjackgame/view/InputView.java
@@ -1,4 +1,13 @@
 package blackjackgame.view;
 
+import java.util.Scanner;
+
 public class InputView {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public String inputPlayersNames() {
+        System.out.println("게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)");
+        return scanner.nextLine();
+    }
 }
