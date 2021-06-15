3e4d30f7a728e3b3aebc8a7ca5051acac5418aa9
이예림 <dulfla_5@naver.com>
2021-06-15 14:49:35 +0900

    (feature) Card 클래스 생성

--- src/main/java/blackjack/Card.java
+++ src/main/java/blackjack/Card.java
@@ -1,58 +1,19 @@
 package blackjack;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 public class Card {
-    private String card;
-    private int value;
+    private Denomination denomination;
+    private Suit suit;
 
-    public Card() {
-        this.card = firstNumber() + secondString();
+    public Card(Denomination denomination, Suit suit) {
+        this.denomination = denomination;
+        this.suit = suit;
     }
 
-    public String getCard() {
-        return card;
+    public String getDenomination() {
+        return denomination.name();
     }
 
-    private String firstNumber() {
-        List<String> firstNumber = new ArrayList<>();
-        for (int i = 2; i < 10; i++) {
-            firstNumber.add(Integer.toString(i));
-        }
-        firstNumber.add("A");
-        firstNumber.add("K");
-        firstNumber.add("Q");
-        firstNumber.add("J");
-
-        Collections.shuffle(firstNumber);
-        return firstNumber.get(0);
-    }
-
-    private String secondString() {
-        List<String> secondString = new ArrayList<>();
-        secondString.add("스페이드");
-        secondString.add("하트");
-        secondString.add("클로버");
-        secondString.add("다이아");
-
-        Collections.shuffle(secondString);
-        return secondString.get(0);
-    }
-
-    public int convertNumber(String value) {
-        if (value.equals("K")|| value.equals("Q") || value.equals("J")) {
-            this.value = 10;
-        }
-
-        List<Integer> a_value = new ArrayList<>();
-        a_value.add(1);
-        a_value.add(11);
-        if (value.equals("A")) {
-            Collections.shuffle(a_value);
-            this.value = a_value.get(0);
-        }
-        return this.value;
+    public String getSuit() {
+        return suit.name();
     }
 }
