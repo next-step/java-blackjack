970fe0a5c87c786d5fd09b5ae213118458f32534
이예림 <dulfla_5@naver.com>
2021-06-15 14:56:53 +0900

    (refactor) card 타입 변경

--- src/main/java/blackjack/Gambler.java
+++ src/main/java/blackjack/Gambler.java
@@ -1,41 +1,41 @@
 package blackjack;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Scanner;
 
 public class Gambler {
     private String name;
-    private String card;
+    private List<Card> cards;
 
     public Gambler() {
-        this("dealer", "확인용");
+        this("dealer");
     }
 
     public Gambler(String name) {
-        this(name, "확인용");
-    }
-
-    public Gambler(String name, String card) {
         this.name = name;
-        this.card = card;
+        this.cards = new ArrayList<>();
     }
 
-    public void drawCard() {
-        card += " " + new Card().getCard();
+    public void drawCard(){
+        List<Card> cardList = Cards.getCards();
+        Collections.shuffle(cardList);
+        this.cards.add(cardList.remove(0));
     }
 
     public String getName() {
         return name;
     }
 
-    public String getCard() {
-        return card;
+    public List<Card> getCards() {
+        return cards;
     }
 
     public boolean selectChoice(String name) {
         System.out.println(name + "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)");
         Scanner scanner = new Scanner(System.in);
-        if (scanner.nextLine() == "y") {
+        if (scanner.nextLine().equals("y")) {
             return true;
         }
         return false;
