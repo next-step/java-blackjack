3dfc859f7887c68a66ec52161d7c487d5fbeb46c
이예림 <dulfla_5@naver.com>
2021-06-28 22:45:48 +0900

    feat : Participants 클래스 생성 (Participant 객체의 일급클래스)

---
+++ src/main/java/blackjack/model/Participants.java
@@ -0,0 +1,11 @@
+package blackjack.model;
+
+import java.util.List;
+
+public class Participants {
+    private List<Participant> participants;
+
+    public Participants(List<Participant> participants) {
+        this.participants = participants;
+    }
+}
