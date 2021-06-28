dc344161c931e9b6c8ccd909dfe77e296e293761
이예림 <dulfla_5@naver.com>
2021-06-28 22:52:50 +0900

    feat : Controller#createParticipants

--- src/main/java/blackjack/controller/Controller.java
+++ src/main/java/blackjack/controller/Controller.java
@@ -1,7 +1,31 @@
 package blackjack.controller;
 
-public class Controller {
-    public void start() {
+import blackjack.model.Participant;
+import blackjack.model.Participants;
+import blackjack.model.Stadium;
+import blackjack.view.InputView;
+import blackjack.view.OutputView;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Controller {
+    private InputView inputView = new InputView();
+    private OutputView outputView = new OutputView();
+
+    public void start() {
+        String names = inputView.inputName();
+        Participants participants = createParticipants(names);
+
+//        Stadium stadium = new Stadium(participants);
+//        stadium.playCardGame();
+    }
+
+    private Participants createParticipants(String names) {
+        List<Participant> participants = new ArrayList<>();
+        for (String name : names.split(",")) {
+            participants.add(new Participant(name));
+        }
+        return new Participants(participants);
     }
 }
