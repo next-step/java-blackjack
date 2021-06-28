5a836be205b036fe6110b45dde5639c42b30f8c7
이예림 <dulfla_5@naver.com>
2021-06-28 22:34:12 +0900

    feat : OutputView#DealerCards 기능 추가 OutputView#PlayerCards OutputView#outputResult OutputView#outputWinOrLose 기능 생성

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,4 +1,25 @@
 package blackjack.view;
 
 public class OutputView {
+    public void outputPlayerCards() {
+//        System.out.print("pobi카드: ");
+//        System.out.println(String.join(", ", cards));
+    }
+
+    public void outputDealerCards() {
+        System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+    }
+
+    public void outputResult() {
+//        for(Participant participant : participants) {
+//            participant.getName() + " 카드: " + String.join(", ", participant.getCards()) + " - 결과: " + participant.getScore();
+//        }
+    }
+
+    public void outputWinOrLose() {
+//        System.out.println("최종 승패");
+//        for(Participant participant : participants) {
+//            participant.getName() + ": " + participant.getWinOrLose();
+//        }
+    }
 }
