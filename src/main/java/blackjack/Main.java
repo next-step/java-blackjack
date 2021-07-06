7314a35338934dc9354b17d6cef5a882040cba30
이예림 <dulfla_5@naver.com>
2021-07-06 16:23:16 +0900

    refactor : Applitcation 삭제 Main 사용

--- src/main/java/blackjack/Main.java
+++ src/main/java/blackjack/Main.java
@@ -2,8 +2,6 @@ package blackjack;
 
 import blackjack.controller.Controller;
 
-import javax.naming.ldap.Control;
-
 public class Main {
     public static void main(String[] args) {
         Controller controller = new Controller();
