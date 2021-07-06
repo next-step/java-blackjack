3449d00060872a0650850fcaef07466b67e8fbd6
이예림 <dulfla_5@naver.com>
2021-07-06 18:22:35 +0900

    feat : Gamer dealer와 player의 추상클래스 생성

--- src/main/java/blackjack/model/Dealer.java
+++ src/main/java/blackjack/model/Dealer.java
@@ -1,34 +1,5 @@
 package blackjack.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+public class Dealer extends Gamer {
 
-public class Dealer {
-    private List<Card> cards = new ArrayList<>();
-
-    public int score() {
-        int score = 0;
-        for (Card card : cards) {
-            score += card.getDenomination().getValue();
-        }
-        return score;
-    }
-
-    public void initialCardSetting() {
-        List<Card> cardList = Cards.getCards();
-        Collections.shuffle(cardList);
-        this.cards.add(cardList.remove(0));
-        this.cards.add(cardList.remove(0));
-    }
-
-    public void additionalCardSetting() {
-        List<Card> cardList = Cards.getCards();
-        Collections.shuffle(cardList);
-        this.cards.add(cardList.remove(0));
-    }
-
-    public List<Card> getCards() {
-        return cards;
-    }
 }
--- src/main/java/blackjack/model/Player.java
+++ src/main/java/blackjack/model/Player.java
@@ -1,43 +1,13 @@
 package blackjack.model;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-public class Player {
+public class Player extends Gamer {
     private final String name;
-    private List<Card> cards = new ArrayList<>();
 
     public Player(String name) {
         this.name = name;
     }
 
-    public int score() {
-        int score = 0;
-        for (Card card : cards) {
-            score += card.getDenomination().getValue();
-        }
-        return score;
-    }
-
-    public void initialCardSetting() {
-        List<Card> cardList = Cards.getCards();
-        Collections.shuffle(cardList);
-        this.cards.add(cardList.remove(0));
-        this.cards.add(cardList.remove(0));
-    }
-
-    public void additionalCardSetting() {
-        List<Card> cardList = Cards.getCards();
-        Collections.shuffle(cardList);
-        this.cards.add(cardList.remove(0));
-    }
-
     public String getName() {
         return name;
-    }
-
-    public List<Card> getCards() {
-        return cards;
     }
 }
