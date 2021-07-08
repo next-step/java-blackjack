9061d2b9e6a00cc4b070665a8f394e5d03149787
이예림 <dulfla_5@naver.com>
2021-07-08 17:25:07 +0900

    feat : Suit 클래스 인스턴스 변수 생성

--- src/main/java/blackjack/model/Suit.java
+++ src/main/java/blackjack/model/Suit.java
@@ -1,5 +1,18 @@
 package blackjack.model;
 
 public enum Suit {
-    CLUBS, DIAMONDS, HEARTS, SPADES
+    CLUBS("클로버"),
+    DIAMONDS("다이아몬드"),
+    HEARTS("하트"),
+    SPADES("스페이드");
+
+    private String value;
+
+    Suit(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
 }
