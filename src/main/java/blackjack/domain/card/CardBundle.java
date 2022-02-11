4e1ac2b7ddad44ec1b5983b1a4cb6bccc249d60c
PandaHun <gnsgnsek@gmail.com>
2022-02-11 17:25:35 +0900

    feat(CardBundle): 카드 52장 생성하는 기능 구현

--- src/main/java/blackjack/domain/card/CardBundle.java
+++ src/main/java/blackjack/domain/card/CardBundle.java
@@ -1,10 +1,21 @@
 package blackjack.domain.card;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class CardBundle {
 
     public List<Card> createCards() {
-        return null;
+        List<Card> cards = new ArrayList<>();
+        for (Suit suit : Suit.values()) {
+            createBySuit(cards, suit);
+        }
+        return cards;
+    }
+
+    private void createBySuit(List<Card> cards, Suit suit) {
+        for (Number number : Number.values()) {
+            cards.add(new Card(suit, number));
+        }
     }
 }
