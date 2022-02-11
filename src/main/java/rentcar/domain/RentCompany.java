3498e4c0f700770c3d220f2a18e3f4b2dc41523f
HyeonjuLee <l67378810@gmail.com>
2022-02-11 16:36:28 +0900

    feat(company): RentCompany 클래스 생성

---
+++ src/main/java/rentcar/domain/RentCompany.java
@@ -0,0 +1,20 @@
+package rentcar.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentCompany {
+
+    private final List<Car> cars;
+
+    private RentCompany() {
+        this.cars = new ArrayList<>();
+    }
+
+    public static RentCompany create() {
+        return new RentCompany();
+    }
+
+
+
+}
