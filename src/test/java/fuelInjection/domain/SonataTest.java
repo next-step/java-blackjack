7071676337c0535b52cc239f207188b848bff83d
hochan222 <hochan049@gmail.com>
2022-02-11 17:35:55 +0900

    test: 거리
    - 거리를 가져야한다.
    - 거리는 `0` 이상 정수이어야 한다.
    
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: chanuuuuu <dn0208@gmail.com>

--- src/test/java/fuelInjection/domain/SonataTest.java
+++ src/test/java/fuelInjection/domain/SonataTest.java
@@ -1,16 +1,21 @@
 package fuelInjection.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class SonataTest {
+    int TEMP_DISTANCE = 10;
+
     @DisplayName("`Sonata` String Type 이름을 가져야한다.")
     @Test
     public void givenSonataClass_whenCreateInstance_thenInstanceNameIsSonata () {
-        Sonata sonata = new Sonata();
+        Sonata sonata = new Sonata(TEMP_DISTANCE);
 
         assertThat(sonata.getName())
             .isEqualTo("Sonata");
@@ -19,9 +24,29 @@ class SonataTest {
     @DisplayName("`10km/리터`의 연비 가져야한다.")
     @Test
     public void givenSonataClass_whenCreateInstance_thenDistancePerLiterIs10 () {
-        Sonata sonata = new Sonata();
+        Sonata sonata = new Sonata(TEMP_DISTANCE);
 
         assertThat(sonata.getDistancePerLiter())
             .isEqualTo(10);
+    }
+
+    @DisplayName("거리 가져야한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {10 ,20 ,30})
+    public void givenValidDistance_whenGetTripDistance_thenNotThrowException (int tripDistance) {
+        Sonata sonata = new Sonata(tripDistance);
+
+        assertThat(sonata.getTripDistance())
+            .isEqualTo(tripDistance);
+    }
+
+    @DisplayName("거리 가져야한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0 ,-1 ,-2})
+    public void givenInvalidDistance_whenGetTripDistance_thenThrowException (int tripDistance) {
+        Sonata sonata = new Sonata(tripDistance);
+
+        assertThatThrownBy(() -> sonata.getTripDistance())
+            .isInstanceOf(IllegalArgumentException.class);
     }
 }
