0687344f2618fd1b5cb9248c85107ac38ee39796
HyeonjuLee <l67378810@gmail.com>
2022-02-11 17:32:51 +0900

    feat(company): 레포트 생성 기능 구현

--- src/main/java/rentcar/domain/RentCompany.java
+++ src/main/java/rentcar/domain/RentCompany.java
@@ -3,8 +3,13 @@ package rentcar.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class RentCompany {
+
+    private static final String DELIMITER = " : ";
+    private static final String LITER = "리터";
+    private static final String NEW_LINE = "\n";
 
     private final List<Car> cars;
 
@@ -20,7 +25,17 @@ public class RentCompany {
         cars.add(car);
     }
 
-    public List<Car> cars() { return Collections.unmodifiableList(cars); }
+    public String generateReport() {
+        return cars.stream()
+            .map(this::getCarInfo)
+            .collect(Collectors.joining(NEW_LINE));
+    }
 
+    public String getCarInfo(Car car) {
+        return car.getName() + DELIMITER + (int) car.getChargeQuantity() + LITER;
+    }
 
+    public List<Car> cars() {
+        return Collections.unmodifiableList(cars);
+    }
 }
