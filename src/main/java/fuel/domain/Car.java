8d9dd7e4682b43b58032f2e3d8e4b325086ec89e
SeungYeonJeong <crowsy@naver.com>
2022-02-11 17:02:49 +0900

    feat: 자동차들이 상속받을 수 있는 Car 클래스 구현
    Co-authored-by: seungyeonjeong <crowsy@naver.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

---
+++ src/main/java/fuel/domain/Car.java
@@ -0,0 +1,14 @@
+package fuel.domain;
+
+public abstract class Car {
+
+    abstract double getDistancePerLiter();
+
+    abstract double getTripDistance();
+
+    abstract String getName();
+
+    double getChargeQuantity() {
+        return getTripDistance() / getDistancePerLiter();
+    }
+}
