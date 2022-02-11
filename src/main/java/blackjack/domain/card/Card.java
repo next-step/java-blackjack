e8eec27aa78cafe87c2c2d27d3fa5c14ce0a7e2e
PandaHun <gnsgnsek@gmail.com>
2022-02-11 17:21:01 +0900

    feat(Card): Create Card with Suit and Number

---
+++ src/main/java/blackjack/domain/card/Card.java
@@ -0,0 +1,31 @@
+package blackjack.domain.card;
+
+import java.util.Objects;
+
+public class Card {
+
+    private final Suit suit;
+    private final Number number;
+
+    public Card(Suit suit, Number number) {
+        this.suit = suit;
+        this.number = number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return suit == card.suit && number == card.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(suit, number);
+    }
+}
