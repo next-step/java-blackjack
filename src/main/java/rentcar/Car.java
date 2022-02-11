fe0aeafa644f466da9e4d6724691c8fb95b54c2d
eohaerim <ehl3288@naver.com>
2022-02-11 23:17:21 +0900

    feat: add constructor to Car

--- src/main/java/rentcar/Car.java
+++ src/main/java/rentcar/Car.java
@@ -1,6 +1,12 @@
 package rentcar;
 
 public abstract class Car implements MotorVehicle {
+    private final int distance;
+
+    public Car (int distance) {
+        this.distance = distance;
+    }
+
     double getChargeQuantity() {
         return getTripDistance() / getDistancePerLiter();
     }
