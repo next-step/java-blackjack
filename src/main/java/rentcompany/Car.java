d0ece66eff43b655fb023f9bf11c3c1d19b1a452
lhh5863 <lhh5863@dgu.ac.kr>
2022-02-11 16:37:37 +0900

    feat(rentcompany): Car 클래스 생성

---
+++ src/main/java/rentcompany/Car.java
@@ -0,0 +1,16 @@
+package rentcompany;
+
+public abstract class Car {
+
+    abstract double getDistancePerLiter();
+
+    abstract double getTripDistance();
+
+    abstract String getName();
+
+    double getChargeQuantity() {
+        return getTripDistance() / getDistancePerLiter();
+    }
+
+}
+성
