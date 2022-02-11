19286f8646011a60be8c985df6e88ab209300b09
HyeonjuLee <l67378810@gmail.com>
2022-02-11 17:33:08 +0900

    test(company): 레포트 생성 기능 테스트

--- src/test/java/rentcar/domain/RentCompanyTest.java
+++ src/test/java/rentcar/domain/RentCompanyTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RentCompanyTest {
+    private static final String NEWLINE = System.getProperty("line.separator");
 
     @DisplayName("RentCompany_객체_생성_테스트")
     @Test
@@ -24,5 +25,25 @@ class RentCompanyTest {
         company.addCar(new Avante(300));
         company.addCar(new K5(390));
         assertThat(company.cars().size()).isEqualTo(6);
+    }
+
+    @DisplayName("RentCompany_레포트_생성_테스트")
+    @Test
+    public void generateReportTest() {
+        RentCompany company = RentCompany.create(); // factory method를 사용해 생성
+        company.addCar(new Sonata(150));
+        company.addCar(new K5(260));
+        company.addCar(new Sonata(120));
+        company.addCar(new Avante(300));
+        company.addCar(new K5(390));
+
+        String report = company.generateReport();
+        assertThat(report).isEqualTo(
+            "Sonata : 15리터" + NEWLINE +
+            "K5 : 20리터" + NEWLINE +
+            "Sonata : 12리터" + NEWLINE +
+            "Avante : 20리터" + NEWLINE +
+            "K5 : 30리터"
+        );
     }
 }
