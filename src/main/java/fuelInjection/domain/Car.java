115b8727d3fa21dae12d2908392b4e2cc6d9a9b0
hochan222 <hochan049@gmail.com>
2022-02-11 17:20:35 +0900

    feat: `Sonata` String Type 이름을 가져야한다.
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: chanuuuuu <dn0208@gmail.com>

--- src/main/java/fuelInjection/domain/Car.java
+++ src/main/java/fuelInjection/domain/Car.java
@@ -4,12 +4,12 @@ public abstract class Car {
     /**
      * 리터당 이동 거리. 즉, 연비
      */
-    abstract double getDistancePerLiter();
+//    abstract double getDistancePerLiter();
 
     /**
      * 여행하려는 거리
      */
-    abstract double getTripDistance();
+//    abstract double getTripDistance();
 
     /**
      * 차종의 이름
@@ -19,7 +19,7 @@ public abstract class Car {
     /**
      * 주입해야할 연료량을 구한다.
      */
-    double getChargeQuantity() {
-        return getTripDistance() / getDistancePerLiter();
-    }
+//    double getChargeQuantity() {
+//        return getTripDistance() / getDistancePerLiter();
+//    }
 }
---
+++ src/main/java/fuelInjection/domain/Sonata.java
@@ -0,0 +1,14 @@
+package fuelInjection.domain;
+
+public class Sonata extends Car {
+
+    private static final String NAME = "Sonata";
+
+    public Sonata () {
+
+    }
+
+    public String getName() {
+        return NAME;
+    };
+}
