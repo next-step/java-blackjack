c27bd1658ef1df927216dcb9bfd5a0f9c3ee4eb2
lhh5863 <lhh5863@dgu.ac.kr>
2022-02-13 17:25:10 +0900

    feat(rentcompany): RentCompany 클래스 생성

---
+++ src/main/java/rentcompany/RentCompany.java
@@ -0,0 +1,13 @@
+package rentcompany;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RentCompany {
+
+    private List<Car> cars;
+
+    private RentCompany() {
+        cars = new ArrayList<>();
+    }
+}
