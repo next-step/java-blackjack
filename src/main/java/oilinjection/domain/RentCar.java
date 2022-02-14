738c9a70e41844d54b065cb30d4bad3730157db2
0JUUU <clleo97@naver.com>
2022-02-14 23:45:32 +0900

    refactor: CarFactory -> RentCar 네이밍 변경
    1. 클래스 이름 변경
    2. 메서드 변경
    
    적절한 이름으로 변경

--- src/main/java/oilinjection/domain/RentCar.java
+++ src/main/java/oilinjection/domain/RentCar.java
@@ -7,23 +7,25 @@ import oilinjection.domain.car.Car;
 import oilinjection.domain.car.K5;
 import oilinjection.domain.car.Sonata;
 
-public enum CarFactory {
-    AVANTE("AVANTE", Avante::new),
-    SONATA("SONATA", Sonata::new),
-    K5("K5", K5::new);
+public enum RentCar {
+    AVANTE("AVANTE", 1, Avante::new),
+    SONATA("SONATA", 2, Sonata::new),
+    K5("K5", 2, K5::new);
 
     private static final double MIN_TRIP_DISTANCE = 1D;
     private static final String UNDER_MIN_TRIP_DISTANCE_EXCEPTION_MESSAGE = "[ERROR] 해당하는 차종이 없습니다.";
     private static final String NOT_FOUND_NAME_EXCEPTION_MESSAGE = "[ERROR] 해당하는 차종이 없습니다.";
     private final String name;
+    private final int quantity;
     private final Function<Double, Car> createCar;
 
-    CarFactory(final String name, final Function<Double, Car> createCar) {
+    RentCar(final String name, int quantity, final Function<Double, Car> createCar) {
         this.name = name;
+        this.quantity = quantity;
         this.createCar = createCar;
     }
 
-    public static Car create(final String name, final double tripDistance) {
+    public static Car rent(final String name, final double tripDistance) {
         validateOverMinTripDistance(tripDistance);
 
         return Arrays.stream(values())
--- src/test/java/oilinjection/domain/RentCarTest.java
+++ src/test/java/oilinjection/domain/RentCarTest.java
@@ -12,26 +12,26 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class CarFactoryTest {
+public class RentCarTest {
 
     @DisplayName("Avante를 반환한다.")
     @Test
     void 아반떼_생성() {
-        final Car avante = CarFactory.create("avante", 200D);
+        final Car avante = RentCar.rent("avante", 200D);
         assertThat(avante).isEqualTo(new Avante(200D));
     }
 
     @DisplayName("K5 반환한다.")
     @Test
     void K5_생성() {
-        final Car k5 = CarFactory.create("K5", 200D);
+        final Car k5 = RentCar.rent("K5", 200D);
         assertThat(k5).isEqualTo(new K5(200D));
     }
 
     @DisplayName("Sonata를 반환한다.")
     @Test
     void 소나타_생성() {
-        final Car sonata = CarFactory.create("sonata", 200D);
+        final Car sonata = RentCar.rent("sonata", 200D);
         assertThat(sonata).isEqualTo(new Sonata(200D));
     }
 
@@ -39,21 +39,21 @@ public class CarFactoryTest {
     @Test
     void 이름_없음() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> CarFactory.create("none", 10D));
+            .isThrownBy(() -> RentCar.rent("none", 10D));
     }
 
     @DisplayName("예약 하는 여행 거리가 최소 거리(1) 보다 작으면 예외가 발생한다.")
     @Test
     void 최소_거리보다_작음() {
         assertThatExceptionOfType(IllegalArgumentException.class)
-            .isThrownBy(() -> CarFactory.create("avante", 0D));
+            .isThrownBy(() -> RentCar.rent("avante", 0D));
     }
 
     @DisplayName("자동차의 이름은 대소문자를 구분하지 않는다.")
     @ValueSource(strings = {"Sonata", "sOnAtA", "SONATA"})
     @ParameterizedTest
     void 소나타_대소문자_구분없이_생성(final String name) {
-        final Car sonata = CarFactory.create(name, 200D);
+        final Car sonata = RentCar.rent(name, 200D);
         assertThat(sonata).isEqualTo(new Sonata(200D));
     }
 }
