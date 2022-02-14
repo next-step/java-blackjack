57df0313577c663e15fe96c04c89a85d513c1e92
jihyun park <girl9505@naver.com>
2022-02-14 17:36:55 +0900

    feat: Card 클래스 구현

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -2,4 +2,12 @@ package blackjack.domain;
 
 public class Card {
 
+    private final Suit suit;
+    private final Denomination denomination;
+
+    public Card(Suit suit, Denomination denomination) {
+        this.suit = suit;
+        this.denomination = denomination;
+    }
+
 }
