c808b5f3518242146bc81d0e59f12c73d3ca7422
Jiin Lee <loveted0225@naver.com>
2022-02-14 18:23:29 +0900

    feat: Gambler 객체 기본 템플릿 작성

--- src/main/java/blackjack/domain/Gambler.java
+++ src/main/java/blackjack/domain/Gambler.java
@@ -1,5 +1,19 @@
 package blackjack.domain;
 
-public class Gambler {
+import java.util.List;
 
+public abstract class Gambler {
+    protected String name;
+    protected List<Card> cards;
+
+    protected abstract int calculateScore();
+    protected abstract void receiveCard();
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Card> getCards() {
+        return cards;
+    }
 }
