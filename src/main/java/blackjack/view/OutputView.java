349996d1484b2b89d17cd58a244ab57f04a15613
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-15 00:57:55 +0900

    feat: 초기 배분한 카드와 카드상태를 출력하는 메서드 구현

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,5 +1,33 @@
 package blackjack.view;
 
+import blackjack.domain.Card;
+import blackjack.domain.Player;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class OutputView {
 
+    private static final String GAME_INITIAL_MANAGER = "딜러와 %s에게 2장의 카드를 나누었습니다.";
+    private static final String COMMA = ", ";
+    private static final String CARDS_LOG = "%s: %s";
+
+    public static void printInitialMessage(List<Player> players) {
+        String playerNames = players.stream()
+            .map(Player::getName)
+            .collect(Collectors.joining(COMMA));
+
+        System.out.println(String.format(GAME_INITIAL_MANAGER, playerNames));
+    }
+
+    public static void printCardStatus(Player player) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        List<Card> cards = player.getCards();
+        for (Card card : cards) {
+            stringBuilder.append(card.getPoint())
+                .append(card.getSymbol())
+                .append(COMMA);
+        }
+        System.out.println(String.format(CARDS_LOG, player.getName(), stringBuilder.toString()));
+    }
 }
