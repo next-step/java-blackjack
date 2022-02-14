5125c16444753dc73581e9b0988ee5050e597be3
YangWoo Yu <yuyw0712@naver.com>
2022-02-14 17:11:11 +0900

    feat: 카드의 종류를 나타내는 Suit Enum class 구현 1. 스페이드, 하트, 다이아몬드, 클로버
    Co-authored-by: seungyeonjeong <crowsy@naver.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

---
+++ src/main/java/blackjack/domain/Card/Suit.java
@@ -0,0 +1,18 @@
+package blackjack.domain.Card;
+
+public enum Suit {
+    CLUBS("클로버"),
+    DIAMONDS("다이아몬드"),
+    HEARTS("하트"),
+    SPADES("스페이드");
+
+    private String value;
+
+    Suit(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
