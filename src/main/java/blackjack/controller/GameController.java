0062d44533d8077c07f56f157aed937718da6ef8
Dami Kim <dami.k.dev@gmail.com>
2022-02-14 23:13:03 +0900

    feat: playerNames를 기준으로 Player 클래스 list를 생성하는 로직 구현

--- src/main/java/blackjack/controller/GameController.java
+++ src/main/java/blackjack/controller/GameController.java
@@ -1,10 +1,16 @@
 package blackjack.controller;
 
+import blackjack.domain.Player;
 import blackjack.view.InputView;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameController {
 
     public void start() {
-        InputView.getPlayerName();
+        List<String> playerNames = InputView.getPlayerName();
+        List<Player> players = playerNames.stream()
+            .map(Player::new)
+            .collect(Collectors.toList());
     }
 }
