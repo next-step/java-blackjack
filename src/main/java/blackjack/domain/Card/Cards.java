940fb0e5376fdcef8c3a1942cf1c74a09200e944
YangWoo Yu <yuyw0712@naver.com>
2022-02-14 17:51:59 +0900

    feat: Cards.java static 필드로 모든 카드 추가
    Co-authored-by: seungyeonjeong <crowsy@naver.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

---
+++ src/main/java/blackjack/domain/Card/Cards.java
@@ -0,0 +1,21 @@
+package blackjack.domain.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cards {
+    private static List<Card> cardList = new ArrayList<>();
+
+    static {
+        for(Denomination denomination : Denomination.values()) {
+            for(Suit suit : Suit.values()) {
+                cardList.add(new Card(denomination, suit));
+            }
+        }
+    }
+
+    public static List<Card> getCardList() {
+        return cardList;
+    }
+
+}
