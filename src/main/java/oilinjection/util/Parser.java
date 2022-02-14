7603b88cf09d07739b2b4591ca7cd8ded34612ba
0JUUU <clleo97@naver.com>
2022-02-14 18:01:50 +0900

    feat: 사용자 입력을 파싱하여 Rent 객체 리스트를 반환

---
+++ src/main/java/oilinjection/util/Parser.java
@@ -0,0 +1,37 @@
+package oilinjection.util;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import oilinjection.domain.vo.Rent;
+
+public class Parser {
+
+    private static final String RESERVATION_DELIMITER = ",";
+    private static final String INFO_DELIMITER = ":";
+    private static final int NAME = 0;
+    private static final int TRIP_DISTANCE = 1;
+    private static final String INPUT_FORMAT_REGEX = "[a-zA-z0-9]+:[0-9]+";
+    private static final String INVALID_FORMAT_EXCEPTION_MESSAGE = "[ERROR] 입력 형식이 맞지 않습니다(eg.${carName}:${tripDistance})";
+
+    public static List<Rent> parse(final String input) {
+
+        final String[] rentInfo = input.split(RESERVATION_DELIMITER);
+
+        return Collections.unmodifiableList(
+            Arrays.stream(rentInfo)
+                .map(rent -> {
+                    validateInfoFormat(rent);
+                    final String[] split = rent.split(INFO_DELIMITER);
+                    return new Rent(split[NAME], Double.parseDouble(split[TRIP_DISTANCE]));
+                })
+                .collect(Collectors.toList()));
+    }
+
+    private static void validateInfoFormat(final String rent) {
+        if (!rent.matches(INPUT_FORMAT_REGEX)) {
+            throw new IllegalArgumentException(INVALID_FORMAT_EXCEPTION_MESSAGE);
+        }
+    }
+}
