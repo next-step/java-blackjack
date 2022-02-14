1fd696b3ec1179935300a3a219021238a8b26fad
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-15 00:23:08 +0900

    feat: 전체 카드의 상태를 들고 있는 클래스 구현
    - 생성자: 정적팩토리메서드
    - removeCard() : 인자로 들어온 플레이어에게 카드를 하나 추가

---
+++ src/main/java/blackjack/domain/CardPack.java
@@ -0,0 +1,54 @@
+package blackjack.domain;
+
+import blackjack.util.CardShuffler;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class CardPack {
+
+    private static final List<String> symbols = Arrays.asList("스페이드", "다이아몬드", "클로버", "하트");
+
+    private final Map<String, List<Card>> map;
+
+    public CardPack(Map<String, List<Card>> map) {
+        this.map = map;
+    }
+
+    public static CardPack create() {
+        Map<String, List<Card>> map = new HashMap<>();
+        for (String symbol : symbols) {
+            List<Card> cards = Arrays.stream(CardType.values())
+                .map(x -> new Card(symbol, x.getName(), x.getPoint()))
+                .collect(Collectors.toList());
+
+            map.put(symbol, cards);
+        }
+
+        return new CardPack(map);
+    }
+
+    public void removeCard(List<Player> players) {
+        for (Player player : players) {
+            for (int i = 0; i < 2; i++) {
+                int symbolIndex = CardShuffler.pickIndexIn(4);
+                List<Card> cards = map.get(symbols.get(symbolIndex));
+                int cardIndex = CardShuffler.pickIndexIn(cards.size());
+                player.addCard(cards.remove(cardIndex));
+            }
+        }
+    }
+
+    public void removeCard(Dealer dealer) {
+        for (int i = 0; i < 2; i++) {
+            int symbolIndex = CardShuffler.pickIndexIn(4);
+            List<Card> cards = map.get(symbols.get(symbolIndex));
+            int cardIndex = CardShuffler.pickIndexIn(cards.size());
+
+            dealer.addCard(cards.remove(cardIndex));
+        }
+    }
+
+}
