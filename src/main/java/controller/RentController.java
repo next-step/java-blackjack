e2eddd1c18fe94e738207cc8b4919de502df089d
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-14 17:00:11 +0900

    feat: rent Application의 start() 추가

--- src/main/java/controller/RentController.java
+++ src/main/java/controller/RentController.java
@@ -1,5 +1,10 @@
 package controller;
 
+import view.InputView;
+
 public class RentController {
 
+    public void start() {
+        System.out.println("userInput >>> " + InputView.getTripDistance());
+    }
 }
