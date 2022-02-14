2e16386fd0b2d457ed472e50c78a24b729a65683
Jiin Lee <loveted0225@naver.com>
2022-02-14 18:21:34 +0900

    feat: Dealer 객체 메소드 추가 - allocateCard()

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -1,5 +1,26 @@
 package blackjack.domain;
 
-public class Dealer {
+import java.util.ArrayList;
+
+public class Dealer extends Gambler{
+
+    public Dealer(String name){
+        this.name = name;
+        this.cards=new ArrayList<>();
+    }
+
+    @Override
+    protected int calculateScore() {
+        return 0;
+    }
+
+    @Override
+    protected void receiveCard() {
+
+    }
+
+    public Card allocateCard(){
+        return null;
+    }
 
 }
