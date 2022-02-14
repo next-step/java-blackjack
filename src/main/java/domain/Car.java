0e531c0f2a25e661d8489ce314c85120ac0105b8
Dami Kim <dami.k.dev@gmail.com>
2022-02-14 16:43:47 +0900

    refactor: Car 추상 클래스에 constructor 생성하여 중복 제거

--- src/main/java/domain/Car.java
+++ src/main/java/domain/Car.java
@@ -2,6 +2,12 @@ package domain;
 
 public abstract class Car {
 
+    protected final int distance;
+
+    public Car(int distance) {
+        this.distance = distance;
+    }
+
     abstract double getTripDistance();
 
     abstract double getDistancePerLiter();
