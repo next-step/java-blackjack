d97b2c647f5ad18b86cb0081af79a10722a8d0fd
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-15 00:25:20 +0900

    feat: Card 클래스 getter() 추가, 생성자 인자로 symbol 추가

--- src/main/java/blackjack/domain/Card.java
+++ src/main/java/blackjack/domain/Card.java
@@ -2,11 +2,21 @@ package blackjack.domain;
 
 public class Card {
 
+    private final String symbol;
     private final String name;
     private final int point;
 
-    public Card(String name, int point) {
+    public Card(String symbol, String name, int point) {
+        this.symbol = symbol;
         this.name = name;
         this.point = point;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getPoint() {
+        return this.point;
     }
 }
