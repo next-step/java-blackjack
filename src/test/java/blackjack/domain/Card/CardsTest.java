849b9b11910e152348988de68e13f97253971bed
YangWoo Yu <yuyw0712@naver.com>
2022-02-14 17:51:23 +0900

    test: CardsTest.java 모든 카드를 출력하는 테스트 코드 작성
    Co-authored-by: seungyeonjeong <crowsy@naver.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

---
+++ src/test/java/blackjack/domain/Card/CardsTest.java
@@ -0,0 +1,19 @@
+package blackjack.domain.Card;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+class CardsTest {
+
+    @Test
+     void 모든_카드를_출력한다() {
+        List<Card> cardList = new ArrayList<>();
+        for(Denomination denomination : Denomination.values()) {
+            for(Suit suit : Suit.values()) {
+                cardList.add(new Card(denomination, suit));
+            }
+        }
+        cardList.forEach(card -> System.out.println(new StringBuilder().append(card.getDenomination().getName()).append(card.getSuit().getValue())));
+    }
+}
