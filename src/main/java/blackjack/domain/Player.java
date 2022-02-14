faa242df5418c30129f580601a18d8d2b5e72e25
Jiin Lee <loveted0225@naver.com>
2022-02-14 18:24:29 +0900

    feat: Player 객체 메소드 작성

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -1,5 +1,24 @@
 package blackjack.domain;
 
-public class Player {
+import java.util.ArrayList;
+import java.util.List;
+
+public class Player extends Gambler {
+
+    public Player(final String name) {
+        this.name = name;
+        this.cards = new ArrayList<>();
+    }
+
+    @Override
+    protected int calculateScore() {
+        return 0;
+    }
+
+    @Override
+    protected void receiveCard() {
+
+    }
+
 
 }
