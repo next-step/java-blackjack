cbcc6a07c7bb4244cf7132d8293c2b221f4167f1
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-14 16:57:33 +0900

    feat: 여행 거리를 입력받을 수 있는 InputView 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -1,5 +1,28 @@
 package view;
 
+import java.util.Scanner;
+
 public class InputView {
 
+    private final static Scanner scanner = new Scanner(System.in);
+    private final static String TRIP_MANAGER = "여행 거리를 입력해 주세요.";
+    private final static String ERROR_LOG = "[ERROR] 0이나 빈 값이 들어올 수 없습니다";
+
+    public static String getTripDistance() {
+        try {
+            System.out.println(TRIP_MANAGER);
+            String input = scanner.nextLine();
+            validateEmptyOrZero(input);
+            return input;
+        } catch (IllegalArgumentException e) {
+            e.printStackTrace();
+            return getTripDistance();
+        }
+    }
+
+    private static void validateEmptyOrZero(String input) {
+        if (input.trim().isEmpty() || input.equals("0")) {
+            throw new IllegalArgumentException(ERROR_LOG);
+        }
+    }
 }
