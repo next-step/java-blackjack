b97be07ec2ea430cbbc91a79a10956fd60329c73
YangWoo Yu <yuyw0712@naver.com>
2022-02-14 17:42:42 +0900

    feat: Denomination 카드의 이름과 값을 가지도록 enum class 수정
    Co-authored-by: seungyeonjeong <crowsy@naver.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/main/java/blackjack/domain/Card/Denomination.java
+++ src/main/java/blackjack/domain/Card/Denomination.java
@@ -1,24 +1,30 @@
 package blackjack.domain.Card;
 
 public enum Denomination {
-    ACE(1),
-    TWO(2),
-    THREE(3),
-    FOUR(4),
-    FIVE(5),
-    SIX(6),
-    SEVEN(7),
-    EIGHT(8),
-    NINE(9),
-    TEN(10),
-    JACK(10),
-    QUEEN(10),
-    KING(10);
+    ACE("A", 1),
+    TWO("2", 2),
+    THREE("3", 3),
+    FOUR("4", 4),
+    FIVE("5", 5),
+    SIX("6", 6),
+    SEVEN("7", 7),
+    EIGHT("8", 8),
+    NINE("9", 9),
+    TEN("10", 10),
+    JACK("J", 10),
+    QUEEN("Q", 10),
+    KING("K", 10);
 
+    private String name;
     private int value;
 
-    Denomination(int value) {
+    Denomination(String name, int value) {
+        this.name = name;
         this.value = value;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public int getValue() {
