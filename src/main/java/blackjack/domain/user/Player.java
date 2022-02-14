e90bbd85341b78d74ef7151170f48a055aa11d22
PandaHun <gnsgnsek@gmail.com>
2022-02-14 16:46:13 +0900

    feat(Player): Create Constructor and name feature

--- src/main/java/blackjack/domain/user/Player.java
+++ src/main/java/blackjack/domain/user/Player.java
@@ -2,10 +2,20 @@ package blackjack.domain.user;
 
 public class Player {
 
+    private final String name;
+
     public Player(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.trim().length() == 0) {
+            throw new IllegalArgumentException("[ERROR]");
+        }
     }
 
     public String name() {
-        return null;
+        return name;
     }
 }
