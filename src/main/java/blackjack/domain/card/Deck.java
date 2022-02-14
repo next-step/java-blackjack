13e6a9ddb2a416fa2cfe51d3e906cedfb46bae89
PandaHun <gnsgnsek@gmail.com>
2022-02-14 17:11:56 +0900

    feat(Deck): Create Deck constructor and spreadCards, refactor testing

--- src/main/java/blackjack/domain/card/Deck.java
+++ src/main/java/blackjack/domain/card/Deck.java
@@ -1,14 +1,24 @@
 package blackjack.domain.card;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Deck {
 
+    private final Queue<Card> deck;
+
+    public Deck(List<Card> cards) {
+        this.deck = new LinkedList<>(cards);
+    }
+
     public List<Card> cards() {
-        return null;
+        return Collections.unmodifiableList(new ArrayList<>(deck));
     }
 
     public Card spreadCard() {
-        return null;
+        return deck.poll();
     }
 }
--- src/test/java/blackjack/domain/card/DeckTest.java
+++ src/test/java/blackjack/domain/card/DeckTest.java
@@ -9,14 +9,16 @@ class DeckTest {
 
     @Test
     void 카드번들을_제대로_받는지_테스트() {
-        Deck deck = new Deck();
+        CardBundle cardBundle = new CardBundle();
+        Deck deck = new Deck(cardBundle.createCards());
         List<Card> cards = deck.cards();
         assertThat(cards.size()).isEqualTo(52);
     }
 
     @Test
     void 카드를_한장_뽑아준다() {
-        Deck deck = new Deck();
+        CardBundle cardBundle = new CardBundle();
+        Deck deck = new Deck(cardBundle.createCards());
         deck.spreadCard();
         List<Card> cards = deck.cards();
         assertThat(cards.size()).isEqualTo(51);
