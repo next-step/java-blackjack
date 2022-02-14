b803fc17aa7867875fd3e04197b5bde4fe3d7f64
0JUUU <clleo97@naver.com>
2022-02-14 22:08:05 +0900

    feat: 대소문자 구분없이 Car 객체를 반환하는지 확인하는 테스트코드 작성

--- src/test/java/oilinjection/domain/CarFactoryTest.java
+++ src/test/java/oilinjection/domain/CarFactoryTest.java
@@ -9,6 +9,8 @@ import oilinjection.domain.car.K5;
 import oilinjection.domain.car.Sonata;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarFactoryTest {
 
@@ -45,5 +47,13 @@ public class CarFactoryTest {
     void 최소_거리보다_작음() {
         assertThatExceptionOfType(IllegalArgumentException.class)
             .isThrownBy(() -> CarFactory.create("avante", 0D));
+    }
+
+    @DisplayName("자동차의 이름은 대소문자를 구분하지 않는다.")
+    @ValueSource(strings = {"Sonata", "sOnAtA", "SONATA"})
+    @ParameterizedTest
+    void 소나타_대소문자_구분없이_생성(final String name) {
+        final Car sonata = CarFactory.create(name, 200D);
+        assertThat(sonata).isEqualTo(new Sonata(200D));
     }
 }
