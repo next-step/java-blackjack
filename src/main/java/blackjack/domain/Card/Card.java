787ac1dfbac9be658b976a435af8316b6715e792
YangWoo Yu <yuyw0712@naver.com>
2022-02-14 17:22:21 +0900

    feat: Card.java denomination와 suit을 상태로 가지는 객체 생성
    Co-authored-by: seungyeonjeong <crowsy@naver.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

---
+++ src/main/java/blackjack/domain/Card/Card.java
@@ -0,0 +1,19 @@
+package blackjack.domain.Card;
+
+public class Card {
+    private final Denomination denomination;
+    private final Suit suit;
+
+    public Card(final Denomination denomination, final Suit suit) {
+        this.denomination = denomination;
+        this.suit = suit;
+    }
+
+    public Denomination getDenomination() {
+        return denomination;
+    }
+
+    public Suit getSuit() {
+        return suit;
+    }
+}
