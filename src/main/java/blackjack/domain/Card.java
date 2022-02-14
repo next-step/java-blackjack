f062dac64759b880fdf3c7b45ebb67c2946a0c13
Dami Kim <dami.k.dev@gmail.com>
2022-02-14 23:12:20 +0900

    feat: Card Class 구현

---
+++ src/main/java/blackjack/domain/Card.java
@@ -0,0 +1,12 @@
+package blackjack.domain;
+
+public class Card {
+
+    private final String name;
+    private final int point;
+
+    public Card(String name, int point) {
+        this.name = name;
+        this.point = point;
+    }
+}
