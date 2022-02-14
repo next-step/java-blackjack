d8a753e367c4dfe1f1eb9e4992453efaad35bdde
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-14 18:01:49 +0900

    feat: RentCompany가 만든 report를 출력하는 기능 구현

--- src/main/java/view/ReportView.java
+++ src/main/java/view/ReportView.java
@@ -1,5 +1,14 @@
 package view;
 
+import java.util.Map;
+
 public class ReportView {
 
+    private static final String MESASGE = "%s : %.2f 리터";
+
+    public static void generateReport(Map<String, Double> report) {
+        for (String carName : report.keySet()) {
+            System.out.println(String.format(MESASGE, carName, report.get(carName)));
+        }
+    }
 }
