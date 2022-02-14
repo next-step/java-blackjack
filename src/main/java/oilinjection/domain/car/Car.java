f408a4c3c9665e86577d2a1c283c913e76cfdbe3
0JUUU <clleo97@naver.com>
2022-02-14 21:37:30 +0900

    feat: Car 객체 내 equals & hashcode 오버라이드

--- src/main/java/oilinjection/domain/car/Car.java
+++ src/main/java/oilinjection/domain/car/Car.java
@@ -1,5 +1,7 @@
 package oilinjection.domain.car;
 
+import java.util.Objects;
+
 public abstract class Car {
 
     final double tripDistance;
@@ -18,5 +20,22 @@ public abstract class Car {
 
     double getChargeQuantity() {
         return getTripDistance() / getDistancePerLiter();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return Double.compare(car.tripDistance, tripDistance) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tripDistance);
     }
 }
