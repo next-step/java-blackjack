94576160d686e379a8dcf8e6a49b6d18cd6a9096
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-15 18:07:57 +0900

    feat: Player class에서 딜러인지 판별하는 상태 추가

--- src/main/java/blackjack/domain/Player.java
+++ src/main/java/blackjack/domain/Player.java
@@ -9,11 +9,13 @@ public class Player {
     private final String name;
     private int score;
     private List<Card> cards;
+    private boolean isDealer;
 
-    public Player(String name) {
+    public Player(String name, boolean isDealer) {
         this.name = name;
         this.score = 0;
         this.cards = new ArrayList<>();
+        this.isDealer = isDealer;
     }
 
     public void addCard(final Card card) {
