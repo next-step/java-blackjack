36af6d52a9738980820b059bf9b5428f2e14a63e
MinChul-Son <smc5236@gmail.com>
2022-02-15 17:37:42 +0900

    test: Deck 테스트코드 작성
    - 모든 카드(52개)를 생성시점에 초기화
    - 덱에서 한장을 카드를 뽑음
    - 덱이 비면 예외
    
    Co-authored-by: judy5050 <qkrgywjd456@naver.com>
    Co-authored-by: minchul-son <smc5236@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>

---
+++ src/test/java/blackjack/DeckTest.java
@@ -0,0 +1,43 @@
+package blackjack;
+
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class DeckTest {
+
+    @Test
+    void 덱의_사이즈는_52개이다() {
+        Deck deck = Deck.create();
+
+        assertThat(deck.deckSize()).isEqualTo(52);
+    }
+
+    @Test
+    void 덱에서_한장의_카드를_뽑을_수_있다() {
+        Card clubEight = new Card(Suit.CLUB, CardNumber.EIGHT);
+        Card clubSeven = new Card(Suit.CLUB, CardNumber.SEVEN);
+
+        List<Card> cards = new ArrayList<>(Arrays.asList(clubEight, clubSeven));
+
+        Deck deck = new Deck(cards);
+
+        Card card = deck.drawCard();
+        Card expected = new Card(Suit.CLUB, CardNumber.EIGHT);
+
+        assertThat(card).isEqualTo(expected);
+    }
+
+    @Test
+    void 덱이_비어있을_때는_런타임_예외가_발생한다() {
+        List<Card> cards = new ArrayList<>();
+        Deck deck = new Deck(cards);
+
+        assertThatThrownBy(deck::drawCard)
+            .isInstanceOf(RuntimeException.class)
+            .hasMessage("더 이상 카드를 뽑을 수 없습니다.");
+    }
+}
