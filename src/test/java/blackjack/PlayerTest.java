5c6ebe816b358c8ea3674bc6e8dba9d9c8df6639
MinChul-Son <smc5236@gmail.com>
2022-02-15 18:22:19 +0900

    refactor: 불필요한 공백 및 public 키워드 제거
    Co-authored-by: judy5050 <qkrgywjd456@naver.com>
    Co-authored-by: minchul-son <smc5236@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>

--- src/test/java/blackjack/PlayerTest.java
+++ src/test/java/blackjack/PlayerTest.java
@@ -10,15 +10,13 @@ class PlayerTest {
     private Player player;
 
     @BeforeEach
-    public void init() {
+    void init() {
         player = new Player("a", new Cards(Arrays.asList(new Card(Suit.CLUB, CardNumber.EIGHT),
             new Card(Suit.DIAMOND, CardNumber.FIVE))));
     }
 
     @Test
-    public void 카드점수_확인_테스트() {
+    void 카드점수_확인_테스트() {
         Assertions.assertThat(player.getTotalScore()).isEqualTo(13);
     }
-
-
 }
