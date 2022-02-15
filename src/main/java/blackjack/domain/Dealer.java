0646780e0207019fdf18a8bed2b5aed09cf6cb31
HyeonjuLee <l67378810@gmail.com>
2022-02-16 01:48:56 +0900

    feat(dealer) : Dealer 클래스 생성

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -1,5 +1,13 @@
 package blackjack.domain;
 
 public class Dealer {
+    private final CardAddible cards;
 
+    public Dealer(CardAddible cards) {
+        this.cards = cards;
+    }
+
+    public CardAddible getCards() {
+        return cards;
+    }
 }
