86799b78b61eabe1affd1b97714492583663e72e
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-15 22:49:34 +0900

    feat: Dealer Class 내부 메서드 로직 변경
    - 게임 로직에 의해 메서드 분리

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -7,29 +7,76 @@ import java.util.List;
 
 public class Dealer {
 
+    private static final int INITIAL_CARD_QUANTITY = 2;
     private static final int BLACKJACK = 21;
+    private static final int DEALER_BOUND = 16;
     private final CardPack cardPack;
 
     public Dealer(CardPack cardPack) {
         this.cardPack = cardPack;
     }
 
-    public void game(List<Player> players) {
+    public void initializeGame(GamePlayers gamePlayers) {
+        List<Player> players = gamePlayers.getPlayers();
+
+        for (int i = 0; i < INITIAL_CARD_QUANTITY; i++) {
+            players.stream()
+                .forEach(player -> giveCardAfterPick(player));
+        }
+    }
+
+    public void playGame(GamePlayers gamePlayers) {
+        List<Player> players = gamePlayers.getPlayers();
+
         for (Player player : players) {
-            if (player.isDealer()) continue;
+            giveCardToPlayer(player);
+        }
+        for (Player player : players) {
+            System.out.println(player.getName()+"의 점수 : "+player.getScore());
+        }
+    }
 
-            if(isLowerThanBlackJack(player)) {
-                OutputView.printQuestionAcceptCard(player);
+    private void giveCardToPlayer(Player player) {
+        if (player.isDealer()) {
+            giveCardToDealerPlayer(player);
+            return;
+        }
+        giveCardToGeneralPlayer(player);
+    }
 
-                while (InputView.getPlayerChoice()) {
-                    cardPack.giveCard(player);
-                    OutputView.printCardStatus(player);
-                }
+    private void giveCardToGeneralPlayer(Player player) {
+        int score = player.getScore();
+
+        if (isLowerThanBlackJack(score)) {
+            OutputView.printQuestionAcceptCard(player);
+
+            while (isLowerThanBlackJack(score) && InputView.getPlayerChoice()) {
+                giveCardAfterPick(player);
+                OutputView.printCardStatus(player);
+                score = player.getScore();
             }
         }
     }
 
-    public boolean isLowerThanBlackJack(Player player) {
-        return player.getScore() < BLACKJACK;
+    private void giveCardToDealerPlayer(Player player){
+        int score = player.getScore();
+        while (isLowerThanDealerBound(score)) {
+            OutputView.printDealrAcceptCard();
+            giveCardAfterPick(player);
+            score = player.getScore();
+        }
+    }
+
+    private void giveCardAfterPick(Player player) {
+        Card pickedCard = cardPack.pickCard(player);
+        player.receiveCard(pickedCard);
+    }
+
+    public boolean isLowerThanBlackJack(int score) {
+        return score < BLACKJACK;
+    }
+
+    public boolean isLowerThanDealerBound(int score) {
+        return score < DEALER_BOUND;
     }
 }
