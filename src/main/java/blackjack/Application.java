13cc902b783685f3bedab5797d48cb0726e3b9a2
HyeonjuLee <l67378810@gmail.com>
2022-02-16 01:52:53 +0900

    feat(main) : blackjack 기능 구현

---
+++ src/main/java/blackjack/Application.java
@@ -0,0 +1,10 @@
+package blackjack;
+
+import blackjack.controller.Controller;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Controller.run();
+    }
+}
---
+++ src/main/java/blackjack/controller/Controller.java
@@ -0,0 +1,142 @@
+package blackjack.controller;
+
+import blackjack.domain.Card;
+import blackjack.domain.CardDeck;
+import blackjack.domain.Cards;
+import blackjack.domain.Dealer;
+import blackjack.domain.Player;
+import blackjack.view.InputView;
+import blackjack.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Controller {
+
+    private static final InputView inputView = new InputView();
+    private static final OutputView outputView = new OutputView();
+
+    public static void run() {
+        List<String> playerNames = inputView.inputPlayers();
+
+        List<Player> players = playerNames.stream()
+            .map(name -> new Player(name, new Cards(CardDeck.pop(2))))
+            .collect(Collectors.toList());
+
+        Dealer dealer = new Dealer(new Cards(CardDeck.pop(2)));
+
+        outputView.printStartMessage(playerNames);
+        System.out.println("딜러 : " + dealer.getCards().cards().get(0)); // 하나만 뽑혀야 함
+
+        players.forEach(player -> outputView.printCurrentCardsState(player.getName(),
+            player.getCards().cards()));
+
+        for (Player player : players) {
+            String yesOrNo = "";
+            do {
+                yesOrNo = inputView.inputYesOrNo(player.getName());
+                if (yesOrNo.equals("y")) {
+                    player.getCards().addCard(CardDeck.pop());
+                    outputView.printCurrentCardsState(player.getName(), player.getCards().cards());
+                    int totalScore = player.getCards().cards().stream()
+                        .filter(card -> !card.getCardNumber().name().equals("A"))
+                        .mapToInt(card -> card.getCardNumber().getScore()).sum();
+
+                    List<Card> ACEList = player.getCards().cards().stream()
+                        .filter(card -> card.getCardNumber().name().equals("A"))
+                        .collect(Collectors.toList());
+
+                    for (Card card : ACEList) {
+                        if (totalScore + 11 > 21) {
+                            totalScore += 1;
+                        } else {
+                            totalScore += card.getCardNumber().getScore();
+                        }
+                    }
+                }
+            } while (!yesOrNo.equals("n"));
+        }
+
+        int dealerTotalScore = dealer.getCards().cards().stream()
+            .filter(card -> !card.getCardNumber().name().equals("A"))
+            .mapToInt(card -> card.getCardNumber().getScore()).sum();
+
+        List<Card> ACEList = dealer.getCards().cards().stream()
+            .filter(card -> card.getCardNumber().name().equals("A"))
+            .collect(Collectors.toList());
+
+        for (Card card : ACEList) {
+            if (dealerTotalScore + 11 > 21) {
+                dealerTotalScore += 1;
+            } else {
+                dealerTotalScore += card.getCardNumber().getScore();
+            }
+        }
+
+        if (dealerTotalScore <= 16) {
+            dealer.getCards().addCard(CardDeck.pop());
+            System.out.println("딜러는 16이하라 한장의 카드를 더 받았습니다.");
+        }
+
+        dealerTotalScore = dealer.getCards().cards().stream()
+            .filter(card -> !card.getCardNumber().name().equals("A"))
+            .mapToInt(card -> card.getCardNumber().getScore()).sum();
+
+        ACEList = dealer.getCards().cards().stream()
+            .filter(card -> card.getCardNumber().name().equals("A"))
+            .collect(Collectors.toList());
+
+        for (Card card : ACEList) {
+            if (dealerTotalScore + 11 > 21) {
+                dealerTotalScore += 1;
+            } else {
+                dealerTotalScore += card.getCardNumber().getScore();
+            }
+        }
+        outputView.printResult("딜러", dealer.getCards().cards(), dealerTotalScore);
+
+        List<Integer> playerResults = new ArrayList<>();
+        for (Player player : players) {
+            int playerTotalScore = player.getCards().cards().stream()
+                .filter(card -> !card.getCardNumber().name().equals("A"))
+                .mapToInt(card -> card.getCardNumber().getScore()).sum();
+
+            List<Card> PlayerACE = player.getCards().cards().stream()
+                .filter(card -> card.getCardNumber().name().equals("A"))
+                .collect(Collectors.toList());
+
+            for (Card card : PlayerACE) {
+                if (playerTotalScore + 11 > 21) {
+                    playerTotalScore += 1;
+                } else {
+                    playerTotalScore += card.getCardNumber().getScore();
+                }
+            }
+            playerResults.add(playerTotalScore);
+            outputView.printResult(player.getName(), player.getCards().cards(), playerTotalScore);
+        }
+
+        int finalDealerTotalScore = dealerTotalScore;
+        Long dealerWin = playerResults.stream().filter(
+            playerResult -> (finalDealerTotalScore > playerResult)
+        ).count();
+        Long dealerLose = playerResults.stream().filter(
+            playerResult -> (finalDealerTotalScore < playerResult)
+        ).count();
+        System.out.printf("딜러 : %d승 %d패%n", dealerWin, dealerLose);
+
+        for (int i = 0; i < players.size(); i++) {
+            String name = players.get(i).getName();
+            int totalScore = playerResults.get(i);
+
+            Long win = playerResults.stream().filter(
+                playerResult -> (totalScore > playerResult)
+            ).count();
+
+            Long lose = playerResults.stream().filter(
+                playerResult -> (totalScore < playerResult)
+            ).count();
+            System.out.printf("%s : %d승 %d패%n", name, win, lose);
+        }
+    }
+}
