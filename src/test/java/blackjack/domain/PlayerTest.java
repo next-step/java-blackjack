c571bc5af6eb30c6fa9566170662bc6e188101e6
HyeonjuLee <l67378810@gmail.com>
2022-02-15 18:33:07 +0900

    style(player): Participant -> Player 로 클래스 이름 변경

--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -6,14 +6,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-class ParticipantTest {
+class PlayerTest {
 
     @DisplayName("참가자_생성_테스트")
     @ParameterizedTest
     @ValueSource(strings = {"pobi", "json"})
-    void createParticipantTest(String name) {
-        Participant participant = new Participant(name);
-        assertThat(participant.getName()).isEqualTo(name);
+    void createPlayerTest(String name) {
+        Player player = new Player(name);
+        assertThat(player.getName()).isEqualTo(name);
     }
-
 }
