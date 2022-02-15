8947cb3a8085b98d123361d0bb1e16d875a5144f
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-15 22:50:19 +0900

    feat: 일급컬렉션 생성으로 인한 Controller 로직 변경
    - 메서드, 클래스 분리

--- src/main/java/blackjack/controller/GameController.java
+++ src/main/java/blackjack/controller/GameController.java
@@ -2,13 +2,11 @@ package blackjack.controller;
 
 import blackjack.domain.CardPack;
 import blackjack.domain.Dealer;
+import blackjack.domain.GamePlayers;
 import blackjack.domain.Player;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
-import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 public class GameController {
 
@@ -16,32 +14,20 @@ public class GameController {
         CardPack cardPack = CardPack.create();
         Dealer dealer = new Dealer(cardPack);
 
-        List<Player> players = makePlayers();
+        GamePlayers gamePlayers = GamePlayers.makePlayers(InputView.getPlayerName());
 
-        cardPack.pickCard(players);
+        dealer.initializeGame(gamePlayers);
+        printInitialStatus(gamePlayers);
+        dealer.playGame(gamePlayers);
+    }
+
+    private void printInitialStatus(GamePlayers gamePlayers) {
+        List<Player> players = gamePlayers.getPlayers();
 
         OutputView.printInitialMessage(players);
 
         for (Player player : players) {
             OutputView.printCardStatus(player);
         }
-
-        dealer.game(players);
-    }
-
-    private List<Player> makePlayers() {
-        List<String> playerNames = InputView.getPlayerName();
-
-        Player dealerPlayer = new Player("딜러", true);
-
-        List<Player> dealerPlayers = Arrays.asList(dealerPlayer);
-
-        List<Player> commonPlayers = playerNames.stream()
-            .map(player -> new Player(player, false))
-            .collect(Collectors.toList());
-
-        return Stream.of(dealerPlayers, commonPlayers)
-            .flatMap(lotto -> lotto.stream())
-            .collect(Collectors.toList());
     }
 }
