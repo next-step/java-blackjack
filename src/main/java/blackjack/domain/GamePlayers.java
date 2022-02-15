90056e24f66869ba3f4d7a95afe61daa6a9cd948
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-15 22:46:58 +0900

    feat: 게임플레이어를 감싸는 일급컬렉션 생성

---
+++ src/main/java/blackjack/domain/GamePlayers.java
@@ -0,0 +1,33 @@
+package blackjack.domain;
+
+import blackjack.view.InputView;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class GamePlayers {
+
+    private final List<Player> players;
+
+    public GamePlayers(List<Player> players) {
+        this.players = players;
+    }
+
+    public List<Player> getPlayers() {
+        return this.players;
+    }
+
+    public static GamePlayers makePlayers(List<String> playerNames) {
+        List<Player> dealerPlayers = Arrays.asList(new Player("딜러", true));
+        List<Player> commonPlayers = playerNames.stream()
+            .map(player -> new Player(player, false))
+            .collect(Collectors.toList());
+
+        List<Player> players = Stream.of(dealerPlayers, commonPlayers)
+            .flatMap(lotto -> lotto.stream())
+            .collect(Collectors.toList());
+
+        return new GamePlayers(players);
+    }
+}
