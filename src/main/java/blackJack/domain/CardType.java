9c12086c91a35eea9171b9414586d1c838c876e8
hochan222 <hochan049@gmail.com>
2022-02-15 18:23:54 +0900

    feat: 주어진 String type 으로 CardType 생성 후 getCardType() 호출시, 주어진 type과 동일하다.
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: chanuuuuu <dn0208@gmail.com>

---
+++ src/main/java/blackJack/domain/CardType.java
@@ -0,0 +1,31 @@
+package blackJack.domain;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class CardType {
+
+    private static final List<String> CARD_TYPE_LIST = Arrays.asList("다이아몬드", "하트", "클로버", "스페이드");
+
+    private String cardType;
+
+    private CardType(String cardType) {
+        this.cardType = cardType;
+
+        validateCardNumberRange();
+    }
+
+    public static CardType from(String cardType) {
+        return new CardType(cardType);
+    }
+
+    public String getCardType() {
+        return cardType;
+    }
+
+    private void validateCardNumberRange() {
+        if (!CARD_TYPE_LIST.contains(cardType)) {
+            throw new RuntimeException("[ERROR] CardType 범위인 String을 입력해주세요.");
+        }
+    }
+}
