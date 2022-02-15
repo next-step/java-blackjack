97e6457ae7b5a76f8b0e3bbc1d2d36b0f7e1b524
YangWoo Yu <yuyw0712@naver.com>
2022-02-15 22:41:04 +0900

    feat: 게임의 결과를 구하고 카드의 합을 구하는 출력문 구현
    Co-authored-by: seungyeonyeong <crowsy@naver.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/main/java/blackjack/controller/GameLauncher.java
+++ src/main/java/blackjack/controller/GameLauncher.java
@@ -1,13 +1,15 @@
 package blackjack.controller;
 
 import blackjack.domain.Card.Dealer;
+import blackjack.domain.Card.GameResult;
 import blackjack.domain.Card.Player;
 import blackjack.domain.Card.Players;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
+import java.util.List;
 
 public class GameLauncher {
-
+    private static final int DEALER_SUM_LIMIT = 16;
     private static final int BLACK_JACK_SUM_LIMIT = 21;
 
     public void start() {
@@ -18,7 +20,7 @@ public class GameLauncher {
         OutputView.printCardsSetting(playerNames);
 
         Dealer dealer = new Dealer();
-        OutputView.printDealerCardsSetting(dealer);
+        OutputView.printDealerCardsSetting(dealer, true);
 
         Players players = new Players(playerNames);
         OutputView.printPlayersStatus(players);
@@ -27,13 +29,24 @@ public class GameLauncher {
             getCardOrNot(player);
         }
 
+        if (dealer.getPlayerCardSum(dealer) < DEALER_SUM_LIMIT) {
+            dealer.addCard(dealer.getCards());
+            OutputView.printDealerAddCard();
+        }
+
+        OutputView.printDealerCardSum(dealer);
+        OutputView.printPlayerCardSum(players);
+
+        OutputView.printFinalGameResult();
+        GameResult gameResult = new GameResult(players, dealer);
+        List<Integer> gameResultList = gameResult.getGameResult();
+        OutputView.printGameResult(gameResultList, players);
     }
 
     private void getCardOrNot(Player player) {
         while (InputView.readAddCardOrNot(player) && player.getPlayerCardSum(player) < BLACK_JACK_SUM_LIMIT) {
             player.addCard(player.getCards());
             OutputView.printPlayerStatus(player);
-
         }
     }
 
--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -10,6 +10,8 @@ public class OutputView {
     private static final String REQUEST_PLAYERS_NAME = "게임에 참여할 사람의 이름을 입력하세요";
     private static final String DELIMITER = ", ";
     private static final String REQUEST_RECEIVE_CARD = "는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String DEALER_ADD_CARD = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
+    private static final String FINAL_GAME_RESULT = "## 최종 승패";
 
     private OutputView() {}
 
@@ -25,23 +27,23 @@ public class OutputView {
         System.out.println("딜러와 " + String.join(DELIMITER, players) + "에게 2장의 카드를 나누었습니다.");
     }
 
-    public static void printDealerCardsSetting(Dealer dealer) {
+    public static void printDealerCardsSetting(Dealer dealer, boolean flag) {
         System.out.print(dealer.getName() + ": ");
-        printPlayerCards(dealer.getCards());
+        printPlayerCards(dealer.getCards(), flag);
     }
 
     public static void printPlayersStatus(Players players) {
         for(Player player : players.getPlayers()) {
             System.out.print(player.getName() + "카드: ");
-            printPlayerCards(player.getCards());
+            printPlayerCards(player.getCards(), true);
         }
     }
 
-    public static void printPlayerCards(List<Card> cards) {
+    public static void printPlayerCards(List<Card> cards, boolean flag) {
         for(Card card : cards) {
             System.out.print(card.getDenomination().getName() + card.getSuit().getValue());
         }
-        System.out.println();
+        if(flag) System.out.println();
     }
 
     public static void requestAddCardOrNot(Player player) {
@@ -50,6 +52,47 @@ public class OutputView {
 
     public static void printPlayerStatus(Player player) {
         System.out.print(player.getName() + ": ");
-        printPlayerCards(player.getCards());
+        printPlayerCards(player.getCards(), true);
+    }
+
+    public static void printDealerAddCard() {
+        System.out.println(DEALER_ADD_CARD);
+        System.out.println();
+    }
+
+    public static void printDealerCardSum(Dealer dealer) {
+        printDealerCardsSetting(dealer, false);
+        System.out.println("- 결과:" + dealer.getPlayerCardSum(dealer));
+
+    }
+
+    public static void printPlayerCardSum(Players players) {
+        for (Player player : players.getPlayers()) {
+            System.out.print(player.getName() + ": ");
+            printPlayerCards(player.getCards(), false);
+            System.out.println("- 결과" + player.getPlayerCardSum(player));
+        }
+    }
+
+    public static void printFinalGameResult() {
+        System.out.println(FINAL_GAME_RESULT);
+    }
+
+    public static void printGameResult(List<Integer> gameResultList, Players players) {
+        StringBuilder stringBuilder = new StringBuilder();
+        long win = gameResultList.stream().filter(result -> result == 0).count();
+        long lose = gameResultList.stream().filter(result -> result == 1).count();
+        stringBuilder.append("딜러: ").append(win + "승").append(lose + "패");
+        System.out.println(stringBuilder);
+        // 플레이어
+        for(int i = 0; i < gameResultList.size(); i++) {
+            if(gameResultList.get(i) == 1) {
+                System.out.println(players.getPlayers().get(i).getName() + "승");
+            }
+            if(gameResultList.get(i) == 0) {
+                System.out.println(players.getPlayers().get(i).getName() + "패");
+            }
+        }
+
     }
 }
