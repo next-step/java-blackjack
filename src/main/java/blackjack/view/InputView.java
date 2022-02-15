087506a31d318b717a9d611549617629cfae8b71
xxell8 <cell8pepper@gmail.com>
2022-02-15 17:16:18 +0900

    feat: 사용자 입력을 위한 InputView 생성

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,21 +1,43 @@
 package blackjack.view;
 
+import blackjack.utils.Spliter;
+import java.util.List;
 import java.util.Scanner;
 
 public class InputView {
 
     private static final String INIT_PLAYER_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
-    private static final String CARD_DISTRIBUTE_MESSAGE = "딜러와 %s에게 2장의 나누었습니다.";
     private static final String MORE_CARD_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
+    private static final String BLANK_EXCEPTION = "1명 이상의 플레이어 이름을 입력해주세요.";
+    private static final String WRONG_ANSWER_EXCEPTION = "응답은 y 또는 n이어야 합니다.";
 
     private static Scanner scanner = new Scanner(System.in);
 
-    public void inputPlayerNames() {
+    public List<String> inputPlayerNames() {
         System.out.println(INIT_PLAYER_MESSAGE);
+        String input = scanner.nextLine();
+        checkBlank(input);
+        return Spliter.commaSplit(input);
     }
 
-    public void getUserResponse() {
+    public String getUserResponse() {
+        System.out.println(MORE_CARD_MESSAGE);
+        String input = scanner.nextLine();
+        checkResponse(input);
+        return input;
+    }
 
+    private void checkResponse(String input) {
+        checkBlank(input);
+        if (!input.contains("y") && !input.contains("n")) {
+            throw new IllegalArgumentException(WRONG_ANSWER_EXCEPTION);
+        }
+    }
+
+    private void checkBlank(String text) {
+        if (text == null || text.trim().equals("")) {
+            throw new IllegalArgumentException(BLANK_EXCEPTION);
+        }
     }
 
 }
