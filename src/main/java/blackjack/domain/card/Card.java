700860017309cd5a1daa296a1835292db962c681
0JUUU <clleo97@naver.com>
2022-02-16 01:58:43 +0900

    feat: equals & hashcode 재정의

--- src/main/java/blackjack/domain/card/Card.java
+++ src/main/java/blackjack/domain/card/Card.java
@@ -1,5 +1,7 @@
 package blackjack.domain.card;
 
+import java.util.Objects;
+
 public class Card {
     private final Pattern pattern;
     private final Rank rank;
@@ -15,5 +17,22 @@ public class Card {
 
     public boolean isAce() {
         return this.rank == Rank.ACE;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return pattern == card.pattern && rank == card.rank;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pattern, rank);
     }
 }
