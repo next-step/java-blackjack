66493b8ba3ddc90fc70949489a54b7e9adfa63c0
hochan222 <hochan049@gmail.com>
2022-02-15 18:14:20 +0900

    feat: 유효하지 않는 CarNumber 범위로 인스턴스가 생성될때, RuntimeException 발생한다.
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: chanuuuuu <dn0208@gmail.com>

--- src/main/java/blackJack/domain/CardNumber.java
+++ src/main/java/blackJack/domain/CardNumber.java
@@ -1,11 +1,18 @@
 package blackJack.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class CardNumber {
+
+    private static final List<String> CARD_NUMBER_LIST = Arrays.asList("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K");
 
     private String cardNumber;
 
     private CardNumber(String cardNumber) {
         this.cardNumber = cardNumber;
+
+        validateCardNumberRange();
     }
 
     public static CardNumber from(String cardNumber) {
@@ -14,5 +21,11 @@ public class CardNumber {
 
     public String getCardNumber() {
         return cardNumber;
+    }
+
+    private void validateCardNumberRange () {
+        if (!CARD_NUMBER_LIST.contains(cardNumber)) {
+            throw new RuntimeException("[ERROR] CardNumber 범위인 String을 입력해주세요.");
+        }
     }
 }
