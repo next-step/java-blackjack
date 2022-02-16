6d6cb04ee13ecb1b6bdba4794fc7478e2c2c2405
chanu94 <qkrcksdn0208@naver.com>
2022-02-16 17:07:16 +0900

    feat : Dealer 클래스 구현
    - Deck을 가지고 있다.
    - UserName을 가지고 있다. - name은 `dealer`라는 기본 이름을 가진다.
    
    Co-authored-by: hochan222 <hochan049@gmail.com>
    Co-authored-by: chanuuuuu <dn0208@gmail.com>

---
+++ src/main/java/blackJack/domain/Dealer.java
@@ -0,0 +1,31 @@
+package blackJack.domain;
+
+import java.util.List;
+
+public class Dealer implements User {
+
+    private final Deck deck;
+    private final UserName userName;
+    private static final String DEALER_NAME = "dealer";
+
+    private Dealer() {
+        this.deck = Deck.create();
+        this.userName = UserName.from(DEALER_NAME);
+    }
+
+    public static Dealer create() {
+        return new Dealer();
+    }
+
+    public List<Card> getDeck() {
+        return deck.getDeck();
+    }
+
+    public String getName() {
+        return userName.getName();
+    }
+
+    public void appendToDeck(Card card) {
+        deck.append(card);
+    }
+}
