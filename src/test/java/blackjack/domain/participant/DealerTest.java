7949bcd44b7d377086dc9e89ec06800915ea2d3e
0JUUU <clleo97@naver.com>
2022-02-16 21:53:52 +0900

    test: Dealer에 대한 테스트 구현
    1. 참가자의 현재 카드 총합 
    2. 카드를 더 받을 수 있는지에 대한 여부 확인

---
+++ src/test/java/blackjack/domain/participant/DealerTest.java
@@ -0,0 +1,47 @@
+package blackjack.domain.participant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Hands;
+import blackjack.domain.card.Pattern;
+import blackjack.domain.card.Rank;
+import java.util.Arrays;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class DealerTest {
+
+    @DisplayName("플레이어의 현재 점수를 가져온다.")
+    @Test
+    void Given카드들_When점수_얻기_Then점수_합_반환() {
+        final Dealer dealer = new Dealer(
+            new Hands(Arrays.asList(
+                new Card(Pattern.CLUB, Rank.EIGHT),
+                new Card(Pattern.SPADE, Rank.EIGHT))));
+
+        assertThat(dealer.getScore()).isEqualTo(16);
+    }
+
+    @DisplayName("카드를 한장 더 받을 수 있다.")
+    @Test
+    void Given카드들_When점수가_17미만_Then참_반환() {
+        final Dealer dealer = new Dealer(
+            new Hands(Arrays.asList(
+                new Card(Pattern.CLUB, Rank.EIGHT),
+                new Card(Pattern.HEART, Rank.FIVE))));
+
+        assertThat(dealer.canDraw()).isTrue();
+    }
+
+    @DisplayName("카드를 한장 더 받을 수 없다.")
+    @Test
+    void Given카드들_When점수가_17이상_Then거짓_반환() {
+        final Dealer dealer = new Dealer(
+            new Hands(Arrays.asList(
+                new Card(Pattern.CLUB, Rank.TEN),
+                new Card(Pattern.HEART, Rank.ACE))));
+
+        assertThat(dealer.canDraw()).isFalse();
+    }
+}
