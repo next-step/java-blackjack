0d26b30617482fd951bed46795dc26aefd1712ec
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-16 18:28:39 +0900

    feat: 도메인 기능 구현
    - Players
    
    Co-authored-by: judy5050 <qkrgywjd456@naver.com>
    Co-authored-by: minchul-son <smc5236@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

---
+++ src/main/java/blackjack/Players.java
@@ -0,0 +1,26 @@
+package blackjack;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Players {
+
+    private final List<Player> players;
+
+    public Players(List<Player> players) {
+        this.players = players;
+    }
+
+    public static Players create(List<String> names) {
+        return new Players(
+            names.stream()
+                .map(name -> new Player(name, new Cards(new ArrayList<>())))
+                .collect(Collectors.toList())
+        );
+    }
+
+    public void initializeDeck(Deck deck) {
+        players.stream().forEach(player -> player.initializeDeck(deck));
+    }
+}
