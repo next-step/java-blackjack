089ff30c0ee0039f037b0c1f35d1d3b6bc5c827f
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-16 18:08:01 +0900

    test(player): PlayerTest 코드 작성

---
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -0,0 +1,20 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @DisplayName("플레이어_생성_테스트")
+    @Test
+    void createPlayer() {
+        Player player = new Player("pobi", new Cards(CardDeck.pop(2)));
+        assertAll(
+            ()->assertThat(player.getName()).isEqualTo("pobi"),
+            ()->assertThat(player).isNotNull()
+        );
+    }
+}
