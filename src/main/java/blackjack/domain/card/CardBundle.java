ea267e28202fd2eb9217397f6dde9ef78eda03c0
PandaHun <gnsgnsek@gmail.com>
2022-02-16 17:21:50 +0900

    test(CardBundle): Testing Cardbundle calculate sum of cards

--- src/main/java/blackjack/domain/card/CardBundle.java
+++ src/main/java/blackjack/domain/card/CardBundle.java
@@ -1,23 +1,25 @@
 package blackjack.domain.card;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class CardBundle {
 
-    public List<Card> createCards() {
-        List<Card> cards = new ArrayList<>();
-        for (Suit suit : Suit.values()) {
-            createBySuit(cards, suit);
-        }
-        Collections.shuffle(cards);
-        return cards;
+    private final List<Card> cards;
+
+    private CardBundle() {
+        this.cards = new ArrayList<>();
     }
 
-    private void createBySuit(List<Card> cards, Suit suit) {
-        for (Number number : Number.values()) {
-            cards.add(new Card(suit, number));
-        }
+    public static CardBundle emptyBundle() {
+        return null;
+    }
+
+    public void addCard(Card card) {
+        cards.add(card);
+    }
+
+    public int calculateScore() {
+        return 0;
     }
 }
