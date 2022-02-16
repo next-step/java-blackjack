4d55fdbbe7e2acdc9e595c652d8b4654b509fec6
eohaerim <ehl3288@naver.com>
2022-02-17 01:12:16 +0900

    refactor: 생성 시점에 카드를 전달 받도록 변경

--- src/main/java/blackjack/domain/person/Player.java
+++ src/main/java/blackjack/domain/person/Player.java
@@ -1,13 +1,15 @@
 package blackjack.domain.person;
 
 import blackjack.domain.card.Card;
+import java.util.List;
 
 public class Player extends Person {
 
     private final String name;
 
-    public Player(String name) {
+    public Player(String name, List<Card> cards) {
         this.name = name;
+        this.cards.addAll(cards);
     }
 
     public String getName() {
--- src/test/java/blackjack/domain/person/PlayerTest.java
+++ src/test/java/blackjack/domain/person/PlayerTest.java
@@ -5,26 +5,33 @@ import static org.assertj.core.api.Assertions.assertThat;
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Denomination;
 import blackjack.domain.card.Shape;
+import java.util.Arrays;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
 
     @Test
-    void 플레이어를_생성하면_이름을_가져야_한다() {
+    void 플레이어를_생성하면_카드를_두장_가져야_한다() {
         //given, when
-        Player player = new Player("kim");
+        Player player = new Player("kim", Arrays.asList(
+            new Card(Shape.SPADE, Denomination.KING),
+            new Card(Shape.SPADE, Denomination.KING)
+        ));
 
         //then
-        assertThat(player.getName()).isEqualTo("kim");
+        assertThat(player.getCards()).hasSize(2);
     }
 
     @Test
     void 플레이어는_받은_카드_1장을_목록에_추가해야_한다() {
         //given
-        Player player = new Player("eo");
+        Player player = new Player("kim", Arrays.asList(
+            new Card(Shape.SPADE, Denomination.KING),
+            new Card(Shape.SPADE, Denomination.KING)
+        ));
         player.addCard(new Card(Shape.HEART, Denomination.KING));
 
         //when then
-        assertThat(player.getCards()).hasSize(1);
+        assertThat(player.getCards()).hasSize(3);
     }
 }
