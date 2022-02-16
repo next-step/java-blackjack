f86f7d455bc64001b9b3d920fd02c8ad67cddda9
sk11ingyuhwang <sk11ingyuhwang@gmail.com>
2022-02-17 01:06:48 +0900

    refactor(main): Controller new 연산자로 생성

--- src/main/java/blackjack/Application.java
+++ src/main/java/blackjack/Application.java
@@ -5,6 +5,7 @@ import blackjack.controller.Controller;
 public class Application {
 
     public static void main(String[] args) {
-        Controller.run();
+        Controller controller = new Controller();
+        controller.run();
     }
 }
