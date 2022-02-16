3b8a0064c8dac0e8df588906395c3493d8b44481
PandaHun <gnsgnsek@gmail.com>
2022-02-16 17:21:02 +0900

    typo: Apply rename CardBundle to CardFactory

--- src/test/java/blackjack/domain/card/DeckTest.java
+++ src/test/java/blackjack/domain/card/DeckTest.java
@@ -7,16 +7,16 @@ import org.junit.jupiter.api.Test;
 class DeckTest {
 
     @Test
-    void 카드번들을_제대로_받는지_테스트() {
-        CardBundle cardBundle = new CardBundle();
-        Deck deck = new Deck(cardBundle.createCards());
+    void 카드뭉치를_제대로_받는지_테스트() {
+        CardFactory cardFactory = CardFactory.getInstance();
+        Deck deck = new Deck(cardFactory.createCards());
         assertThat(deck.remainCardSize()).isEqualTo(52);
     }
 
     @Test
     void 카드를_한장_뽑아준다() {
-        CardBundle cardBundle = new CardBundle();
-        Deck deck = new Deck(cardBundle.createCards());
+        CardFactory cardFactory = CardFactory.getInstance();
+        Deck deck = new Deck(cardFactory.createCards());
         deck.spreadCard();
         assertThat(deck.remainCardSize()).isEqualTo(51);
     }
