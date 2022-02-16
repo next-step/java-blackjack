4a7d92bb1256b146049ad5b1eae66eb588359684
xxell-8 <cell8pepper@gmail.com>
2022-02-16 19:42:55 +0900

    feat: 새로운 딜러를 생성하는 팩토리 메서드 추가

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -1,10 +1,17 @@
 package blackjack.domain;
 
+import java.util.ArrayList;
+
 public class Dealer extends Participant {
     private static final int LIMIT_MORE_CARD = 16;
 
     public Dealer(Cards cards) {
         super(cards);
+    }
+
+    public static Dealer create() {
+        Cards cards = new Cards(new ArrayList<>());
+        return new Dealer(cards);
     }
 
     public boolean checkHitOrNot() {
