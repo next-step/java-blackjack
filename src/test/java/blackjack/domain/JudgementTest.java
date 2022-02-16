0b6eb9afb66051f4b20e821b3923e62a7ab4790b
Jiin Lee <loveted0225@naver.com>
2022-02-16 22:36:01 +0900

    test(JudgementTest): JudgeMent 테스트 코드 작성
    - testFindWinner

--- src/test/java/blackjack/domain/JudgementTest.java
+++ src/test/java/blackjack/domain/JudgementTest.java
@@ -12,20 +12,9 @@ class JudgementTest {
     @DisplayName("플레이어와 딜러의 결과값을 가지고 승자를 찾는다")
     @Test
     void testFindWinner() {
-        Player player1 = new Player("a");
-        Player player2 = new Player("b");
-        Dealer dealer = new Dealer();
-        List<Player> players = Arrays.asList(player1, player2, dealer);
-        player1.receiveCard(new Card(Suit.DIAMONDS, Denomination.ACE));
-        player1.receiveCard(new Card(Suit.SPADES, Denomination.NINE));  // 20
-        player2.receiveCard(new Card(Suit.SPADES, Denomination.FIVE));
-        player2.receiveCard(new Card(Suit.CLOVERS, Denomination.SEVEN));
-        player2.receiveCard(new Card(Suit.SPADES, Denomination.NINE));   // 21
-        dealer.receiveCard(new Card(Suit.SPADES, Denomination.KING));
-        dealer.receiveCard(new Card(Suit.SPADES, Denomination.SIX));    // 16
-
-        Judgement judgement = new Judgement(players);
-        List<String> winners = judgement.findWinners();
-        assertThat(winners).containsExactly("b");
+        Map<String, String> playerResults = judgement.findWinners();
+        assertThat(playerResults.get("a")).isEqualTo("승");
+        assertThat(playerResults.get("b")).isEqualTo("승");
+        assertThat(playerResults.get("딜러")).isEqualTo("0승 2패");
     }
 }
