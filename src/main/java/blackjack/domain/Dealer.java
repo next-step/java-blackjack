75b59e2311832e39ad475074cd05d60447008487
Jiin Lee <loveted0225@naver.com>
2022-02-16 17:50:08 +0900

    feat: Dealer의 카드 합이 16이하일 때 카드를 추가하는 메소드 구현
    -addOneMoreCard

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -2,17 +2,19 @@ package blackjack.domain;
 
 public class Dealer extends Player {
 
+    private static final int SCORE_THRESHOLD = 16;
     private static final String DEALER = "딜러";
 
     private final Deck deck;
+
     public Dealer() {
         super(DEALER);
-        this.deck= new Deck();
+        this.deck = new Deck();
     }
 
     public void allocateCard(Player player) {
         Card popped = deck.popCard();
-        player.receiveCard (popped);
+        player.receiveCard(popped);
     }
 
     public void addOneMoreCard() {
