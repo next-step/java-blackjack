87b93b3117f4efa75a475d36a39ba987c3b833a0
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-16 18:06:44 +0900

    feat: CardShuffler에서의 메서드 분리
    - symbols에서 랜덤 symbol을 뽑는 메서드와
    - 해당 symbol에서 랜덤 cardIndex를 뽑는 메서드

--- src/main/java/blackjack/util/CardShuffler.java
+++ src/main/java/blackjack/util/CardShuffler.java
@@ -1,12 +1,18 @@
 package blackjack.util;
 
+import java.util.List;
 import java.util.Random;
 
 public class CardShuffler {
 
     private static final Random random = new Random();
 
-    public static int pickIndexIn(int bound) {
+    public static String pickSymbolIn(List<String> symbols) {
+        int pickIndex = random.nextInt(symbols.size());
+        return symbols.get(pickIndex);
+    }
+
+    public static int pickCardIndexIn(int bound) {
         return random.nextInt(bound);
     }
 }
