24226e44a73940eb88f30dd68414dab6a68191a4
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-17 12:13:28 +0900

    feat: PlayerTest 내부 구현 로직 변경
    - 특정 카드를 받았을 때 그 카드의 점수만큼 정확하게 계산한다.
    - 플레이어가 Ace카드를 포함하고 있을 때 최선의 방법으로 결과값을 반환하는지 검증한다.

--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -3,19 +3,37 @@ package blackjack.domain;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import blackjack.domain.card.Card;
+import blackjack.domain.card.CardSymbol;
+import blackjack.domain.card.CardType;
 import blackjack.domain.gameplayer.GamePlayer;
+import blackjack.domain.gameplayer.Name;
+import blackjack.domain.gameplayer.Player;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
 
     @Test
-    public void Ace를_받았을때_합이_21을_넘을경우_1을_더해준다() {
+    public void 특정카드를_받았을때_그만큼의_점수를_더해준다() {
         //given
-        GamePlayer player = new GamePlayer("pobi", 15, false);
+        GamePlayer player = new Player(new Name("pobi"));
 
         //when
-        player.receiveCard(new Card("스페이드", "A", 11, 1));
+        player.receiveCard(new Card(CardSymbol.CLOVER, CardType.FIVE));
 
+        //then
+        assertThat(player.getScore()).isEqualTo(5);
+    }
+
+    @Test
+    public void Ace를_받았을때_합이_21을_넘을경우_1을_더해준다() {
+        //given
+        GamePlayer player = new Player(new Name("pobi"));
+
+        //when
+        player.receiveCard(new Card(CardSymbol.CLOVER, CardType.FIVE));
+        player.receiveCard(new Card(CardSymbol.CLOVER, CardType.FIVE));
+        player.receiveCard(new Card(CardSymbol.CLOVER, CardType.FIVE));
+        player.receiveCard(new Card(CardSymbol.CLOVER, CardType.ACE));
         //then
         assertThat(player.getScore()).isEqualTo(16);
     }
@@ -23,12 +41,14 @@ class PlayerTest {
     @Test
     public void Ace를_받았을때_합이_21을_넘지않을경우_11을_더해준다() {
         //given
-        GamePlayer player = new GamePlayer("pobi", 8, false);
+        GamePlayer player = new Player(new Name("pobi"));
 
         //when
-        player.receiveCard(new Card("스페이드", "A", 11, 1));
+        player.receiveCard(new Card(CardSymbol.CLOVER, CardType.FIVE));
+        player.receiveCard(new Card(CardSymbol.CLOVER, CardType.FIVE));
+        player.receiveCard(new Card(CardSymbol.CLOVER, CardType.ACE));
 
         //then
-        assertThat(player.getScore()).isEqualTo(19);
+        assertThat(player.getScore()).isEqualTo(21);
     }
 }
