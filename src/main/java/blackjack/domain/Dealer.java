316a51054ad901617bdadda4a8f62e52768c9460
parkhyojeong <qkrgywjd456@naver.com>
2022-02-17 21:48:56 +0900

    feat: Dealer CardInfo 반환 기능 구현
    Co-authored-by: judy5050 <qkrgywjd456@naver.com>
    Co-authored-by: minchul-son <smc5236@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>

--- src/main/java/blackjack/domain/Dealer.java
+++ src/main/java/blackjack/domain/Dealer.java
@@ -1,15 +1,29 @@
 package blackjack.domain;
 
+import blackjack.dto.CardInfo;
+import java.util.ArrayList;
+
 public class Dealer extends Person {
 
     private static final int DEALER_DRAW_LIMIT = 16;
+    private static final String DEALER_NAME = "딜러";
 
-    public Dealer(String userName,Cards cards) {
-        super(userName,cards);
+    public Dealer(String userName, Cards cards) {
+        super(userName, cards);
+    }
+
+    public Dealer() {
+        this(DEALER_NAME, new Cards(new ArrayList<>()));
     }
 
     @Override
     public boolean canDrawCard() {
         return cards.totalScore() <= DEALER_DRAW_LIMIT;
     }
+
+    @Override
+    public CardInfo openCards() {
+        return new CardInfo(userName,cards.openCardOne());
+    }
+
 }
