9458008fb049d84792a0245f1d14b17780b33cf0
한승진 <gkstmdwls100@naver.com>
2022-02-17 18:39:18 +0900

    feat: 딜러, 참가자 모두를 관리하는 클래스
    - 덱 생성
    - 참가자 초기화
    - 현재 참가자 상태 저장

--- src/main/java/blackjack/domain/Participants.java
+++ src/main/java/blackjack/domain/Participants.java
@@ -3,29 +3,66 @@ package blackjack.domain;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.stream.Collectors;
 
-public class Players {
+public class Participants {
 
-    private final List<Participant> players;
+    private final List<Participant> participants;
+    private final Deck deck;
+    private int targetIndex;
 
-    private Players(List<Participant> players) {
-        this.players = new ArrayList<>(players);
+    private Participants(List<Participant> participants) {
+        this.deck = Deck.create();
+        this.participants = new ArrayList<>(participants);
+        this.targetIndex = 0;
     }
 
-    public static Players from(final List<String> names) {
-        return new Players(names.stream()
-            .map(Player::new)
-            .collect(Collectors.toList()));
+    public static Participants from(Players players, Dealer dealer) {
+        List<Participant> participants = new ArrayList<>(players.getPlayers());
+        participants.add(dealer);
+        return new Participants(participants);
     }
 
-    public void drawCardMultiple(Deck deck, int number) {
-        for (Participant player : players) {
-            player.drawCardMultiple(deck, number);
+    public void drawCardMultiple(int number) {
+        for (Participant participant : participants) {
+            participant.drawCardMultiple(deck, number);
         }
     }
 
-    public List<Participant> getPlayers() {
-        return Collections.unmodifiableList(players);
+    public void drawCard(Participant participant) {
+
+    }
+
+    public void judgeScore(){
+        int maxScore = calculateMaxScore();
+        for(Participant participant : participants){
+            participant.judgeScore(maxScore);
+        }
+    }
+
+    private int calculateMaxScore(){
+        return participants.stream()
+            .mapToInt(participant -> participant.sumCardScore())
+            .max()
+            .orElseThrow(() -> {
+                throw new IllegalStateException("최대값을 구할 수 없습니다.");
+            });
+    }
+
+    public boolean isNext(){
+        return targetIndex > participants.size();
+    }
+
+    public void nextTarget(){
+        if(!isNext())
+            return;
+        targetIndex ++;
+    }
+
+    public Participant getTarget() {
+        return participants.get(targetIndex);
+    }
+
+    public List<Participant> getParticipants() {
+        return Collections.unmodifiableList(participants);
     }
 }
