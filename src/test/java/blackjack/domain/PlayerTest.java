6c7cb0c59e59338c2a2c9c951ba010452f417b07
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-17 17:27:51 +0900

    feat: GamePlayer의 구현체 단위테스트 작성
    - 게임플레이어가 카드를 더 받을 수 있는 조건은 일반플레이어와 딜러플레이어가 다르다
    - 게임플리어와 딜러플레이어의 최종승패 출력 방식은 다르다

--- src/test/java/blackjack/domain/PlayerTest.java
+++ src/test/java/blackjack/domain/PlayerTest.java
@@ -6,8 +6,12 @@ import blackjack.domain.card.Card;
 import blackjack.domain.card.CardSymbol;
 import blackjack.domain.card.CardType;
 import blackjack.domain.gameplayer.GamePlayer;
+import blackjack.domain.gameplayer.GamePlayers;
 import blackjack.domain.gameplayer.Name;
+import blackjack.domain.gameplayer.Names;
 import blackjack.domain.gameplayer.Player;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 
 class PlayerTest {
@@ -50,5 +54,57 @@ class PlayerTest {
 
         //then
         assertThat(player.getScore()).isEqualTo(21);
+    }
+
+    @Test
+    public void 플레이어는_카드의합이_21이하면_카드를_받을수있는_조건이_된다() {
+        //given
+        GamePlayers gamePlayers = GamePlayers.makePlayers(new Names(
+            Arrays.asList(new Name("pobi"))));
+
+        List<GamePlayer> players = gamePlayers.getPlayers();
+        GamePlayer player = players.get(0);
+
+        //when
+        player.receiveCard(new Card(CardSymbol.DIAMOND, CardType.NINE));
+        player.receiveCard(new Card(CardSymbol.DIAMOND, CardType.TEN));
+
+        //then
+        assertThat(player.isLowerThanBound()).isTrue();
+    }
+
+    @Test
+    public void 딜러는_카드의합이_21초과면_카드를_받을수없다() {
+        //given
+        GamePlayers gamePlayers = GamePlayers.makePlayers(new Names(
+            Arrays.asList(new Name("pobi"))));
+
+        List<GamePlayer> players = gamePlayers.getPlayers();
+        GamePlayer player = players.get(0);
+
+        //when
+        player.receiveCard(new Card(CardSymbol.DIAMOND, CardType.NINE));
+        player.receiveCard(new Card(CardSymbol.DIAMOND, CardType.TEN));
+        player.receiveCard(new Card(CardSymbol.DIAMOND, CardType.EIGHT));
+
+        //then
+        assertThat(player.isLowerThanBound()).isFalse();
+    }
+
+    @Test
+    public void 일반플레이어의_최종승패는_올바르게_계산된다() {
+        //given
+        GamePlayers gamePlayers = GamePlayers.makePlayers(new Names(Arrays.asList(new Name("pobi"), new Name("jason"))));
+        List<GamePlayer> players = gamePlayers.getPlayers();
+        players.get(0).receiveCard(new Card(CardSymbol.CLOVER, CardType.NINE));
+        players.get(1).receiveCard(new Card(CardSymbol.CLOVER, CardType.FOUR));
+
+        //when
+        String pobiResult = players.get(0).getGameResult(gamePlayers);
+        String jasonResult = players.get(1).getGameResult(gamePlayers);
+
+        //then
+        assertThat(pobiResult).isEqualTo("승");
+        assertThat(jasonResult).isEqualTo("패");
     }
 }
