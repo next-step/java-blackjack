ebf319eb05ef429eb20109c353083dc674471352
YangWoo Yu <yuyw0712@naver.com>
2022-02-17 12:09:38 +0900

    test: 모든 카드는 52장이다.
    Co-authored-by: seungyeonjeong <crowsy@naver.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/test/java/blackjack/domain/CardsTest.java
+++ src/test/java/blackjack/domain/CardsTest.java
@@ -1,5 +1,7 @@
 package blackjack.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import blackjack.domain.card.Card;
 import blackjack.domain.card.Denomination;
 import blackjack.domain.card.Suit;
@@ -10,13 +12,13 @@ import org.junit.jupiter.api.Test;
 class CardsTest {
 
     @Test
-     void 모든_카드를_출력한다() {
+     void 모든_카드는_52장이다() {
         List<Card> cardList = new ArrayList<>();
         for(Denomination denomination : Denomination.values()) {
             for(Suit suit : Suit.values()) {
                 cardList.add(new Card(denomination, suit));
             }
         }
-        cardList.forEach(card -> System.out.println(new StringBuilder().append(card.getDenomination().getName()).append(card.getSuit().getValue())));
+        assertThat(cardList.size()).isEqualTo(52);
     }
 }
