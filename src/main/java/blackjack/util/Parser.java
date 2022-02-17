b2e2f123a6703ae310c90b065be77c8f0a99efc5
gal2o <hunmin8788@gmail.com>
2022-02-18 00:34:29 +0900

    feat(Blackjack): 처음 딜러, 플레이어 생성하는 InputView, Parser 생성

--- src/main/java/blackjack/util/Parser.java
+++ src/main/java/blackjack/util/Parser.java
@@ -1,5 +1,18 @@
 package blackjack.util;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Parser {
 
+    private static final String DELIMITER = ",";
+    private static final String SPACE = " ";
+    private static final String NOT_SPACE = "";
+
+    public static List<String> parseInput(String input) {
+        String[] splitInput = input.replace(SPACE, NOT_SPACE).split(DELIMITER);
+
+        return Arrays.stream(splitInput).collect(Collectors.toList());
+    }
 }
