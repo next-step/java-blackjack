cdadf6db79c1b50de25855edda925d9b49a2720d
gal2o <hunmin8788@gmail.com>
2022-02-18 00:07:59 +0900

    feat(Blackjack): 블랙잭 게임을 하는 플레이어 추상 클래스 생성

--- src/main/java/blackjack/domain/GamePlayers.java
+++ src/main/java/blackjack/domain/GamePlayers.java
@@ -2,4 +2,26 @@ package blackjack.domain;
 
 public abstract class GamePlayers {
 
+    protected final String playerName;
+    protected final OwnCards ownCards;
+
+    protected GamePlayers(String playerName) {
+        this.playerName = playerName;
+        this.ownCards = new OwnCards();
+    }
+
+    protected GamePlayers(String playerName, OwnCards ownCards) {
+        this.playerName = playerName;
+        this.ownCards = ownCards;
+    }
+
+    public abstract boolean isAvailDraw();
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public OwnCards myOwnCards() {
+        return ownCards;
+    }
 }
