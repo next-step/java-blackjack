ffb0e10a6e4a7f74396ca5bd8bd1579fcc9f40cb
HyeonjuLee <l67378810@gmail.com>
2022-02-18 00:25:16 +0900

    refactor(game): game 기능 구현 && test(game)

--- src/main/java/blackjack/domain/Game.java
+++ src/main/java/blackjack/domain/Game.java
@@ -1,5 +1,9 @@
 package blackjack.domain;
 
+import blackjack.domain.card.CardDeck;
+import blackjack.domain.player.Dealer;
+import blackjack.domain.player.Player;
+import blackjack.domain.state.State;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -8,6 +12,8 @@ public class Game {
 
     private static final int PASS_CARD_NUMBER = 2;
     private static final String DEALER_NAME = "딜러";
+    private final static int DEALER_THRESHOLD = 16;
+
     private final List<Player> players;
     private final Dealer dealer;
 
@@ -27,7 +33,25 @@ public class Game {
     }
 
     private static State handOutCards() {
-        return new State(CardDeck.pop(PASS_CARD_NUMBER));
+        return new State(CardDeck.pop(PASS_CARD_NUMBER), true);
+    }
+
+    public boolean giveCardToDealer() {
+        if (dealer.getCards().cards().sumScore() <= DEALER_THRESHOLD) {
+            dealer.getCards().addCard(CardDeck.pop());
+            return true;
+        }
+        return false;
+    }
+
+    public int getTotalScoreOfPlayer(Player player) {
+        return player.getCards().cards().sumScore();
+    }
+
+    public List<Integer> getScoresOfPlayers() {
+        return players.stream()
+            .mapToInt(this::getTotalScoreOfPlayer)
+            .boxed().collect(Collectors.toList());
     }
 
     public List<Player> getPlayers() {
@@ -36,9 +60,5 @@ public class Game {
 
     public Dealer getDealer() {
         return dealer;
-    }
-
-    public void addCard(Player player) {
-        player.getCards().addCard(CardDeck.pop());
     }
 }
--- src/test/java/blackjack/domain/GameTest.java
+++ src/test/java/blackjack/domain/GameTest.java
@@ -1,7 +1,7 @@
 package blackjack.domain;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,8 +19,18 @@ class GameTest {
             .collect(Collectors.toList());
         Game game = new Game(playerNames);
         assertAll(
-            ()->assertThat(game.getPlayers()).isNotNull(),
-            ()->assertThat(game.getDealer()).isNotNull()
+            () -> assertThat(game.getPlayers()).isNotNull(),
+            () -> assertThat(game.getDealer()).isNotNull()
         );
+    }
+
+    @DisplayName("가진 카드가 16 이하일 때 딜러에게 카드 전달 테스트")
+    @Test
+    void giveCardToDealerTest() {
+        List<String> playerNames = Arrays.stream("pobi, jason".split(","))
+            .map(String::trim)
+            .collect(Collectors.toList());
+        Game game = new Game(playerNames);
+        assertThat(game.giveCardToDealer()).isNotNull(); // Fix
     }
 }
