8de7a716aa32aa8e345c9291e6f51030e7ac4f54
gal2o <hunmin8788@gmail.com>
2022-02-18 00:37:04 +0900

    feat(Blackjack): 처음 게임 시작에 필요한 ResultView-inputPlayersPrint 메서드 생성

--- src/main/java/blackjack/controller/BlackJackController.java
+++ src/main/java/blackjack/controller/BlackJackController.java
@@ -1,5 +1,19 @@
 package blackjack.controller;
 
+import blackjack.domain.Dealer;
+import blackjack.domain.Deck;
+import blackjack.domain.Players;
+import blackjack.view.InputView;
+import blackjack.view.ResultView;
+
 public class BlackJackController {
 
+    public static void playBlackJack() {
+        Deck deck = Deck.createDeck();
+        Dealer dealer = new Dealer();
+        Players players = Players.createPlayers(InputView.getPlayers());
+
+        ResultView resultView = new ResultView(dealer, players);
+        resultView.inputPlayersPrint();
+    }
 }
--- src/main/java/blackjack/view/ResultView.java
+++ src/main/java/blackjack/view/ResultView.java
@@ -1,5 +1,29 @@
 package blackjack.view;
 
+import blackjack.domain.Dealer;
+import blackjack.domain.GamePlayers;
+import blackjack.domain.Players;
+import java.util.stream.Collectors;
+
 public class ResultView {
+
+    private static String DELIMETER = ", ";
+    private static String INIT_DISPENSER_MESSAGE = "에게 2장의 카드를 나누었습니다.";
+
+    private final Dealer dealer;
+    private final Players players;
+
+    public ResultView(Dealer dealer, Players players) {
+        this.dealer = dealer;
+        this.players = players;
+    }
+
+    public void inputPlayersPrint() {
+        String resultPlayers = dealer.getPlayerName() + DELIMETER + players.getPlayers().stream()
+            .map(GamePlayers::getPlayerName).collect(Collectors.joining(DELIMETER))
+            + INIT_DISPENSER_MESSAGE;
+
+        System.out.println(resultPlayers);
+    }
 
 }
