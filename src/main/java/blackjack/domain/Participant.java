412aa73313c2f91134436af89ff17a0ba94644c8
한승진 <gkstmdwls100@naver.com>
2022-02-17 18:34:48 +0900

    feat: 승, 패 점수 계산 메서드 추가

--- src/main/java/blackjack/domain/Participant.java
+++ src/main/java/blackjack/domain/Participant.java
@@ -6,17 +6,25 @@ abstract public class Participant {
 
     protected final String name;
     protected final ParticipantCards cards;
-    protected final int winCount, loseCount;
+    protected int winScore, loseScore;
 
     public Participant(String name, ParticipantCards cards, int winCount, int loseCount) {
         this.name = name;
         this.cards = cards;
-        this.winCount = winCount;
-        this.loseCount = loseCount;
+        this.winScore = winCount;
+        this.loseScore = loseCount;
     }
 
     public Participant(String name) {
         this(name, new ParticipantCards(new ArrayList<>()), 0, 0);
+    }
+
+    public final void judgeScore(int maxScore) {
+        if(sumCardScore() == maxScore){
+            winScore++;
+            return;
+        }
+        loseScore++;
     }
 
     public final int sumCardScore() {
