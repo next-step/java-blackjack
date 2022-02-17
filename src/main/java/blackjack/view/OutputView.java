48e99494a449c93c8ad3a741d36de202fad85511
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-17 11:12:22 +0900

    feat: OutputView에서 공통코드 메서드 분리
    및 요구사항대로 뷰를 출력하기 위한 리팩터링

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -7,19 +7,30 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
+    private static final String ENTER = "\n";
     private static final String GAME_INITIAL_MANAGER = "%s에게 2장의 카드를 나누었습니다.";
     private static final String COMMA = ", ";
     private static final String CARDS_LOG = "%s: %s";
-    private static final String DEALER_RECEIVE_ONE_CARD_MESSAGE = "딜러는 한장의 카드를 더 받았습니다.";
+    private static final String DEALER_RECEIVE_ONE_CARD_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.";
     private static final String RESULT_CARDS_LOG = "%s카드: %s - 결과: %d";
     private static final String RESULT_GAME_LOG = "%s: %s";
 
-    public static void printInitialMessage(List<GamePlayer> players) {
+    public static void printInitialMessage(GamePlayers gamePlayers) {
+        List<GamePlayer> players = gamePlayers.getAllPlayers();
+
         String playerNames = players.stream()
             .map(GamePlayer::getName)
             .collect(Collectors.joining(COMMA));
 
-        System.out.println(String.format(GAME_INITIAL_MANAGER, playerNames));
+        System.out.println(ENTER + String.format(GAME_INITIAL_MANAGER, playerNames));
+    }
+
+    public static void printCardAllStatus(GamePlayers gamePlayers) {
+        List<GamePlayer> players = gamePlayers.getAllPlayers();
+
+        players.stream()
+                .forEach(player -> System.out.println(String.format(CARDS_LOG, player.getName(), getPlayerCardStatus(player))));
+        System.out.println();
     }
 
     public static void printCardStatus(GamePlayer player) {
@@ -31,11 +42,13 @@ public class OutputView {
     }
 
     public static void printCardResult(GamePlayers gamePlayers) {
+        System.out.println();
         List<GamePlayer> players = gamePlayers.getAllPlayers();
 
         for (GamePlayer player : players) {
             System.out.println(String.format(RESULT_CARDS_LOG, player.getName(), getPlayerCardStatus(player), player.getScore()));
         }
+        System.out.println();
     }
 
     private static String getPlayerCardStatus(GamePlayer gamePlayer) {
