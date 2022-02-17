1193866a663af5aaae5795ca1e9a2c73473d5cb8
G1.Jeon <g1.tommy.jeon@gmail.com>
2022-02-18 00:18:01 +0900

    feat: View
    - InputView
    - ResultView
    
    Co-authored-by: judy5050 <qkrgywjd456@naver.com>
    Co-authored-by: minchul-son <smc5236@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>"

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -1,5 +1,7 @@
 package blackjack.view;
 
+import blackjack.dto.NameInfo;
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
@@ -10,12 +12,45 @@ public class InputView {
     private static final Scanner SCANNER;
     private static final String SPLITTER = ",";
     private static final String MESSAGE_GET_NAMES = "게임에 참여할 사람의 이름을 입력하세요.(쉼표 기준으로 분리)";
+    private static final String MESSAGE_UNEXPECTABLE_INPUT = "의도치 않은 입력이 들어왔습니다.";
+    private static final String MESSAGE_CHOICE_DECISION = "%s는 한장의 카드를 더 받겠습니까?(예는 %s, 아니오는 %s)\n";
+    private static final String ANSWER_YES = "y";
+    private static final String ANSWER_NO = "n";
 
     static {
         SCANNER = new Scanner(System.in);
     }
 
-    private InputView() {
+    enum Answer {
+        YES(ANSWER_YES, true),
+        NO(ANSWER_NO, false);
+
+        private final String choice;
+
+        private final boolean decision;
+        Answer(String choice, boolean decision) {
+            this.choice = choice;
+            this.decision = decision;
+        }
+
+        public static Answer of(final String choice) {
+            return Arrays.stream(values())
+                .filter(answer -> answer.choice.equals(choice))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException(MESSAGE_UNEXPECTABLE_INPUT));
+        }
+
+        public boolean getDecision() {
+            return decision;
+        }
+
+    }
+
+    private InputView() {}
+
+    public static boolean drawChoice(NameInfo activePlayerNameInfo) {
+        System.out.format(MESSAGE_CHOICE_DECISION, activePlayerNameInfo.getPersonName(), Answer.YES.choice, Answer.NO.choice);
+        return Answer.of(SCANNER.nextLine()).getDecision();
     }
 
     public static List<String> getNames() {
@@ -25,4 +60,5 @@ public class InputView {
             .map(String::trim)
             .collect(Collectors.toList());
     }
+
 }
--- src/main/java/blackjack/view/ResultView.java
+++ src/main/java/blackjack/view/ResultView.java
@@ -3,7 +3,9 @@ package blackjack.view;
 import static blackjack.domain.Person.INIT_CARD_COUNT;
 
 import blackjack.dto.CardInfo;
+import blackjack.dto.MatchInfo;
 import blackjack.dto.NameInfo;
+import blackjack.dto.ScoreInfo;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -11,8 +13,12 @@ public class ResultView {
 
     private static final String INIT_SHARE_MESSAGE = "%s와 %s에게 %d장을 받았습니다.";
     private static final String DELIMITER = ", ";
-    private static final String DELETER_FORMAT = "%s: %s";
+    private static final String DEALER_FORMAT = "%s: %s";
     private static final String PLAYER_FORMAT = "%s카드: %s";
+    private static final String SCOREBOARD_FORMAT = "%s 카드: %s - 결과: %s\n";
+    private static final String MATCH_RESULT_MESSAGE = "## 최종 승패";
+    private static final String DEALER_DRAW_FORMAT = "%s는 %s이하라 한장의 카드를 더 받았습니다.\n";
+    private static final String MATCH_RESULT_FORMAT = "%s: %s\n";
 
     public static void shareCards(String dealerName, List<NameInfo> playerNames) {
         String playersName = playerNames.stream()
@@ -22,7 +28,7 @@ public class ResultView {
     }
 
     public static void openCardInfo(CardInfo dealerCardInfo, List<CardInfo> playerCardsInfo) {
-        System.out.println(String.format(DELETER_FORMAT, dealerCardInfo.getPersonName(), join(dealerCardInfo)));
+        System.out.println(String.format(DEALER_FORMAT, dealerCardInfo.getPersonName(), join(dealerCardInfo)));
 
         for (CardInfo playerCardInfo : playerCardsInfo) {
             System.out.println(String.format(PLAYER_FORMAT, playerCardInfo.getPersonName(), join(playerCardInfo)));
@@ -34,5 +40,39 @@ public class ResultView {
             .collect(Collectors.joining(DELIMITER));
     }
 
+    public static void playerCardsInfo(CardInfo activePlayerCardsInfo) {
+        System.out.println(String.format(PLAYER_FORMAT, activePlayerCardsInfo.getPersonName(), join(activePlayerCardsInfo)));
+    }
 
+    public static void dealerDrawDecision(NameInfo dealerNameInfo, int limit) {
+        System.out.format(DEALER_DRAW_FORMAT, dealerNameInfo.getPersonName(), limit);
+    }
+
+    public static void margin() {
+        System.out.println();
+    }
+
+    public static void scoreboard(List<ScoreInfo> scoreInfo) {
+        scoreInfo.forEach(ResultView::scoreboard);
+        margin();
+    }
+
+    public static void scoreboard(ScoreInfo scoreInfo) {
+        System.out.format(SCOREBOARD_FORMAT, scoreInfo.getName(), scoreInfo.getCardNames().stream().collect(
+            Collectors.joining(DELIMITER)), scoreInfo.getScore());
+    }
+
+    public static void matchResult(MatchInfo dealerMatchInfo, List<MatchInfo> playersMatchInfo) {
+        System.out.println(MATCH_RESULT_MESSAGE);
+        printMatchResult(dealerMatchInfo);
+        printMatchResult(playersMatchInfo);
+    }
+
+    private static void printMatchResult(List<MatchInfo> playersMatchInfo) {
+        playersMatchInfo.forEach(ResultView::printMatchResult);
+    }
+
+    private static void printMatchResult(MatchInfo matchInfo) {
+        System.out.format(MATCH_RESULT_FORMAT, matchInfo.getName(), matchInfo.getMatches());
+    }
 }
