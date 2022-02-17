d6aa26a69ab0c35bbfd598fdd5d402be884bee43
eohaerim <ehl3288@naver.com>
2022-02-17 17:16:36 +0900

    refactor: setUp 활용

--- src/test/java/blackjack/domain/person/DealerTest.java
+++ src/test/java/blackjack/domain/person/DealerTest.java
@@ -6,15 +6,20 @@ import blackjack.domain.card.Card;
 import blackjack.domain.card.Denomination;
 import blackjack.domain.card.Shape;
 import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class DealerTest {
 
+    private Dealer dealer;
+
+    @BeforeEach
+    void setUp() {
+        dealer = new Dealer();
+    }
+
     @Test
     void 딜러가_생성되면_카드를_두장_가지고_있어야_한다() {
-        //given
-        Dealer dealer = new Dealer();
-
         //when then
         assertThat(dealer.getCards()).hasSize(2);
     }
@@ -22,7 +27,6 @@ class DealerTest {
     @Test
     void 딜러는_받은_카드_1장을_목록에_추가해야_한다() {
         //given
-        Dealer dealer = new Dealer();
         dealer.addCard(new Card(Shape.HEART, Denomination.NINE));
 
         //when then
@@ -31,15 +35,10 @@ class DealerTest {
 
     @Test
     void 딜러는_전달_받은_수만큼_카드덱에서_카드를_뽑아_반환한다() {
-        //given
-        Dealer dealer = new Dealer();
-
         //when
         List<Card> pickedCard = dealer.getPickedCard(4);
 
         //then
         assertThat(pickedCard).hasSize(4);
     }
-
-
 }
