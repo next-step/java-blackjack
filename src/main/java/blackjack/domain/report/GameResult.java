b28d592b6b7780d62b46b39e165757fc75ced712
PandaHun <gnsgnsek@gmail.com>
2022-02-17 21:48:27 +0900

    feat(GameResult): Create compare two CardBundle returning result

--- src/main/java/blackjack/domain/report/GameResult.java
+++ src/main/java/blackjack/domain/report/GameResult.java
@@ -1,6 +1,7 @@
 package blackjack.domain.report;
 
 import blackjack.domain.card.CardBundle;
+import java.util.Arrays;
 
 public enum GameResult {
 
@@ -17,6 +18,35 @@ public enum GameResult {
     }
 
     public static GameResult comparing(CardBundle playerCardBundle, CardBundle dealerCardBundle) {
-        return null;
+        isComparable(playerCardBundle, dealerCardBundle);
+        if (playerCardBundle.isBurst()) {
+            return GameResult.LOSE;
+        }
+        if (dealerCardBundle.isBurst()) {
+            return GameResult.WIN;
+        }
+        int result = Integer.compare(playerCardBundle.calculateScore(),
+            dealerCardBundle.calculateScore());
+        return findResult(result);
+    }
+
+    public String message() {
+        return message;
+    }
+
+    private static void isComparable(CardBundle dealerCardBundle, CardBundle playerCardBundle) {
+        if (dealerCardBundle == null) {
+            throw new IllegalArgumentException("[ERROR] 딜러의 카드가 비었습니다");
+        }
+        if (playerCardBundle == null) {
+            throw new IllegalArgumentException("[ERROR] 플레이어의 카드가 비었습니다.");
+        }
+    }
+
+    private static GameResult findResult(int result) {
+        return Arrays.stream(values())
+            .filter(gameResult -> gameResult.result == result)
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException("[ERROR] 올바르지 않은 값 입니다"));
     }
 }
