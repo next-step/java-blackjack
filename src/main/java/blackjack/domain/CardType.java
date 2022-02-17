ec4503d7b68cb0c76398319185af9409211350cd
gal2o <hunmin8788@gmail.com>
2022-02-18 00:16:22 +0900

    feat(Blackjack): 카드의 문양을 가지는 CardType 생성

--- src/main/java/blackjack/domain/CardType.java
+++ src/main/java/blackjack/domain/CardType.java
@@ -1,4 +1,18 @@
 package blackjack.domain;
 
 public enum CardType {
+    SPADE("스페이드"),
+    CLOBER("클로버"),
+    HEART("하트"),
+    DIAMOND("다이아몬드");
+
+    private final String type;
+
+    CardType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
