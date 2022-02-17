76af023c955cad2ea20e16e7c638a77cabf70145
parkhyojeong <qkrgywjd456@naver.com>
2022-02-17 21:47:50 +0900

    feat: GameController 구현
    Co-authored-by: judy5050 <qkrgywjd456@naver.com>
    Co-authored-by: minchul-son <smc5236@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>

--- src/main/java/blackjack/controller/GameController.java
+++ src/main/java/blackjack/controller/GameController.java
@@ -1,13 +1,21 @@
 package blackjack.controller;
 
-import blackjack.Players;
+import blackjack.domain.Dealer;
+import blackjack.domain.Deck;
+import blackjack.domain.Players;
 import blackjack.view.InputView;
-import java.util.List;
 
 public class GameController {
 
     public void run() {
-        List<String> playerNames = InputView.getNames();
-        Players.create(playerNames);
+        final PeopleController peopleController = new PeopleController(
+            Players.create(InputView.getNames()), new Dealer());
+
+        final Deck deck = Deck.create();
+
+        peopleController.setUpPerson(deck);
+        peopleController.play(deck);
+        peopleController.showGameResult();
     }
+
 }
