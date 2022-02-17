9ee25c206a4662978d08b895f240e11d20f0bb67
kimtaejun97 <kim_taejun@naver.com>
2022-02-18 00:14:08 +0900

    refactor: getWinOrLose 로직 메서드 추출 및 네이밍 변경.

--- src/main/java/blackjack/domain/judge/Judge.java
+++ src/main/java/blackjack/domain/judge/Judge.java
@@ -8,36 +8,43 @@ import java.util.Map;
 public class Judge {
 
     private static final int LIMIT_SCORE = 21;
-    private static final String WIN = "승";
-    private static final String LOSE = "패";
+    private static final String PLAYER_WIN = "승";
+    private static final String PLAYER_LOSE = "패";
     private static final String DEALER_WIN_OR_LOSE_FORMAT = "%d승 %d패";
 
     public Map<String, String> getWinOrLose(final Participant dealer, final List<Participant> players) {
-        final Map<String, String> result = new LinkedHashMap<>();
+        final Map<String, String> winOrLose = new LinkedHashMap<>();
 
-        players.forEach(player -> result.put(
+        players.forEach(player -> winOrLose.put(
             player.getName(),
             getWinOrLosePerPlayer(player.getScore(), dealer.getScore())));
 
-        final int winCount = (int) result.values().stream()
-            .filter(v -> v.equals(LOSE))
-            .count();
+        final int dealerWinCount = getDealerWinCount(winOrLose);
+        winOrLose.put(dealer.getName(),
+            String.format(DEALER_WIN_OR_LOSE_FORMAT,
+                    dealerWinCount, getDealerLoseCount(winOrLose, dealerWinCount)));
 
-        result.put(dealer.getName(),
-            String.format(DEALER_WIN_OR_LOSE_FORMAT, winCount, result.size() - winCount));
-
-        return result;
+        return winOrLose;
     }
 
     private String getWinOrLosePerPlayer(final int playerScore, final int dealerScore) {
         if (playerScore < LIMIT_SCORE && dealerScore > LIMIT_SCORE) {
-            return WIN;
+            return PLAYER_WIN;
         }
-
         if (playerScore > LIMIT_SCORE || playerScore <= dealerScore) {
-            return LOSE;
+            return PLAYER_LOSE;
         }
 
-        return WIN;
+        return PLAYER_WIN;
+    }
+
+    private int getDealerWinCount(Map<String, String> result) {
+        return (int) result.values().stream()
+                .filter(v -> v.equals(PLAYER_LOSE))
+                .count();
+    }
+
+    private int getDealerLoseCount(Map<String, String> winOrLose, int dealerWinCount) {
+        return winOrLose.size() - dealerWinCount;
     }
 }
