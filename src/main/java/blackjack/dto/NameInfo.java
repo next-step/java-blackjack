d85d6e7b3fecdfef41f60671682acf4889b8043b
parkhyojeong <qkrgywjd456@naver.com>
2022-02-17 21:52:01 +0900

    feat: NameInfo 구현
    Co-authored-by: judy5050 <qkrgywjd456@naver.com>
    Co-authored-by: minchul-son <smc5236@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>

---
+++ src/main/java/blackjack/dto/NameInfo.java
@@ -0,0 +1,14 @@
+package blackjack.dto;
+
+public class NameInfo {
+
+    private String personName;
+
+    public NameInfo(String personName) {
+        this.personName = personName;
+    }
+
+    public String getPersonName() {
+        return personName;
+    }
+}
