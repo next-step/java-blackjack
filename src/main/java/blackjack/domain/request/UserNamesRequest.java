10f6ca9d8659dfba803050b8f52bbd5733f2d1ca
PandaHun <gnsgnsek@gmail.com>
2022-02-17 22:05:25 +0900

    test(UserNamesRequest): Test inititalize user names requests

---
+++ src/main/java/blackjack/domain/request/UserNamesRequest.java
@@ -0,0 +1,18 @@
+package blackjack.domain.request;
+
+import java.util.List;
+
+public class UserNamesRequest {
+
+    private UserNamesRequest(List<String> userNames) {
+
+    }
+
+    public static UserNamesRequest from(String userNames) {
+        return null;
+    }
+
+    public List<String> userNames() {
+        return null;
+    }
+}
