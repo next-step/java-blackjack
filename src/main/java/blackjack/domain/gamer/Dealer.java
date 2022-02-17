1bad02bc9d0b96671aed32b44ae4f8036b33a4d7
YangWoo Yu <yuyw0712@naver.com>
2022-02-17 15:07:44 +0900

    test: PlayerTest 테스트 코드 구현 1. 플레이어가_딜러랑_카드합이_같으면_무승부다 2. 플레이어가_딜러보다_카드합이_크면_승리한다 3. 플레이어가_딜러보다_카드합이_작으면_패배한다
    Co-authored-by: seungyeonjeong <crowsy@naver.com>
    Co-authored-by: YuYangWoo <yuyw0712@naver.com>

--- src/main/java/blackjack/domain/gamer/Dealer.java
+++ src/main/java/blackjack/domain/gamer/Dealer.java
@@ -1,8 +1,14 @@
 package blackjack.domain.gamer;
 
-public class Dealer extends Gamer{
+import blackjack.domain.card.Card;
+import java.util.List;
 
+public class Dealer extends Gamer {
     public Dealer() {
         super("딜러");
+    }
+
+    public Dealer(List<Card> cards) {
+        super("딜러", cards);
     }
 }
---
+++ src/test/java/blackjack/domain/gamer/PlayerTest.java
@@ -0,0 +1,44 @@
+package blackjack.domain.gamer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import blackjack.domain.card.Card;
+import blackjack.domain.card.Denomination;
+import blackjack.domain.card.Suit;
+import blackjack.domain.matchInfo.MatchResult;
+import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @Test
+    void 플레이어가_딜러랑_카드합이_같으면_무승부다() {
+        Player player = new Player("yang", Arrays.asList(new Card(Denomination.ACE, Suit.DIAMONDS),
+            new Card(Denomination.TEN, Suit.DIAMONDS)));
+        Dealer dealer = new Dealer(Arrays.asList(new Card(Denomination.ACE, Suit.DIAMONDS),
+            new Card(Denomination.TEN, Suit.DIAMONDS)));
+        MatchResult matchResult = player.getMatchResult(player, dealer);
+        assertThat(matchResult.getName()).isEqualTo("무");
+    }
+
+    @Test
+    void 플레이어가_딜러보다_카드합이_크면_승리한다() {
+        Player player = new Player("yang", Arrays.asList(new Card(Denomination.ACE, Suit.DIAMONDS),
+            new Card(Denomination.TEN, Suit.DIAMONDS)));
+        Dealer dealer = new Dealer(Arrays.asList(new Card(Denomination.ACE, Suit.DIAMONDS),
+            new Card(Denomination.FIVE, Suit.DIAMONDS)));
+        MatchResult matchResult = player.getMatchResult(player, dealer);
+        assertThat(matchResult.getName()).isEqualTo("승");
+    }
+
+    @Test
+    void 플레이어가_딜러보다_카드합이_작으면_패배한다() {
+        Player player = new Player("yang", Arrays.asList(new Card(Denomination.ACE, Suit.DIAMONDS),
+            new Card(Denomination.TWO, Suit.DIAMONDS)));
+        Dealer dealer = new Dealer(Arrays.asList(new Card(Denomination.ACE, Suit.DIAMONDS),
+            new Card(Denomination.FIVE, Suit.DIAMONDS)));
+        MatchResult matchResult = player.getMatchResult(player, dealer);
+        assertThat(matchResult.getName()).isEqualTo("패");
+    }
+}
