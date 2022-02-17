0816dbb1f680d314a79c85dbcbf01fb14d1fffb5
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-17 11:39:23 +0900

    feat: 이름을 사용하는 이외 로직에서도 포장된 이름 객체를 활용하도록 변경

--- src/main/java/blackjack/controller/GameController.java
+++ src/main/java/blackjack/controller/GameController.java
@@ -3,21 +3,35 @@ package blackjack.controller;
 import blackjack.domain.card.CardPack;
 import blackjack.domain.Dealer;
 import blackjack.domain.gameplayer.GamePlayers;
+import blackjack.domain.gameplayer.Name;
+import blackjack.domain.gameplayer.Names;
 import blackjack.view.InputView;
 import blackjack.view.OutputView;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class GameController {
 
     public void start() {
         final CardPack cardPack = CardPack.create();
         final Dealer dealer = new Dealer(cardPack);
-        final GamePlayers gamePlayers = new GamePlayers(InputView.getPlayerName());
+        final GamePlayers gamePlayers = new GamePlayers(getConsoleNames());
 
         dealer.initializeGame(gamePlayers);
         printInitialStatus(gamePlayers);
 
         dealer.playGame(gamePlayers);
         printFinalStatus(gamePlayers);
+    }
+
+    private Names getConsoleNames() {
+        return convertStringsToNames(InputView.getPlayerName());
+    }
+
+    private Names convertStringsToNames(List<String> originNames) {
+        return new Names(originNames.stream()
+            .map(Name::new)
+            .collect(Collectors.toList()));
     }
 
     private void printInitialStatus(GamePlayers gamePlayers) {
