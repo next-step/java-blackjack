f288843a89b8fd1937d1b81edc92b6933018766e
gal2o <hunmin8788@gmail.com>
2022-02-18 00:12:16 +0900

    feat(Blackjack): GamePlayers에 필요한 사용자들이 사용하는 OwnCards 생성

--- src/main/java/blackjack/domain/OwnCards.java
+++ src/main/java/blackjack/domain/OwnCards.java
@@ -1,5 +1,21 @@
 package blackjack.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OwnCards {
 
+    private final List<Card> ownCards;
+
+    public OwnCards() {
+        this.ownCards = new ArrayList<>();
+    }
+
+    public void addCard(Card card) {
+        ownCards.add(card);
+    }
+
+    public List<Card> getOwnCards() {
+        return ownCards;
+    }
 }
