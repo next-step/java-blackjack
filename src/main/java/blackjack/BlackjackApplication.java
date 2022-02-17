37712190bab8f709dc07586ef3fb8107140e9383
한승진 <gkstmdwls100@naver.com>
2022-02-17 22:31:18 +0900

    feat: main 함수 추가

---
+++ src/main/java/blackjack/BlackjackApplication.java
@@ -0,0 +1,10 @@
+package blackjack;
+
+import blackjack.controller.GameController;
+
+public class BlackjackApplication {
+
+    public static void main(String[] args) {
+        GameController.start();
+    }
+}
