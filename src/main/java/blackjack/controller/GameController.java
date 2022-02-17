3a04921b3df94e8d9d6bf17980535bb092e41da5
한승진 <gkstmdwls100@naver.com>
2022-02-17 22:30:56 +0900

    feat: 게임 컨트롤러 추가

--- src/main/java/blackjack/controller/GameController.java
+++ src/main/java/blackjack/controller/GameController.java
@@ -1,40 +1,28 @@
 package blackjack.controller;
 
 import blackjack.domain.Dealer;
-import blackjack.domain.Participant;
 import blackjack.domain.Participants;
 import blackjack.domain.Players;
-import java.util.ArrayList;
+import blackjack.view.InputView;
+import blackjack.view.PrintView;
 import java.util.List;
 
 public class GameController {
 
-    public Participant start() {
-        // 사용자 입력
-        List<String> name = new ArrayList<>();
-        name.add("pobi");
-        name.add("jason");
+    public static void start() {
+        List<String> name = InputView.inputNames();
 
-        // Players 생성
         Players players = Players.from(name);
-
-        // 딜러 추가
-        Dealer dealer = new Dealer("dealer");
-
-        // 사용자 추가
+        Dealer dealer = new Dealer("딜러");
         Participants participants = Participants.from(players, dealer);
 
-        // 딜러 , 사용자 에게 카드 2장씩 분배
         participants.drawCardMultiple(2);
+        PrintView.printFirstDrawCards(participants);
 
-        // 사용자 1장씩 뽑기
-        participants.drawCardMultiple(1);
-
-        // 사용자별 카드 최종 상태 출력
+        participants.drawCardContinue(1);
+        PrintView.printGameResult(participants);
 
         participants.judgeScore();
-        // 최종 승패 출력
-
-        return null;
+        PrintView.printWinLose(participants);
     }
 }
