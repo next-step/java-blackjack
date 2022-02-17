eb50a57f1d5fe56019ccc8942f61ca976c4a1b03
parkhyojeong <qkrgywjd456@naver.com>
2022-02-17 21:44:14 +0900

    feat: ResultView 뷰 로직 구현
    Co-authored-by: judy5050 <qkrgywjd456@naver.com>
    Co-authored-by: minchul-son <smc5236@gmail.com>
    Co-authored-by: kworkbee <g1.tommy.jeon@gmail.com>

--- src/main/java/blackjack/view/ResultView.java
+++ src/main/java/blackjack/view/ResultView.java
@@ -1,5 +1,37 @@
 package blackjack.view;
 
+import static blackjack.domain.Person.INIT_CARD_COUNT;
+
+import blackjack.dto.CardInfo;
+import blackjack.dto.NameInfo;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ResultView {
+
+    private static final String INIT_SHARE_MESSAGE = "%s와 %s에게 %d장을 받았습니다.";
+    private static final String DELIMITER = ", ";
+    private static final String DELETER_FORMAT = "%s: %s";
+    private static final String PLAYER_FORMAT = "%s카드: %s";
+
+    public static void shareCards(String dealerName, List<NameInfo> playerNames) {
+        String playersName = playerNames.stream().map(NameInfo::getPersonName)
+            .collect(Collectors.joining(DELIMITER));
+        System.out.println(String.format(INIT_SHARE_MESSAGE, dealerName, playersName, INIT_CARD_COUNT));
+    }
+
+    public static void openCardInfo(CardInfo dealerCardInfo, List<CardInfo> playerCardsInfo) {
+        System.out.println(String.format(DELETER_FORMAT, dealerCardInfo.getPersonName(), join(dealerCardInfo)));
+
+        for (CardInfo playerCardInfo : playerCardsInfo) {
+            System.out.println(String.format(PLAYER_FORMAT, playerCardInfo.getPersonName(), join(playerCardInfo)));
+        }
+    }
+
+    private static String join(CardInfo cardInfo) {
+        return cardInfo.getCardNames().stream()
+            .collect(Collectors.joining(DELIMITER));
+    }
+
 
 }
