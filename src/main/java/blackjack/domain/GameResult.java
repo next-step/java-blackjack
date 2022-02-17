2c62248ffe0a21dc99fb9ba552eaa3e0fd2c7b59
gal2o <hunmin8788@gmail.com>
2022-02-18 00:43:12 +0900

    feat(Blackjack): 게임 결과에 사용할 GameResult, ResultValue 클래스 생성

--- src/main/java/blackjack/domain/GameResult.java
+++ src/main/java/blackjack/domain/GameResult.java
@@ -1,5 +1,82 @@
 package blackjack.domain;
 
+import static blackjack.domain.ResultValue.DRAW;
+import static blackjack.domain.ResultValue.LOSE;
+import static blackjack.domain.ResultValue.WIN;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class GameResult {
 
+    private final LinkedHashMap<String, ResultValue> gameStatus;
+
+    private GameResult(LinkedHashMap<String, ResultValue> gameStatus) {
+        this.gameStatus = gameStatus;
+    }
+
+    public static GameResult calGameResult(Dealer dealer, Players players) {
+        LinkedHashMap<String, ResultValue> result = new LinkedHashMap<>();
+
+        players.getPlayers()
+            .forEach(player -> result.put(player.getPlayerName(), getPlayerResult(dealer, player)));
+
+        return new GameResult(result);
+    }
+
+    private static ResultValue getPlayerResult(Dealer dealer, Player player) {
+        OwnCards dealerDeck = dealer.ownCards;
+        OwnCards playerDeck = player.ownCards;
+
+        if (playerDeck.isScoreLimit()) {
+            return LOSE;
+        }
+
+        if (dealerDeck.isScoreLimit()) {
+            return WIN;
+        }
+
+        if (playerDeck.isBlackJack() && dealerDeck.isBlackJack()) {
+            return DRAW;
+        }
+
+        if (playerDeck.isBlackJack()) {
+            return WIN;
+        }
+
+        if (dealerDeck.isBlackJack()) {
+            return LOSE;
+        }
+
+        if (playerDeck.getTotalScore() == dealerDeck.getTotalScore()) {
+            return DRAW;
+        }
+
+        if (playerDeck.getTotalScore() > dealerDeck.getTotalScore()) {
+            return WIN;
+        }
+
+        return LOSE;
+    }
+
+    public List<String> getDealerGameResult() {
+        return gameStatus
+            .values()
+            .stream()
+            .map(ResultValue::reverseScore)
+            .collect(Collectors.groupingBy(group -> group, Collectors.counting()))
+            .entrySet()
+            .stream()
+            .sorted(Map.Entry.comparingByKey())
+            .map(m -> m.getValue() + m.getKey().getName())
+            .collect(Collectors.toList());
+    }
+
+    public List<String> getplayerGameResult() {
+        return gameStatus.entrySet().stream()
+            .map(player -> player.getKey() + " : " + player.getValue().getName()).collect(
+                Collectors.toList());
+    }
 }
--- src/main/java/blackjack/domain/ResultValue.java
+++ src/main/java/blackjack/domain/ResultValue.java
@@ -1,5 +1,32 @@
 package blackjack.domain;
 
-public class ResultValue {
+import java.util.Arrays;
 
+public enum ResultValue {
+
+    WIN(1, "승"),
+    DRAW(0, "무"),
+    LOSE(-1, "패");
+
+    private static final String INVALID_KEY = "존재하지 않는 값입니다.";
+    private int score;
+    private String name;
+
+    ResultValue(int score, String name) {
+        this.score = score;
+        this.name = name;
+    }
+
+    private static ResultValue create(int score) {
+        return Arrays.stream(ResultValue.values()).filter(value -> value.score == score)
+            .findFirst().orElseThrow(() -> new IllegalArgumentException(INVALID_KEY));
+    }
+
+    public ResultValue reverseScore() {
+        return ResultValue.create(-score);
+    }
+
+    public String getName() {
+        return name;
+    }
 }
