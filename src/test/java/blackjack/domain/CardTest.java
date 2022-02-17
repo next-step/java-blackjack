e6fd564c3a345b4aa487d89544e7f7e889ca9ef2
한승진 <gkstmdwls100@naver.com>
2022-02-17 18:42:10 +0900

    feat: Test 추가

---
+++ src/test/java/blackjack/domain/CardTest.java
@@ -0,0 +1,68 @@
+package blackjack.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import blackjack.domain.Card.CardType;
+import blackjack.domain.Card.CardValue;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class CardTest {
+
+    @DisplayName("cardValue 가 같은지 확인")
+    @Test
+    void cardValueEqual() {
+        // given
+        Card card1 = new Card(CardType.SPADE, CardValue.KING);
+        Card card2 = new Card(CardType.CLOVER, CardValue.KING);
+
+        // when
+        boolean result = card1.getCardValue().isEqualCardValue(card2);
+
+        // then
+        assertThat(result).isEqualTo(true);
+    }
+
+    @DisplayName("일반카드 점수 확인")
+    @Test
+    void normalCardScore() {
+        // given
+        Card card = new Card(CardType.SPADE, CardValue.KING);
+
+        // when
+        int result = card.getScore(0);
+
+        // then
+        assertThat(result).isEqualTo(10);
+    }
+
+    @DisplayName("에이스 점수가 1이 되는 경우")
+    @ValueSource(ints = {15, 18, 20})
+    @ParameterizedTest
+    void aceCardScoreIsOne(final int currentScore) {
+        // given
+        Card card = new Card(CardType.SPADE, CardValue.ACE);
+
+        // when
+        int result = card.getScore(currentScore);
+
+        // then
+        assertThat(result).isEqualTo(1);
+    }
+
+    @DisplayName("에이스 점수가 1이 되는 경우")
+    @ValueSource(ints = {0, 5, 14})
+    @ParameterizedTest
+    void aceCardScoreIsEleven(final int currentScore) {
+        // given
+        Card card = new Card(CardType.SPADE, CardValue.ACE);
+
+        // when
+        int result = card.getScore(currentScore);
+
+        // then
+        assertThat(result).isEqualTo(11);
+    }
+}
