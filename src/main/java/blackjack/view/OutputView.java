3bd09b55a57f9ec38fc71406e3a694d2b4c86588
kimtaejun97 <kim_taejun@naver.com>
2022-02-18 16:50:09 +0900

    feat: getStartCardNames 추가.
    - 요구 사항 변경.
    - 딜러는 처음에 한장의 카드만을 보여준다.

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,5 +1,6 @@
 package blackjack.view;
 
+import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Participant;
 import java.util.List;
 import java.util.Map;
@@ -7,17 +8,17 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
-    private static final String DELIMITER = ", ";
+    private static final String DEAL_MESSAGE_FORMAT = "%s에게 카드를 %d장씩 분배하였습니다.%n";
     private static final int NUMBER_OF_INIT_HANDS = 2;
-    private static final String DISTRIBUTE_MESSAGE_FORMAT = "%s에게 카드를 %d장씩 분배하였습니다.%n";
+    private static final String DELIMITER = ", ";
     private static final String PARTICIPANT_STATUS_FORMAT = "%s카드: %s%n";
+    private static final int FIRST_CARD = 0;
     private static final String PARTICIPANT_FINAL_STATUS_FORMAT = "%s카드: %s - 결과: %d%n";
     private static final String WINNING_OR_LOSE_HEADER_MESSAGE = "## 최종 승패\n";
     private static final String WINNING_OR_LOSE_FORMAT = "%s: %s%n";
     private static final String DEALER_DRAW_MESSAGE = "딜러는 16이하라 한장의 카드를 더 받았습니다.\n";
 
-    private OutputView() {
-    }
+    private OutputView() {}
 
     public static void printStartStatus(final List<Participant> participants) {
         printDealResult(participants);
@@ -25,27 +26,38 @@ public class OutputView {
     }
 
     private static void printDealResult(final List<Participant> participants) {
-        System.out.printf(DISTRIBUTE_MESSAGE_FORMAT, getNames(participants), NUMBER_OF_INIT_HANDS);
+        System.out.printf(DEAL_MESSAGE_FORMAT, getNames(participants), NUMBER_OF_INIT_HANDS);
     }
 
     private static String getNames(final List<Participant> participants) {
         return participants.stream()
-            .map(Participant::getName)
-            .collect(Collectors.joining(DELIMITER));
+                .map(Participant::getName)
+                .collect(Collectors.joining(DELIMITER));
     }
 
     public static void printParticipantsStatus(final List<Participant> participants) {
         StringBuilder status = new StringBuilder();
         participants.forEach(participant ->
-            status.append(String.format(
-                PARTICIPANT_STATUS_FORMAT, participant.getName(), getCardNames(participant))));
+                status.append(String.format(
+                        PARTICIPANT_STATUS_FORMAT, participant.getName(), getStartCardNames(participant))));
 
         System.out.println(status);
     }
 
+    private static String getStartCardNames(final Participant participant) {
+        if (participant instanceof Dealer) {
+            return getOneCard(participant);
+        }
+        return getCardNames(participant);
+    }
+
+    private static String getOneCard(Participant participant) {
+        return participant.getCardNames().get(FIRST_CARD);
+    }
+
     private static String getCardNames(final Participant participant) {
         return participant.getCardNames().stream()
-            .collect(Collectors.joining(DELIMITER));
+                .collect(Collectors.joining(DELIMITER));
     }
 
     public static void printGameResult(List<Participant> participants, Map<String, String> result) {
@@ -56,8 +68,9 @@ public class OutputView {
     private static void printFinalStatus(List<Participant> participants) {
         StringBuilder status = new StringBuilder();
         participants.forEach(participant ->
-            status.append(String.format(
-                PARTICIPANT_FINAL_STATUS_FORMAT, participant.getName(), getCardNames(participant), participant.getScore())));
+                status.append(String.format(
+                        PARTICIPANT_FINAL_STATUS_FORMAT, participant.getName(), getCardNames(participant),
+                        participant.getScore())));
 
         System.out.println(status);
     }
@@ -65,9 +78,9 @@ public class OutputView {
     private static void printWinOrLose(Map<String, String> results) {
         StringBuilder winOrLose = new StringBuilder(WINNING_OR_LOSE_HEADER_MESSAGE);
 
-        results.forEach((name, result)->
-            winOrLose.append(String.format(
-                WINNING_OR_LOSE_FORMAT, name, result)));
+        results.forEach((name, result) ->
+                winOrLose.append(String.format(
+                        WINNING_OR_LOSE_FORMAT, name, result)));
 
         System.out.println(winOrLose);
     }
