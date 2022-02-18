11525dbd2384eb35f70a9ab58998d4e7e27d3d2f
0JUUU <clleo97@naver.com>
2022-02-18 16:50:03 +0900

    feat: 초기 딜러 카드 출력 관련 수정
    기존 2장이 나오던 결과를 1장만 출력될 수 있도록 변경

--- src/main/java/blackjack/view/OutputView.java
+++ src/main/java/blackjack/view/OutputView.java
@@ -1,5 +1,6 @@
 package blackjack.view;
 
+import blackjack.domain.participant.Dealer;
 import blackjack.domain.participant.Participant;
 import java.util.List;
 import java.util.Map;
@@ -7,10 +8,11 @@ import java.util.stream.Collectors;
 
 public class OutputView {
 
-    private static final String DELIMITER = ", ";
+    private static final String DEAL_MESSAGE_FORMAT = "%s에게 카드를 %d장씩 분배하였습니다.%n";
     private static final int NUMBER_OF_INIT_HANDS = 2;
-    private static final String DISTRIBUTE_MESSAGE_FORMAT = "%s에게 카드를 %d장씩 분배하였습니다.%n";
+    private static final String DELIMITER = ", ";
     private static final String PARTICIPANT_STATUS_FORMAT = "%s카드: %s%n";
+    private static final int FIRST_CARD = 0;
     private static final String PARTICIPANT_FINAL_STATUS_FORMAT = "%s카드: %s - 결과: %d%n";
     private static final String WINNING_OR_LOSE_HEADER_MESSAGE = "## 최종 승패\n";
     private static final String WINNING_OR_LOSE_FORMAT = "%s: %s%n";
@@ -25,7 +27,7 @@ public class OutputView {
     }
 
     private static void printDealResult(final List<Participant> participants) {
-        System.out.printf(DISTRIBUTE_MESSAGE_FORMAT, getNames(participants), NUMBER_OF_INIT_HANDS);
+        System.out.printf(DEAL_MESSAGE_FORMAT, getNames(participants), NUMBER_OF_INIT_HANDS);
     }
 
     private static String getNames(final List<Participant> participants) {
@@ -38,9 +40,20 @@ public class OutputView {
         StringBuilder status = new StringBuilder();
         participants.forEach(participant ->
             status.append(String.format(
-                PARTICIPANT_STATUS_FORMAT, participant.getName(), getCardNames(participant))));
+                PARTICIPANT_STATUS_FORMAT, participant.getName(), getStartCardNames(participant))));
 
         System.out.println(status);
+    }
+
+    private static String getStartCardNames(final Participant participant) {
+        if (participant instanceof Dealer) {
+            return getOneCard(participant);
+        }
+        return getCardNames(participant);
+    }
+
+    private static String getOneCard(Participant participant) {
+        return participant.getCardNames().get(FIRST_CARD);
     }
 
     private static String getCardNames(final Participant participant) {
@@ -57,7 +70,8 @@ public class OutputView {
         StringBuilder status = new StringBuilder();
         participants.forEach(participant ->
             status.append(String.format(
-                PARTICIPANT_FINAL_STATUS_FORMAT, participant.getName(), getCardNames(participant), participant.getScore())));
+                PARTICIPANT_FINAL_STATUS_FORMAT,
+                participant.getName(), getCardNames(participant), participant.getScore())));
 
         System.out.println(status);
     }
@@ -65,7 +79,7 @@ public class OutputView {
     private static void printWinOrLose(Map<String, String> results) {
         StringBuilder winOrLose = new StringBuilder(WINNING_OR_LOSE_HEADER_MESSAGE);
 
-        results.forEach((name, result)->
+        results.forEach((name, result) ->
             winOrLose.append(String.format(
                 WINNING_OR_LOSE_FORMAT, name, result)));
 
