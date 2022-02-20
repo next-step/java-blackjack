035065402da6663873cea41cddc2deca8458218c
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-20 15:15:48 +0900

    feat: CardPackTest 카드팩이 정상적으로 셔플되어지는지 검증 테스트

--- src/test/java/blackjack/domain/card/CardPackTest.java
+++ src/test/java/blackjack/domain/card/CardPackTest.java
@@ -2,6 +2,8 @@ package blackjack.domain.card;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.junit.jupiter.api.Test;
 
@@ -29,5 +31,23 @@ class CardPackTest {
 
         //then
         assertThat(cardPack.getCardPack().size()).isEqualTo(51);
+    }
+
+    @Test
+    public void 카드가_정상적으로_셔플되어진다() {
+        //given
+        CardPack cardPack = new CardPack();
+
+        //when
+        List<Card> originCards = new ArrayList<>();
+        for (final CardSymbol symbol : CardSymbol.values()) {
+            Arrays.stream(CardType.values())
+                .forEach(type -> originCards.add(new Card(symbol, type)));
+        }
+
+        List<Card> shuffleCards = cardPack.getCardPack();
+
+        //then
+        assertThat(originCards).doesNotContainSequence(shuffleCards.toArray(new Card[0]));
     }
 }
