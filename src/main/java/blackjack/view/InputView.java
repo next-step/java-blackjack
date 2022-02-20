d49b8fc9a591808654b75f45fb5892c5cd6f979d
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-20 15:53:46 +0900

    refactor: InputView의 매직넘버 처리

--- src/main/java/blackjack/view/InputView.java
+++ src/main/java/blackjack/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
     private static final Scanner scanner = new Scanner(System.in);
     private static final String GET_PLAYER_MESSAGE = "게임에 참여할 사람의 이름을 입력하세요.";
     private static final String QUESTION_ACCEPT_CARD_MESSAGE = "%s는 한장의 카드를 더 받겠습니까?(예는 y, 아니오는 n)";
-
+    private static final String USER_OK = "y";
 
     public static String getPlayerName() {
         System.out.println(GET_PLAYER_MESSAGE);
@@ -23,7 +23,7 @@ public class InputView {
             System.out.println(String.format(QUESTION_ACCEPT_CARD_MESSAGE, player.getName()));
             String input = scanner.nextLine();
             validateEmpty(input);
-            return input.equals("y");
+            return input.equals(USER_OK);
         } catch (IllegalArgumentException e) {
             e.printStackTrace();
             return getPlayerChoice(player);
