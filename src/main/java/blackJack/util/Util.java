d2ece34d94efb1324c8a54f6ad671bea6fbaa934
chanu94 <qkrcksdn0208@naver.com>
2022-02-21 23:39:59 +0900

    refactor : 불필요한 메서드 제거

--- src/main/java/blackJack/util/Util.java
+++ src/main/java/blackJack/util/Util.java
@@ -5,15 +5,6 @@ import java.util.List;
 
 public class Util {
 
-    public static boolean isNumber(String number) {
-        try {
-            Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            return false;
-        }
-        return true;
-    }
-
     public static List<String> stringToStringList(String unrefinedString) {
         return Arrays.asList(unrefinedString.split(", "));
     }
