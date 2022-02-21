7dfdabf9679c6b5f43d16596e08bfb657aaf0471
Dami Kim <dami.k.dev@gmail.com>
2022-02-21 22:25:23 +0900

    refactor: getter 위치 하단으로 이동 (CardPack)

--- src/main/java/blackjack/domain/card/CardPack.java
+++ src/main/java/blackjack/domain/card/CardPack.java
@@ -8,26 +8,29 @@ import java.util.List;
 public class CardPack {
 
     private final List<Card> cardPack;
+    private static final List<Card> cards = new ArrayList<>();
+
+    static {
+        for (final CardSymbol symbol : CardSymbol.values()) {
+            Arrays.stream(CardType.values())
+                .forEach(type -> cards.add(new Card(symbol, type)));
+        }
+    }
 
     public CardPack(List<Card> cardPack) {
         this.cardPack = cardPack;
     }
 
     public static CardPack create() {
-        final List<Card> cards = new ArrayList<>();
-        for (final CardSymbol symbol : CardSymbol.values()) {
-            Arrays.stream(CardType.values())
-                .forEach(type -> cards.add(new Card(symbol, type)));
-        }
         Collections.shuffle(cards);
         return new CardPack(new ArrayList<>(cards));
     }
 
-    public List<Card> getCardPack() {
-        return Collections.unmodifiableList(cardPack);
-    }
-
     public Card pick() {
         return cardPack.remove(0);
+    }
+
+    public List<Card> getCardPack() {
+        return Collections.unmodifiableList(cardPack);
     }
 }
