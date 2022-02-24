b7208608810a2b7ece2556e8d5ec80711186ce4e
kimsunghyun <dhkdghehfdl@gmail.com>
2022-02-24 21:05:53 +0900

    feat: Card 값 비교를 위해 equals and hashcode 추가

--- src/main/java/blackjack/domain/card/Card.java
+++ src/main/java/blackjack/domain/card/Card.java
@@ -1,5 +1,7 @@
 package blackjack.domain.card;
 
+import java.util.Objects;
+
 public class Card {
 
     private final CardSymbol cardSymbol;
@@ -16,5 +18,22 @@ public class Card {
 
     public CardType getCardType() {
         return cardType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Card card = (Card) o;
+        return cardSymbol == card.cardSymbol && cardType == card.cardType;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(cardSymbol, cardType);
     }
 }
