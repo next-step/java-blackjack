42d6313cffd5a52126b62cb0364087b6d65601f5
Jaegil Jeong <jaegil123@naver.com>
2024-06-18 15:56:27 +0900

    final

--- src/main/java/blackjack/BlackJackController.java
+++ src/main/java/blackjack/BlackJackController.java
@@ -8,6 +8,35 @@ import java.util.stream.Stream;
 
 public class BlackJackController {
     public static void main(String[] args) {
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
 
+        final List<String> names = inputView.insertPlayer();
+        List<Player> players = names.stream()
+                .map(name -> new Player(name))
+                .toList();
+
+        Dealer dealer = new Dealer();
+
+        //init
+        dealer.deal(dealer);
+        dealer.deal(dealer);
+        for(Player player : players) {
+            dealer.deal(player);
+            dealer.deal(player);
+        }
+        inputView.init(dealer, players);
+        for(Player player : players) {
+            while (inputView.isProvide(player.getName())) {
+                dealer.deal(player);
+                inputView.printCards(player);
+            }
+        }
+        while (outputView.isProvide(dealer)) {
+            dealer.deal(dealer);
+        }
+        outputView.printTotalScore(dealer, players);
+        final Table table = new Table(players, dealer);
+        outputView.printResult(table);
     }
 }
